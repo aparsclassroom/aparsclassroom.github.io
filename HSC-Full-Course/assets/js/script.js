f0c83964d19cfb42638a20564b57133f8c4dd5eb
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-01-07 12:26:26 +0600

    up

--- HSC-Full-Course/assets/js/script.js
+++ HSC-Full-Course/assets/js/script.js
@@ -1,26 +1,80 @@
-$( document ).ready(function() {
-    var w = window.innerWidth;
-   
-    if(w > 767){
-        $('#menu-jk').scrollToFixed();
-    }else{
-        $('#menu-jk').scrollToFixed();
-    }
-    
-})
-
-
-
-
-$(document).ready(function(){
-    $("#testimonial-slider").owlCarousel({
-        items:1,
-        itemsDesktop:[1000,1],
-        itemsDesktopSmall:[979,1],
-        itemsTablet:[768,1],
-        pagination:false,
-        navigation:true,
-        navigationText:["",""],
-        autoPlay:true
-    });
+var app_firebase = {};
+(function () { 
+    var firebaseConfig = {
+     apiKey: "AIzaSyDWpAK3PGkY_mCf2tH-n4kLiK_ffj2TnWg",
+    authDomain: "hsc-full-course-website.firebaseapp.com",
+    projectId: "hsc-full-course-website",
+    storageBucket: "hsc-full-course-website.appspot.com",
+    messagingSenderId: "202898992993",
+    appId: "1:202898992993:web:fa6cc64869a75017a8df20",
+    measurementId: "G-C0LYS6QJ6G"
+  };
+  firebase.initializeApp(firebaseConfig);
+  app_firebase = firebase;
+})()
+document.addEventListener("contextmenu", function(e) {
+  e.preventDefault();
 });
+     var mainApp = {};
+(function() {
+    var firebase = app_firebase;
+var uid = null;
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    uid = user.uid;
+    uname = user.displayName;
+    email = user.email;
+    secured = user.providerId;
+    emailVerified = user.emailVerified;
+    photoUrl = user.photoURL;
+    document.getElementById("login").style.display = "none";
+        document.getElementById("dp").innerHTML = '<img src="' + photoUrl +'" class="rounded mx-auto d-block" alt="dp">';
+        document.getElementById("userName").innerHTML = "<strong>Name :</strong> " + uname;
+        document.getElementById("userName2").innerHTML = "<strong>Phone :</strong> N/A";
+        document.getElementById("uemail").innerHTML = "<strong>Email :</strong> " + email;
+        document.getElementById("licenseFor").innerHTML = "<strong>License :</strong> " + uid;
+        if(emailVerified == true) {
+          document.getElementById("Subscription").innerHTML = "<strong>Subscription :</strong> Free Trial";
+        }
+        if(email == null) {
+          document.getElementById("uemail").innerHTML = "<strong>Email :</strong> N/A";
+        }
+        if(emailVerified != true) {
+          document.getElementById("Subscription").innerHTML = "<strong>Subscription :</strong> Free Trial";
+        }
+        if (photoUrl == null) {
+            document.getElementById("dp").innerHTML = "";
+        }
+        if(uname == null) {
+          document.getElementById("userName").innerHTML = "<strong>Name :</strong> N/A";
+          document.getElementById("uemail").innerHTML = "<strong>Email :</strong> N/A";
+          document.getElementById("dp").innerHTML = "";
+        }
+if (user != null) {
+  user.providerData.forEach(function (profile) {
+    document.getElementById("secured").innerHTML = "<strong>Verified by :</strong> " + profile.providerId;
+  });
+}
+if (user != null && uname == null) {
+  user.providerData.forEach(function (profile) {
+    document.getElementById("secured").innerHTML = "<strong>Verified by :</strong> " + profile.providerId;
+    document.getElementById("userName2").innerHTML = "<strong>Phone :</strong> " + profile.uid;
+  });
+}
+  }else {
+    document.getElementById("login").style.display = "inline-block";
+    document.getElementById("accmd").style.display = "none";
+  }
+});
+    function logOut(){
+        firebase.auth().signOut();
+    document.getElementById("login").style.display = "inline-block";
+    document.getElementById("accmd").style.display = "none";
+     $('#accModal').modal('hide');
+     alert("signed out");
+    }
+    mainApp.logOut = logOut;
+})()
+function onSubmit(token) {
+      document.getElementById("form").submit();
+    }
