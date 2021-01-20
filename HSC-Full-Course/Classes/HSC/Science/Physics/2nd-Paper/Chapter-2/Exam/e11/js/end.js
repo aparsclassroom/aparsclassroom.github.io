a8cecef82e78342d271735e378733921ea1b8d6e
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-01-20 16:26:26 +0600

    updated

--- HSC-Full-Course/Classes/HSC/Science/Physics/2nd-Paper/Chapter-2/Exam/e11/js/end.js
+++ HSC-Full-Course/Classes/HSC/Science/Physics/2nd-Paper/Chapter-2/Exam/e11/js/end.js
@@ -11,11 +11,15 @@ const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
-function scoreUpdated () {
-score.value = mostRecentScore;
-$.get('https://json.geoiplookup.io/', function(res) {var a = ("IP Address : " + res.ip + "\n" + "ISP : " + res.isp + "\n" + "Organization : " + res.org + "\n" + "Hostname : " + res.hostname + "\n" + "Latitude : " + res.latitude + "\n" + "Longitude : " + res.longitude + "\n" + "Postal Code : " + res.postal_code + "\n" + "Neighbourhood : " + res.city + "\n" + "Region : " + res.region + "\n" + "District : " + res.district + "\n" + "Country Code : " + res.country_code + "\n" + "Country : " + res.country_name + "\n" + "Continent : " + res.continent_name + "\n" + "Timezone Name : " + res.timezone_name + "\n" + "Connection Tyoe : " + res.connection_type + "\n" + "ASN Organization : " + res.asn_org + "\n" + "ASN : " + res.asn + "\n" + "Currency Code : " + res.currency_code  + "\n" + "Currency : " + res.currency_name);document.getElementById("ip-details").value = a;});
+
+function scoreUpdated() {
+    score.value = mostRecentScore;
+    $.get('https://json.geoiplookup.io/', function(res) {
+        var a = ("IP Address : " + res.ip + "\n" + "ISP : " + res.isp + "\n" + "Organization : " + res.org + "\n" + "Hostname : " + res.hostname + "\n" + "Latitude : " + res.latitude + "\n" + "Longitude : " + res.longitude + "\n" + "Postal Code : " + res.postal_code + "\n" + "Neighbourhood : " + res.city + "\n" + "Region : " + res.region + "\n" + "District : " + res.district + "\n" + "Country Code : " + res.country_code + "\n" + "Country : " + res.country_name + "\n" + "Continent : " + res.continent_name + "\n" + "Timezone Name : " + res.timezone_name + "\n" + "Connection Tyoe : " + res.connection_type + "\n" + "ASN Organization : " + res.asn_org + "\n" + "ASN : " + res.asn + "\n" + "Currency Code : " + res.currency_code + "\n" + "Currency : " + res.currency_name);
+        document.getElementById("ip-details").value = a;
+    });
 }
-scoreUpdated ()
+scoreUpdated()
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
     online.disabled = !username.value;
@@ -37,13 +41,16 @@ saveHighScore = (e) => {
 };
 
 
-const scriptURL = '#'
- const form = document.forms['highScore']
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw-3McRe6seMWisYWi_dWsIAOfS6GJZEuNiUduWmdTzN5e7lN7AmJ1kJw/exec'
+const form = document.forms['highScore']
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-       .then(_response => alert("Saved Online!!!"))
-       .catch(error => console.error('Error!', error.message))
-       document.getElementById("online").style.display = "none";
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(_response => alert("Saved Online!!!"))
+        .catch(error => console.error('Error!', error.message))
+    document.getElementById("online").innerText = "Please Wait..";
+    setTimeout(function timer() {
+        document.getElementById("online").style.display = "none";
+    }, 2500)
 })
