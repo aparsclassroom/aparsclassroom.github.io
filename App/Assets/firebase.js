1230f0f33e34904c79b2401a946a06a3cb9a3bad
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-01-29 02:01:29 +0600

    up

--- App/Assets/firebase.js
+++ App/Assets/firebase.js
@@ -1,19 +1,19 @@
-// var app_firebase = {};
-// (function() {
-//     var firebaseConfig = {
-//         apiKey: "AIzaSyB3jSPyVWw1xFZwM2QHvEvGfjka1bvPJdU",
-//         authDomain: "apars-qna-app.firebaseapp.com",
-//         projectId: "apars-qna-app",
-//         storageBucket: "apars-qna-app.appspot.com",
-//         messagingSenderId: "1020053403181",
-//         appId: "1:1020053403181:web:f32e42e90b160f5402eaa4",
-//         measurementId: "G-9GFZCDHVVJ"
-//     };
-//     // Initialize Firebase
-//     firebase.initializeApp(firebaseConfig);
-//     app_firebase = firebase;
-//     firebase.analytics();
-// })()
-// document.addEventListener("contextmenu", function(e) {
-//   e.preventDefault();
-// });
+var app_firebase = {};
+(function() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyB3jSPyVWw1xFZwM2QHvEvGfjka1bvPJdU",
+        authDomain: "apars-qna-app.firebaseapp.com",
+        projectId: "apars-qna-app",
+        storageBucket: "apars-qna-app.appspot.com",
+        messagingSenderId: "1020053403181",
+        appId: "1:1020053403181:web:f32e42e90b160f5402eaa4",
+        measurementId: "G-9GFZCDHVVJ"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    app_firebase = firebase;
+    firebase.analytics();
+})()
+document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+});
