ed4c6a94c05da5e3bd3845269a00244b3225997e
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-02-17 19:12:48 +0600

    up

--- BioDictionary/Assets/js/explain.js
+++ BioDictionary/Assets/js/explain.js
@@ -13,67 +13,91 @@ window.addEventListener('load', function() {
         .then((loadedData) => {
             a = loadedData;
 
-            var Question = a[serial].Word;
+            var Word = a[serial].Word;
             var Subject = a[serial].Subject;
             var chapter = a[serial].Chapter;
-            var Topic = a[serial].Ref;
-            var Answer1 = a[serial].Meaning;
-            var Answer2 = a[serial].Description;
+            var ref = a[serial].Ref;
+            var meaning = a[serial].Meaning;
+            var description = a[serial].Description;
             var img = a[serial].img;
+            var video = a[serial].Video;
+            var audio = a[serial].Audio;
             var link = a[serial].Return;
             document.title = a[serial].Word + ' | BioDictionary';
+            // document.getElementById('explainations').innerHTML = `
+
+            //     <h3>${Word}</h3>
+            //     <p></p>
+            //                 <audio controls>
+            //                 <source src="${audio}" type="audio/mpeg">
+            //               Your browser does not support the audio element.
+            //               </audio>
+            //                 </div>
+
+            // `;
             document.getElementById('qnaCard').innerHTML = `
 
-                <div class="wrapper">
-            <div class="card front-face">
-                <div class="info">
-                    <div class="title">প্রশ্ন</div>
-                    <div class="ques">
-                        <p id="qu" style="font-size: 20px;">${Question}</p>
+                    <div class="wrapper">
+                <div class="card front-face">
+                    <div class="info">
+                        <div class="title">প্রশ্ন</div>
+                        <div class="ques">
+                            <p id="qu" style="font-size: 20px;">${Word}</p>
+                        </div>
+                        <div class="meta">
+                            <div class="sub">
+                                <p>${Subject}</p>
+                            </div>
+                            <div class="chap">
+                                <p>${chapter}</p>
+                            </div>
+                            <div class="chapname">
+                                <p>${ref}</p>
+                            </div>
+                            <img src="/App/Assets/Logo/Asset 2[190].svg">
+                            <div class="contributor">
+                                <p>&copy; Apar's Classroom</p>
+                            </div>
+                        </div>
                     </div>
-                    <div class="meta">
-                        <div class="sub">
-                            <p>${Subject}</p>
+                </div>
+                <div id="back" class="card back-face">
+                    <div id="visans" class="title">চিত্র</div>
+                    <img src="${img}" id="gif" style="display: none;">
+                    <div class="info">
+                        <div id="textAns">
+                            <div class="title">উত্তর</div>
+                            <div class="ans">
+                                <p>${meaning}</p>
+                                <p>${description}</p>
+                            </div>
                         </div>
-                        <div class="chap">
-                            <p>${chapter}</p>
+                        <div id="aud">
+                        <audio controls>
+                        <source src="${audio}" type="audio/mpeg">
+                      Your browser does not support the audio element.
+                      </audio>
                         </div>
-                        <div class="chapname">
-                            <p>${Topic}</p>
-                        </div>
-                        <img src="/App/Assets/Logo/Asset 2[190].svg">
-                        <div class="contributor">
-                            <p>&copy; Apar's Classroom</p>
+                        <div class="visit">
+                            <a id="change1" type="button" onclick="openVisualise()">Visualise</a>
+                            <a id="change2" type="button" onclick="openText()">Text</a>
+                            <a type="button" href="${link}">Return</a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div id="back" class="card back-face">
-                <div id="visans" class="title">চিত্র</div>
-                <img src="${img}" id="gif" style="display: none;">
-                <div class="info">
-                    <div id="textAns">
-                        <div class="title">উত্তর</div>
-                        <div class="ans">
-                            <p>${Answer1}</p>
-                            <p>${Answer2}</p>
-                        </div>
-                    </div>
-                    <div class="visit">
-                        <a id="change1" type="button" onclick="openVisualise()">Visualise</a>
-                        <a id="change2" type="button" onclick="openText()">Text</a>
-                        <a type="button" href="${link}">Return</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-                `;
+                    `;
             if (img == "") {
                 document.getElementById('change1').style.display = "none";
             } else {
                 document.getElementById('change1').style.cssText = "display:inline-block;";
             }
 
+            if (audio == "") {
+                document.getElementById('aud').style.display = "none";
+            } else {
+                document.getElementById('aud').style.cssText = "display:inline-block;";
+            }
 
         })
         .catch((err) => {
