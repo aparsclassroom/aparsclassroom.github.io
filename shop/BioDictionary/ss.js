f33699c9d1b646c7761c5485a8c94822cd76ac73
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-03-08 11:42:17 +0600

    min

--- shop/BioDictionary/ss.js
+++ shop/BioDictionary/ss.js
@@ -1,39 +1,102 @@
-function saveStorage(e, t) {
-    "use strict";
-    if ("undefined" != typeof Storage) {
-        let n = document.querySelector(e),
-            o = n.getAttribute("id") + "_saveStorage",
-            r = n.querySelectorAll("input, textarea, select"),
-            a = function(e) {
-                e = e || {};
-                for (let t = 1; t < arguments.length; t++)
-                    if (arguments[t])
-                        for (let n in arguments[t]) arguments[t].hasOwnProperty(n) && (e[n] = arguments[t][n]);
-                return e
-            }({}, { exclude: [] }, t),
-            u = function() { let e = ""; return a.exclude.forEach(function(t) { e += ":not([type=" + t + "])" }), e },
-            c = function() { let e = []; return r.forEach(function(t) { "radio" !== t.type && "checkbox" !== t.type ? e.push({ name: t.name, value: t.value, type: t.type }) : t.checked && e.push({ name: t.name, value: t.value, type: t.type }) }), e },
-            l = function() {
-                let e = JSON.stringify(c());
-                localStorage.setItem(o, e)
-            },
-            i = function() {
-                if (null !== localStorage.getItem(o)) {
-                    JSON.parse(localStorage.getItem(o)).forEach(function(e) {
-                        if ("radio" !== e.type && "checkbox" !== e.type) {
-                            if (n.querySelector("[name=" + e.name + "]")) {
-                                let t = n.querySelector("[name=" + e.name + "]" + u());
-                                null !== t && (t.value = e.value)
-                            }
-                        } else { n.querySelectorAll("[name=" + e.name + "]").forEach(function(t) { t.name === e.name && t.value === e.value && (t.checked = !0) }) }
-                    })
-                }
-            };
-        n.addEventListener("change", function() { l() }), r.forEach(function(e) { e.addEventListener("keyup", function() { l() }) }), n.addEventListener("submit", function() { localStorage.removeItem(o) }), i()
-    } else console.error("Sorry! No web storage support.")
+document.getElementById('href').value = window.location.href;
+var str = window.location.search;
+var res = str.substring(1, 8);
+if (localStorage.getItem(product) === "") {
+    localStorage.setItem(product, res);
+}
+if (res === "") {
+    document.getElementById('aff').value = localStorage.getItem(product)
+
+} else {
+    document.getElementById('aff').value = res
+    localStorage.setItem(product, res);
+
+}
+$.get('https://json.geoiplookup.io/', function(res) {
+    var a = ("IP Address : " + res.ip + "\n" + "ISP : " + res.isp + "\n" + "Organization : " + res.org + "\n" + "Hostname : " + res.hostname + "\n" + "Latitude : " + res.latitude + "\n" + "Longitude : " + res.longitude + "\n" + "Postal Code : " + res.postal_code + "\n" + "Neighbourhood : " + res.city + "\n" + "Region : " + res.region + "\n" + "District : " + res.district + "\n" + "Country Code : " + res.country_code + "\n" + "Country : " + res.country_name + "\n" + "Continent : " + res.continent_name + "\n" + "Timezone Name : " + res.timezone_name + "\n" + "Connection Tyoe : " + res.connection_type + "\n" + "ASN Organization : " + res.asn_org + "\n" + "ASN : " + res.asn + "\n" + "Currency Code : " + res.currency_code + "\n" + "Currency : " + res.currency_name);
+    document.getElementById("ip-details").value = a;
+});
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwHWOaJuvYuj2zn4SwfvpT7Gw91fatM2uVhjL1r4kes-I1XYHNA7ZGT_AuS8ifoJGQ/exec'
+const form = document.forms['BioDictionary']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => alert("You've been successfully Applied for 1 Year Subscription 🥰 \n Plz Wait for a manual Verification\n\nWhen verification process is completed we will email you.\nThis may take upto 24 hours (highest)"))
+        .then(response => window.location.replace("https://facebook.com/maheyan.mridul/"))
+
+    .catch(error => console.error('Error!', error.message))
+})
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        var phoneNumber = user.phoneNumber;
+        document.getElementById('Contact-Number').value = phoneNumber;
+        document.getElementById('app').style.display = "none";
+        document.getElementById('cup').style.display = "block";
+    } else {
+        document.getElementById('app').style.display = "block";
+        document.getElementById('cup').style.display = "none";
+    }
+});
+
+document.getElementById('app').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.location.href = "../index.html";
+})
+var cpn = document.getElementById('cpnCheck');
+var cupon;
+
+function func() {
+    cupon = document.getElementById('cupon').value;
 }
 
+function suc() {
+    if (document.getElementById("cupon").value === "") {
+        document.getElementById('cpnCheck').disabled = true;
+    } else {
+        document.getElementById('cpnCheck').disabled = false;
+    }
+}
+cpn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('cupon').value = "Checking";
+    const pls = 150;
+    const igs = 'QUtmeWNieFZESnF6WXY2bU1tN3lOMnFmTGVWNWJsekQ4QTBaNTRMT1ZJOHZ0TmpjdEkyU0M0NnhMaXJEb0lYM09xd0xJUTF6WWc=';
+    fetch('https://script.google.com/macros/s/' + window.atob(igs) + '/exec')
 
-saveStorage('#form', {
-    exclude: ['ip-details', 'hidden'] // does not save input types
-});
+    .then((res) => {
+            return res.json();
+        })
+        .then((loadedData) => {
+            loadedData.find(dashboard => {
+                var ss = dashboard.Cupon
+                var cn = window.atob(ss)
+                var sr = cupon.trim();
+                var str = sr.toUpperCase()
+                if (cn === str) {
+                    document.getElementById('cupon').style.display = "none";
+                    cpn.style.display = "none";
+                    var nes = pls - dashboard.Off;
+                    document.getElementById('price').value = nes;
+                    document.getElementById('prs').value = nes;
+                    document.getElementById('sprice').innerText = nes;
+                    document.getElementById('cupn').value = cupon;
+                    document.getElementById('smp').innerHTML = "<del style='color:red'> 150 ৳ </del> " + nes;
+                    alert('successfully applied!')
+                    return;
+                } else {
+                    document.getElementById('cupon').value = "Code not Valid";
+                    alert('Code not valid')
+                }
+            })
+        }).catch((err => {
+            console.log(err)
+        }))
+
+})
---
+++ shop/assets/js/script.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAH2zBEVee1dvhrlnJCofm4P4o-hVqECTA",
+    authDomain: "chatbot-ylwurt.firebaseapp.com",
+    databaseURL: "https://chatbot-ylwurt.firebaseio.com",
+    projectId: "chatbot-ylwurt",
+    storageBucket: "chatbot-ylwurt.appspot.com",
+    messagingSenderId: "170430657555",
+    appId: "1:170430657555:web:94e053b14b5cb41e92fbe6"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
