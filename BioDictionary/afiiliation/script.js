46890cc800ac698490f264b8ac8daee45de50335
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-03-17 16:42:52 +0600

    fix

--- BioDictionary/afiiliation/script.js
+++ BioDictionary/afiiliation/script.js
@@ -28,6 +28,9 @@ function initApp() {
                 document.getElementById('email').value = user.email;
                 document.getElementById('uid').value = user.uid;
                 document.getElementById('afflink').innerText = loadedData.Affiliation_Link;
+                document.getElementById('totalPending').innerText = loadedData.Total_Pending;
+                document.getElementById('totalEarning').innerText = loadedData.Total_Income;
+                document.getElementById('ttsell').innerText = loadedData.Total_Sell;
                 college.value = loadedData.College_University;
                 document.getElementById('Mobile').value = loadedData.Mobile;
                 Bkash.value = loadedData.Bkash;
@@ -58,110 +61,6 @@ function initApp() {
                         updateBtn.innerText = "Can't Update 😶";
                     })
                 })
-                var label = ['Link 1', 'Link 2'];
-                var ctx = document.getElementById('totalPending').getContext('2d');
-                var myChart = new Chart(ctx, {
-                    type: 'line',
-                    data: {
-                        labels: label,
-                        datasets: [{
-                            label: 'Pending Earnings',
-                            data: [penl1, penl2, penl3, penl4, penl5],
-                            backgroundColor: [
-                                'rgba(255, 99, 132, 0.2)',
-                                'rgba(54, 162, 235, 0.2)',
-                                'rgba(255, 206, 86, 0.2)',
-                                'rgba(75, 192, 192, 0.2)',
-                                'rgba(153, 102, 255, 0.2)'
-                            ],
-                            borderColor: [
-                                'rgba(255, 99, 132, 1)',
-                                'rgba(54, 162, 235, 1)',
-                                'rgba(255, 206, 86, 1)',
-                                'rgba(75, 192, 192, 1)',
-                                'rgba(153, 102, 255, 1)'
-                            ],
-                            borderWidth: 1
-                        }]
-                    },
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true
-                                }
-                            }]
-                        }
-                    }
-                });
-                var ctx = document.getElementById('totalEarning').getContext('2d');
-                var myChart = new Chart(ctx, {
-                    type: 'bar',
-                    data: {
-                        labels: label,
-                        datasets: [{
-                            label: 'Total Sell',
-                            data: [sell1, sell2, sell3, sell4, sell5],
-                            backgroundColor: [
-                                'rgba(255, 99, 132, 0.2)',
-                                'rgba(54, 162, 235, 0.2)',
-                                'rgba(255, 206, 86, 0.2)',
-                                'rgba(75, 192, 192, 0.2)',
-                                'rgba(153, 102, 255, 0.2)'
-                            ],
-                            borderColor: [
-                                'rgba(255, 99, 132, 1)',
-                                'rgba(54, 162, 235, 1)',
-                                'rgba(255, 206, 86, 1)',
-                                'rgba(75, 192, 192, 1)',
-                                'rgba(153, 102, 255, 1)'
-                            ],
-                            borderWidth: 1
-                        }]
-                    },
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true
-                                }
-                            }]
-                        }
-                    }
-                });
-
-                var ctx = document.getElementById("ttsell").getContext("2d");
-
-                var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
-
-                gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
-                gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
-                gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
-
-
-                var myChart = new Chart(ctx, {
-                    type: 'bar',
-                    responsive: true,
-                    legend: {
-                        display: false
-                    },
-                    data: {
-                        labels: label,
-                        datasets: [{
-                            label: "Verified Income",
-                            fill: true,
-                            backgroundColor: gradientStroke,
-                            hoverBackgroundColor: gradientStroke,
-                            borderColor: '#1f8ef1',
-                            borderWidth: 2,
-                            borderDash: [],
-                            borderDashOffset: 0.0,
-                            data: [verl1, verl2, verl3, verl4, verl5],
-                        }]
-                    },
-                    options: gradientBarChartConfiguration
-                });
-
                 var gradientBarChartConfiguration = {
                     maintainAspectRatio: false,
                     legend: {
