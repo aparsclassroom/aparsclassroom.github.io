b7f1735b3096b06222c1cb436f22b87a730886f2
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-03-21 14:38:34 +0600

    Update Videospage.js

--- BioDictionary/Assets/js/Videospage.js
+++ BioDictionary/Assets/js/Videospage.js
@@ -3,8 +3,13 @@ var mainApp = {};
     var firebase = app_firebase;
     var uid = null;
     firebase.auth().onAuthStateChanged(function(user) {
-        if (user.isAnonymous === false) {
-            uid = user.uid;
+        if (user) {
+            if (user.isAnonymous === true) {
+                alert("This is a Premium Feature!");
+                return;
+            } else {
+                uid = user.uid;
+            }
         } else {
             window.location.replace("/BioDictionary/login.html");
         }
