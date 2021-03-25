120fe3e013268c844160398f6ad0928ae340e35d
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-03-25 18:37:40 +0600

    updated

--- BioDictionary/Assets/js/chz.js
+++ BioDictionary/Assets/js/chz.js
@@ -4,6 +4,14 @@ var mainApp = {};
     var uid = null;
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
+            if (user.isAnonymous === true) {
+                var x = document.querySelectorAll(".Bio");
+                var i;
+                for (i = 0; i < x.length; i++) {
+                    x[i].classList.add("lock");
+                    x[i].removeAttribute("href");
+                }
+            }
             console.log("%cDon't YOU Ever Try To STEAL the SOURCE CODE 🤬", "color:red;Background-Color:white;padding:100px;font-size:50px")
         } else {
             window.location.replace("/BioDictionary/login.html");
