885dcffdd36cef5c17f4111384dd1c1938e4e26e
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-03-25 23:30:54 +0600

    updared

--- BioDictionary/Assets/js/scriptB.js
+++ BioDictionary/Assets/js/scriptB.js
@@ -92,7 +92,10 @@ var mainApp = {};
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h4 class="modal-title">Bookmarks</h4>
-                                    <a type="button" href="/BioDictionary/dashboard.html"><img src="/BioDictionary/Assets/images/Icons/profile.png" alt="dp" height="25px" width="25px"></a>
+                                    <a type="button" href="/BioDictionary/dashboard.html"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="yellow" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                                  </svg></a>
                                 </div>
                                 <div class="modal-body">
                                     <div id="bookmarksResults"></div>
