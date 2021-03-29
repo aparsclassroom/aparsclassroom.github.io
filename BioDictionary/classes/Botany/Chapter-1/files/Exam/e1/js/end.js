841a9ce5a88204f85bd074050a98d7b3194c7d14
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-03-30 00:26:57 +0600

    up

--- BioDictionary/classes/Botany/Chapter-1/files/Exam/e1/js/end.js
+++ BioDictionary/classes/Botany/Chapter-1/files/Exam/e1/js/end.js
@@ -1,4 +1,5 @@
 const usid = document.getElementById('uid');
+const user = document.getElementById('user');
 const username = document.getElementById('username');
 const score = document.getElementById('score');
 const duration = document.getElementById('duration');
@@ -21,6 +22,7 @@ var mainApp = {};
             } else {
                 uid = user.uid;
                 usid.value = uid;
+                user.value = user.displayName;
                 username.value = user.displayName;
                 finalScore.innerText = mostRecentScore;
                 document.querySelector("span.time_taken").innerHTML = user_min + " min " + user_sec + " sec";
