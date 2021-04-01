749f24e66aab440922e11629e152569441323f92
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-04-01 12:30:11 +0600

    Update dashboard.js

--- BioDictionary/Assets/js/dashboard.js
+++ BioDictionary/Assets/js/dashboard.js
@@ -20,12 +20,13 @@ window.addEventListener('load', () => {
     console.log("%cDon't YOU Ever Try To STEAL the SOURCE CODE 🤬", "color:red;Background-Color:white;padding:100px;font-size:50px")
     aseKina()
     aseKinaRecent()
-
-    if (piex < 1) {
-        document.getElementById('tracker').style.display = "none";
-    } else {
-        var ctx = document.getElementById('chart-area').getContext('2d');
-        window.myPie = new Chart(ctx, config);
+    if (things != null) {
+        if (piex < 1) {
+            document.getElementById('tracker').style.display = "none";
+        } else {
+            var ctx = document.getElementById('chart-area').getContext('2d');
+            window.myPie = new Chart(ctx, config);
+        }
     }
 })
 
