2c39bf8ee437e269ea6033b048e461de97549a7b
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-04-02 11:44:30 +0600

    Update solve.js

--- BioDictionary/Assets/js/solve.js
+++ BioDictionary/Assets/js/solve.js
@@ -25,7 +25,7 @@ var mainApp = {};
                                 .then((loadedQuestions) => {
                                     if (loadedQuestions.code === 200) {
                                         var exam = JSON.parse(loadedQuestions.Exam);
-                                        var ans1 = exam[0].choice1 + " " + exam[0].answer;
+                                        var ans1 = "exam[0].choice" + exam[0].answer;
                                         var a1 = ans1;
                                         document.getElementById('solve').innerHTML = `
                                         <h3>প্রশ্ন ১. ${exam[0].question}</h3>
