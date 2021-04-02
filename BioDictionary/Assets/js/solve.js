8449b1598a877b1e450786a1dcca39743f982432
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-04-02 10:47:17 +0600

    up

---
+++ BioDictionary/Assets/js/solve.js
@@ -0,0 +1,42 @@
+var mainApp = {};
+(function() {
+    var firebase = app_firebase;
+    var uid = null;
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            if (user.isAnonymous === true) {
+                alert("This is a Premium Feature!");
+                return location.replace("/BioDictionary/index.html");
+            } else {
+                uid = user.uid;
+                fetch(scriptURL + '?q=Indivisual&uid=' + uid)
+                    .then((res) => {
+                        return res.json();
+                    }).then((loadedData) => {
+                        if (loadedData.code === 200) {
+
+                            var as = window.location.pathname.toString();
+                            const ID = as.split('/')[3] + "/" + as.split('/')[4] + "/video-" + as.split('/')[7].substring(1, 16);
+
+                            fetch('https://script.google.com/macros/s/AKfycbx2dj1PI7ROIp_8swqHiquG7ZeBriFNIMudGMvPBTy9o72F2cc07QUAJkDUAtOTxxcK/exec' + "?q=Exam&ID=" + ID)
+                                .then((res) => {
+                                    return res.json();
+                                })
+                                .then((loadedQuestions) => {
+                                    if (loadedQuestions.code === 200) {
+                                        questions = JSON.parse(loadedQuestions.Exam);
+                                        document.getElementById('solve').innerHTML = questions;
+                                    } else {
+                                        alert(loadedQuestions.code + "  " + loadedQuestions.message);
+                                        return close();
+                                    }
+                                })
+                                .catch((err) => {
+                                    console.error(err);
+                                });
+                        }
+                    })
+            }
+        }
+    });
+})()
