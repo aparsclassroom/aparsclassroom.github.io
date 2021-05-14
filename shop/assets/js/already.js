d9e76e2a86e4c8fa730dc774a56a93e7075c36c5
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-05-14 22:12:59 +0600

    vd

---
+++ shop/assets/js/already.js
@@ -0,0 +1,44 @@
+firebase.auth().onAuthStateChanged(function(e) {
+    if (e) {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var raw = JSON.stringify({
+            "product": product,
+            'uid': e.uid
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("https://asg-shop.herokuapp.com/purchase", requestOptions)
+            .then(response => {
+                return response.json()
+            })
+            .then(result => {
+                if (result.code === 200) {
+                    var data = result.data;
+                    document.getElementById('info').innerHTML = `
+                    <h3>Purchase Information</h3>
+                    Invoice : ${data.Invoice}<br>
+                    Product : ${data.ProductName}<br>
+                    Paid Amount : ${currency_amount} ৳ <br>
+                    Username : ${data.CustomerName}<br>
+                    Transaction Id : ${data.tran_id}<br>
+                    Email : ${data.value_b}<br>
+                    Phone No. ${data.value_c}<br>
+                    HSC : ${data.HSC}<br>
+                    Timestamp : ${data.Timestamp}
+                    `;
+                    console.log(result)
+                } else {
+                    location.replace("./");
+                }
+            })
+    } else {
+        location.replace("./");
+    }
+})
