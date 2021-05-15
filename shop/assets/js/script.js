54b9891c7ab365064fa12307889b615200ea8fe8
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-05-16 01:00:01 +0600

    cs

--- shop/assets/js/script.js
+++ shop/assets/js/script.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAH2zBEVee1dvhrlnJCofm4P4o-hVqECTA",
-    authDomain: "chatbot-ylwurt.firebaseapp.com",
-    databaseURL: "https://chatbot-ylwurt.firebaseio.com",
-    projectId: "chatbot-ylwurt",
-    storageBucket: "chatbot-ylwurt.appspot.com",
-    messagingSenderId: "170430657555",
-    appId: "1:170430657555:web:94e053b14b5cb41e92fbe6"
+    apiKey: "AIzaSyD4WuQA56koZ-qWV56rDXDTtczaCVnGft8",
+    authDomain: "asg-shop.firebaseapp.com",
+    projectId: "asg-shop",
+    storageBucket: "asg-shop.appspot.com",
+    messagingSenderId: "374714320984",
+    appId: "1:374714320984:web:d2d308f1ea2a9f46bbe22d",
+    measurementId: "G-P18HXDWK2Y"
 };
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
