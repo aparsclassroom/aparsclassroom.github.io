1ab624536686d5aa1db74065b90c953e874deffc
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-05-26 13:53:54 +0600

    Update dashboard.js

--- BioDictionary/affiliation/assets/dashboard.js
+++ BioDictionary/affiliation/assets/dashboard.js
@@ -41,7 +41,7 @@ function initApp() {
                                     backgroundColor: [
                                         'rgba(255, 99, 132, 0.2)',
                                         'rgba(54, 162, 235, 0.2)',
-                                        'rgba(238, 133, 171, 1)'
+                                        'rgba(238, 133, 171, 0.2)'
                                     ],
                                     borderColor: [
                                         'rgba(255, 99, 132, 1)',
