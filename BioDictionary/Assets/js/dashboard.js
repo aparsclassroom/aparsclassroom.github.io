2dea648246a57e99e6a38f56104edff7771dfa2b
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-06-02 21:17:53 +0600

    Update dashboard.js

--- BioDictionary/Assets/js/dashboard.js
+++ BioDictionary/Assets/js/dashboard.js
@@ -11,10 +11,6 @@ $(function() {
         });
     });
 }(jQuery));
-document.addEventListener("contextmenu", function(e) {
-    e.preventDefault();
-});
-
 
 window.addEventListener('load', () => {
     console.log("%cDon't YOU Ever Try To STEAL the SOURCE CODE 🤬", "color:red;Background-Color:white;padding:100px;font-size:50px");
