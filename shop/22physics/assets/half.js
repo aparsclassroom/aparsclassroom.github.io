d71c0dc22e5dca682086d701ae0964a10ca97a6c
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-06-21 03:05:42 +0600

    fix

--- shop/22physics/assets/half.js
+++ shop/22physics/assets/half.js
@@ -248,7 +248,59 @@ firebase.auth().onAuthStateChanged(function(e) {
                 }
             })
             .catch(() => {
-                console.log('data not found !')
+                const mform = document.forms['purchase']
+                mform.addEventListener('submit', em => {
+                    em.preventDefault();
+                    var mail = document.getElementById('email').value.toLowerCase().trim();
+                    document.getElementById('buy').innerText = "Please wait...."
+                    document.getElementById("buy").disabled = true;
+                    var myHeaders = new Headers();
+                    myHeaders.append("Content-Type", "application/json");
+                    var raw = JSON.stringify({
+                        "dicount_amount": disOFF,
+                        "product": product,
+                        "cus_name": document.getElementById('name').value.trim(),
+                        "email": mail,
+                        "college": document.getElementById('college').value.trim(),
+                        "hsc": document.getElementById('hscBatch').value.trim(),
+                        "phone": document.getElementById('phone').value.trim(),
+                        "aff": localStorage.getItem(product).trim(),
+                        "Cupon": document.getElementById('disC').value.trim(),
+                        'uid': e.uid
+                    });
+
+                    var requestOptions = {
+                        method: 'POST',
+                        headers: myHeaders,
+                        body: raw,
+                        redirect: 'follow'
+                    };
+
+                    fetch(`https://${shopName}.herokuapp.com/${productCode2}/init`, requestOptions)
+                        .then(response => {
+                            return response.json()
+                        })
+                        .then(result => {
+                            if (result.status != 420) {
+                                location.href = result.GatewayPageURL
+                            } else {
+                                swal({
+                                    title: result.message,
+                                    icon: "error"
+                                }).then(() => {
+                                    location.href = result.GatewayPageURL
+                                })
+                            }
+                        })
+                        .catch(() => {
+                            swal({
+                                title: "Error",
+                                icon: "error",
+                                text: "Server Busy 😶\nPlease Try Again later",
+                                button: "Ok"
+                            })
+                        });
+                })
             })
         document.getElementById('moda').setAttribute("data-target", "#purchaseFrm");
         if (t != null) {
