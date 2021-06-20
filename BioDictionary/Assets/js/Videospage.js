86352a0f4570ee2a650c0f9f13560fe373c3729f
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-06-20 17:40:42 +0600

    Update Videospage.js

--- BioDictionary/Assets/js/Videospage.js
+++ BioDictionary/Assets/js/Videospage.js
@@ -30,8 +30,11 @@ var mainApp = {};
                             document.getElementById('hed').innerText = "BioCast Episode " + epNo;
                             document.getElementById('player').setAttribute("data-plyr-embed-id", loadedData.Video_Id);
                             const player = new Plyr('#player');
+                            // const player = new Plyr('#player', {
+                            //     title: 'Example Title',
+                            // });
                             document.getElementById('forward').addEventListener('click', e => {
-                                console.log(e.detail.plyr)
+                                player.forward(10)
                             })
                             document.getElementById('previewP').setAttribute("src", "https://drive.google.com/file/d/" + loadedData.Pdf_Id + "/preview");
                             document.getElementById('down').setAttribute('onclick', "window.open('https://drive.google.com/u/0/uc?id=" + loadedData.Pdf_Id + "&export=download')");
