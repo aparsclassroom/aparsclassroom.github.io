8b7a3d6263c98a2335530ccbd403c1d1008d0cbc
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-06-23 04:54:13 +0600

    Update info.js

--- shop/22physics/assets/info.js
+++ shop/22physics/assets/info.js
@@ -3,7 +3,7 @@ firebase.auth().onAuthStateChanged(function(e) {
         var myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
         var raw = JSON.stringify({
-            "product": product
+            "uid": e.uid
         });
 
         var requestOptions = {
