a7085f590d4a025ba91b6d8ce71f8c4aba5026a1
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-07-12 20:52:37 +0600

    cd

--- shop/22physics/assets/half.js
+++ shop/22physics/assets/half.js
@@ -317,6 +317,7 @@ firebase.auth().onAuthStateChanged(function(e) {
                 })
             })
         document.getElementById('moda').setAttribute("data-target", "#purchaseFrm");
+        document.getElementById('moda').innerText = "Purchase Form";
         if (t != null) {
             document.getElementById('phone').value = t;
             document.getElementById('phone').setAttribute("readonly", true);
