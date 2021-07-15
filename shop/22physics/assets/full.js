7395c22b29c3c149663ec64a93d5e0195c3f4489
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-07-15 06:23:39 +0600

    Update full.js

--- shop/22physics/assets/full.js
+++ shop/22physics/assets/full.js
@@ -160,15 +160,15 @@ firebase.auth().onAuthStateChanged(function(e) {
                                     return response.json()
                                 })
                                 .then(result => {
-                                    if (result.status != 420) {
-                                        location.href = result
-                                    } else {
+                                    if (result.status == 420) {
                                         swal({
                                             title: result.message,
                                             icon: "error"
                                         }).then(() => {
                                             location.href = result.GatewayPageURL
                                         })
+                                    } else {
+                                        location.href = result
                                     }
                                 })
                                 .catch(() => {
@@ -219,15 +219,15 @@ firebase.auth().onAuthStateChanged(function(e) {
                                 return response.json()
                             })
                             .then(result => {
-                                if (result.status != 420) {
-                                    location.href = result
-                                } else {
+                                if (result.status == 420) {
                                     swal({
                                         title: result.message,
                                         icon: "error"
                                     }).then(() => {
                                         location.href = result.GatewayPageURL
                                     })
+                                } else {
+                                    location.href = result
                                 }
                             })
                             .catch(() => {
@@ -265,15 +265,15 @@ firebase.auth().onAuthStateChanged(function(e) {
                                 return response.json()
                             })
                             .then(result => {
-                                if (result.status != 420) {
-                                    location.href = result
-                                } else {
+                                if (result.status == 420) {
                                     swal({
                                         title: result.message,
                                         icon: "error"
                                     }).then(() => {
                                         location.href = result.GatewayPageURL
                                     })
+                                } else {
+                                    location.href = result
                                 }
                             })
                             .catch(() => {
