ace45b39385edfaf751f448d2cdd5932bdf38d08
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-07-18 02:11:21 +0600

    updated

---
+++ shop/batch-protishruti/assets/Subjects/biology/select.js
@@ -0,0 +1,47 @@
+(function($) {
+    $.fn.getOptions = function(options) {
+        var that = this;
+        var settings = $.extend({
+            url: "",
+            key: "key",
+            val: "val",
+            firstOption: {
+                key: null,
+                val: null
+            }
+        }, options);
+
+        $.getJSON(settings.url, function(resp) {
+            if (resp) {
+                that.empty();
+                if (settings.firstOption.key !== null || settings.firstOption.val !== null) {
+                    that.append(new Option(settings.firstOption.key, settings.firstOption.val));
+                }
+                $.each(resp, function(key, val) {
+                    that.append(new Option(val[settings.key], val[settings.val]));
+                });
+            }
+        });
+        return that;
+    };
+}(jQuery));
+
+const subj = "biology";
+
+$('#Chapter').getOptions({
+    url: "../../assets/Subjects/biology/chapters.json",
+    key: "ChapterName",
+    val: "Value",
+    firstOption: {
+        key: "--Select Any--",
+        val: ""
+    }
+});
+
+
+var slider = document.getElementById("duration");
+var output = document.getElementById("output");
+output.value = slider.value;
+slider.oninput = function() {
+    output.value = this.value;
+}
