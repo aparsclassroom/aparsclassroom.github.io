694ddbb948a7e9b0fe92cf0cb7d030f176c853a3
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-07-25 05:53:30 +0600

    bfbgf

--- shop/assets/js/key.js
+++ shop/assets/js/key.js
@@ -1,2 +1,3 @@
 const shopName = "asg-shop";
 const cuponApi = "https://script.google.com/macros/s/AKfycbxMaZT0KTzKL-MomGP5vdc1krnFVfnD91K7O92TwR2MjOKVXPSN_q36VcEsC0HguACX/exec";
+const courseContent = "https://script.google.com/macros/s/AKfycbwTZvoqoO94lxPlIsiTt2R9hup49gdZ6Xzsr30O18Lb54QuMQavHHbKubc8SaON9pKpgg/exec";
