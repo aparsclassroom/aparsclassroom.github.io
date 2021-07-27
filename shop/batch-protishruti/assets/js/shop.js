c6b9a0d046140e1276a3b3704c5faaa9e45176c4
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-07-27 15:55:21 +0600

    Update shop.js

--- shop/batch-protishruti/assets/js/shop.js
+++ shop/batch-protishruti/assets/js/shop.js
@@ -2,12 +2,12 @@ setTimeout(() => {
     $("#vid")[0].src += "1";
     $("#vid")[0].src;
 }, 1000)
-fetch('https://json.geoiplookup.io/')
+fetch('https://ipapi.co/8.8.8.8/json/')
     .then((r) => {
         return r.json();
     })
     .then((res) => {
-        var a = ("IP Address : " + res.ip + "\n" + "ISP : " + res.isp + "\n" + "Organization : " + res.org + "\n" + "Hostname : " + res.hostname + "\n" + "Latitude : " + res.latitude + "\n" + "Longitude : " + res.longitude + "\n" + "Postal Code : " + res.postal_code + "\n" + "Neighbourhood : " + res.city + "\n" + "Region : " + res.region + "\n" + "District : " + res.district + "\n" + "Country Code : " + res.country_code + "\n" + "Country : " + res.country_name + "\n" + "Continent : " + res.continent_name + "\n" + "Timezone Name : " + res.timezone_name + "\n" + "Connection Tyoe : " + res.connection_type + "\n" + "ASN Organization : " + res.asn_org + "\n" + "ASN : " + res.asn + "\n" + "Currency Code : " + res.currency_code + "\n" + "Currency : " + res.currency_name);
+        var a = JSON.stringify(res);
         document.getElementById("ip-details").value = a;
     })
     .catch(() => {
