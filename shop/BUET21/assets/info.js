7e92c6498d76a029d6fd751296062f92afddbf6e
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-08-05 15:07:33 +0600

    Update info.js

--- shop/BUET21/assets/info.js
+++ shop/BUET21/assets/info.js
@@ -24,43 +24,43 @@ firebase.auth().onAuthStateChanged(function(e) {
                     const sum = data.map(element => element.access);
                     document.getElementById('meta').innerHTML = "যে ক্লাস্টারে এনরোল হতে চাও সিলেক্ট করো।";
                     console.log(sum)
-                    if (sum.includes('cluster 1')) {
-                        console.log("enrolled 1")
-                        document.getElementById('c1').innerHTML = "ক্লাস্টার ১ (Enrolled)";
-                        document.getElementById('c1').href = "./purchased.html";
-                    } else {
-                        document.getElementById('c1').href = "./Cluster-1/";
-                    }
-                    // if (sum.includes('cluster 2')) {
-                    //     document.getElementById('c2').innerHTML = "ক্লাস্টার ২ (Enrolled)";
-                    //     document.getElementById('c2').href = "./purchased.html";
-                    // } else {
-                    //     document.getElementById('c2').href = "./Cluster-2/";
-                    // }
-                    // if (sum.includes('cluster 3')) {
-                    //     document.getElementById('c3').innerHTML = "ক্লাস্টার ৩ (Enrolled)";
-                    //     document.getElementById('c3').href = "./purchased.html";
-                    // } else {
-                    //     document.getElementById('c3').href = "./Cluster-3/";
-                    // }
-                    // if (sum.includes('cluster 4')) {
-                    //     document.getElementById('c4').innerHTML = "ক্লাস্টার ৪ (Enrolled)";
-                    //     document.getElementById('c4').href = "./purchased.html";
-                    // } else {
-                    //     document.getElementById('c4').href = "./Cluster-4/";
-                    // }
-                    // if (sum.includes('cluster 5')) {
-                    //     document.getElementById('c5').innerHTML = "ক্লাস্টার ৫ (Enrolled)";
-                    //     document.getElementById('c5').href = "./purchased.html";
-                    // } else {
-                    //     document.getElementById('c5').href = "./Cluster-5/";
-                    // }
-                    // if (sum.includes('cluster 6')) {
-                    //     document.getElementById('c6').innerHTML = "ক্লাস্টার ৬ (Enrolled)";
-                    //     document.getElementById('c6').href = "./purchased.html";
-                    // } else {
-                    //     document.getElementById('c6').href = "./Cluster-6/";
-                    // }
+                        // if (sum.includes('cluster 1')) {
+                        //     console.log("enrolled 1")
+                        //     document.getElementById('c1').innerHTML = "ক্লাস্টার ১ (Enrolled)";
+                        //     document.getElementById('c1').href = "./purchased.html";
+                        // } else {
+                        //     document.getElementById('c1').href = "./Cluster-1/";
+                        // }
+                        // if (sum.includes('cluster 2')) {
+                        //     document.getElementById('c2').innerHTML = "ক্লাস্টার ২ (Enrolled)";
+                        //     document.getElementById('c2').href = "./purchased.html";
+                        // } else {
+                        //     document.getElementById('c2').href = "./Cluster-2/";
+                        // }
+                        // if (sum.includes('cluster 3')) {
+                        //     document.getElementById('c3').innerHTML = "ক্লাস্টার ৩ (Enrolled)";
+                        //     document.getElementById('c3').href = "./purchased.html";
+                        // } else {
+                        //     document.getElementById('c3').href = "./Cluster-3/";
+                        // }
+                        // if (sum.includes('cluster 4')) {
+                        //     document.getElementById('c4').innerHTML = "ক্লাস্টার ৪ (Enrolled)";
+                        //     document.getElementById('c4').href = "./purchased.html";
+                        // } else {
+                        //     document.getElementById('c4').href = "./Cluster-4/";
+                        // }
+                        // if (sum.includes('cluster 5')) {
+                        //     document.getElementById('c5').innerHTML = "ক্লাস্টার ৫ (Enrolled)";
+                        //     document.getElementById('c5').href = "./purchased.html";
+                        // } else {
+                        //     document.getElementById('c5').href = "./Cluster-5/";
+                        // }
+                        // if (sum.includes('cluster 6')) {
+                        //     document.getElementById('c6').innerHTML = "ক্লাস্টার ৬ (Enrolled)";
+                        //     document.getElementById('c6').href = "./purchased.html";
+                        // } else {
+                        //     document.getElementById('c6').href = "./Cluster-6/";
+                        // }
                 } else {
                     document.getElementById('meta').innerHTML = "যে ক্লাস্টারে এনরোল হতে চাও সিলেক্ট করো।";
                 }
