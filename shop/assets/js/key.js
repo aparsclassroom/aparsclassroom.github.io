f8d7631e8b7057bf7fbc5c3bbd82a96f20d7f675
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-08-27 00:29:44 +0600

    Update key.js

--- shop/assets/js/key.js
+++ shop/assets/js/key.js
@@ -1,3 +1,3 @@
 const shopName = "asg-shop";
 const cuponApi = "https://script.google.com/macros/s/AKfycbxMaZT0KTzKL-MomGP5vdc1krnFVfnD91K7O92TwR2MjOKVXPSN_q36VcEsC0HguACX/exec";
-const courseContent = "https://script.google.com/macros/s/AKfycbzleEIxoeR8uVxaGLONZ348BskDfVab0RA0ZeWk2Ywin6f5_rVDIhcctfAidxbnYq4obQ/exec";
+const courseContent = "https://script.google.com/macros/s/AKfycbxRpfcdiGeOjVByWUiRkH_XBY5XqGC5ue-TAD1FkMqTwkvvntPoWz_NAEXDrvHeP6coFw/exec";
