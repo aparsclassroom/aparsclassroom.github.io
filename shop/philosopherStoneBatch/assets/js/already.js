e98435fd651344f7c0de9dc344345d5a50fed3c6
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-09-20 22:00:27 +0600

    Update already.js

--- shop/philosopherStoneBatch/assets/js/already.js
+++ shop/philosopherStoneBatch/assets/js/already.js
@@ -28,17 +28,17 @@ firebase.auth().onAuthStateChanged(function(e) {
                     if (data.Enrollment != "enrolled") {
                         document.getElementById('info').innerHTML = `
                         <h3>Purchase Information</h3>
-                        Invoice : ${data.Invoice}<br>
+                        Invoice : ${data.invoice}<br>
                         Product : ${data.ProductName}<br>
                         Discount Coupon : ${data.DiscountCupon}<br>
                         Paid Amount : ${data.currency_amount} ৳ <br>
-                        Username : ${data.CustomerName}<br>
+                        Username : ${data.Name}<br>
                         Password : <span id="pass">${data.tran_id}</span>&nbsp;&nbsp;&nbsp;<button id="cpBtn" class="cp btn btn-success d-print-none" data-clipboard-target="#pass">Copy Passoword</button><br>
-                        Email : ${data.value_b}<br>
-                        Phone No. ${data.value_c}<br>
+                        Email : ${data.email}<br>
+                        Phone No. ${data.phone}<br>
                         College : ${data.College}<br>
                         HSC : ${data.HSC}<br>
-                        Purchased at : ${data.Timestamp}<br><br>
+                        Purchased at : ${data.time}<br><br>
                         Secret Group Link : <br><a href="${appl}" target="_blank">${appl}</a>
                         <br><br>
                         <h3>Your Group Join request should be approved in 24-48 hours.<br> so please have patience.</h3>
@@ -47,17 +47,17 @@ firebase.auth().onAuthStateChanged(function(e) {
                     } else {
                         document.getElementById('info').innerHTML = `
                         <h3>Enrollment Information</h3>
-                        Invoice : ${data.Invoice}<br>
+                        Invoice : ${data.invoice}<br>
                         Product : ${data.ProductName}<br>
                         Discount Coupon : ${data.DiscountCupon}<br>
                         Paid Amount : ${data.currency_amount} ৳ <br>
-                        Username : ${data.CustomerName}<br>
+                        Username : ${data.Name}<br>
                         Joining ID : <span id="pass">${data.tran_id}</span><br>
-                        Email : ${data.value_b}<br>
-                        Phone No. ${data.value_c}<br>
+                        Email : ${data.email}<br>
+                        Phone No. ${data.phone}<br>
                         College : ${data.College}<br>
                         HSC : ${data.HSC}<br>
-                        Purchased at : ${data.Timestamp}<br><br>
+                        Purchased at : ${data.time}<br><br>
                         Secret Group Link : <br><a href="${appl}" target="_blank">${appl}</a><br><br>
                         Status : You are Currently joined in the Facebook Group !
                         <br><br>
