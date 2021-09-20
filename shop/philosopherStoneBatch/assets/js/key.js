f1133d103de7a1cb6d26dcd5989e4dad0f00b379
Maheyan <68837961+Maheyan@users.noreply.github.com>
2021-09-20 19:52:25 +0600

    updated

---
+++ shop/philosopherStoneBatch/assets/js/key.js
@@ -0,0 +1,3 @@
+const shopName = "asg-shop";
+const cuponApi = "https://asg-shop.herokuapp.com/Coupon";
+const courseContent = "https://script.google.com/macros/s/AKfycbz9m4Cu24_Vf9m8LYacYR8adF_d-CIL9yLZJPNjSG6TN7C20v_-Tlr6NRqM9_NpKSI0kg/exec";
---
+++ shop/philosopherStoneBatch/assets/ss.js
@@ -0,0 +1,128 @@
+const product = "ASG Philosopher's Stone Batch";
+const appl = "https://www.facebook.com/groups/aparchinmoyadmission21/";
+const WebApp = "https://buet-physics.herokuapp.com/login";
+const productCode = "129";
+const fix = 2000;
+const pls = 500;
+
+const vidD = document.getElementById('video');
+const clprc = document.getElementById('clprc');
+if (screen.width <= 600) {
+    clprc.classList.add('fixed-bottom');
+} else {
+    clprc.classList.remove('fixed-bottom');
+    vidD.style.position = 'sticky';
+}
+
+
+fetch(`${courseContent}?productCode=${productCode}`)
+    .then((res) => {
+        return res.json()
+    })
+    .then((data) => {
+        let types = data.types;
+        let guidelines = data.guidelines;
+
+        document.getElementById('con1').innerHTML = `<i class="fab fa-youtube"></i>&nbsp; ${types[0].TotalClasses} ${types[0].ContType}`;
+        document.getElementById('con2').innerHTML = `<i class="fas fa-clipboard"></i>&nbsp; ${types[1].TotalClasses} ${types[1].ContType} `;
+        document.getElementById('con3').innerHTML = `<i class="fas fa-clinic-medical"></i>&nbsp;${types[2].TotalClasses} ${types[2].ContType} `;
+        document.getElementById('con4').innerHTML = `<i class="fas fa-question-circle"></i>&nbsp;${types[3].TotalClasses} ${types[3].ContType} `;
+        document.getElementById('con5').innerHTML = `<i class="fas fa-calendar-check"></i>&nbsp;${types[4].TotalClasses} ${types[4].ContType} `;
+        document.getElementById('enrolled').innerHTML = `${types[4].TotalClasses} জন`;
+        document.getElementById('duration').innerHTML = `${types[5].TotalClasses} দিন`;
+        document.getElementById('con7').innerHTML = `<i class="fas fa-tablet-alt"></i>&nbsp; Dedicated Web App`;
+        document.getElementById('con9').innerHTML = `<i class="fab fa-facebook-square"></i></i>&nbsp; Discussion Group`;
+        document.getElementById('loading1').innerHTML = "";
+        document.getElementById('loading2').innerHTML = "";
+        document.getElementById('pra11').innerHTML = "";
+        document.getElementById('pra22').innerHTML = "";
+        document.getElementById('exloading11').innerHTML = "";
+        document.getElementById('exloading22').innerHTML = "";
+        document.getElementById('gload').innerHTML = "";
+
+        let classes = data.classes;
+
+        var as = classes.filter((all) => {
+            return all.SubAndPaper == 'Biology 1st Paper'
+        })
+
+        var as2 = classes.filter((all) => {
+            return all.SubAndPaper == 'Biology 2nd Paper'
+        })
+
+        let domC = document.getElementById('Classes1');
+        as.forEach(element => {
+            if (element.Playlist != "") {
+                domC.innerHTML += `
+                <li>
+                <a href = "${element.Playlist}" style="color:rgb(37, 173, 55);text-decoration:none;font-weight:bold;" target="blank">🗝️🔒 ${element.Chapter} - ${element.LiveClass} Classes</a>
+                </li>`
+            } else {
+                domC.innerHTML += `
+                <li class="disabled">
+                🔒 ${element.Chapter} - ${element.LiveClass} Classes
+                </li>`
+            }
+
+        });
+
+        let domC2 = document.getElementById('Classes2');
+        as2.forEach(element => {
+            if (element.Playlist != "") {
+                domC2.innerHTML += `
+                <li>
+                <a href = "${element.Playlist}" style="color:rgb(37, 173, 55);text-decoration:none;font-weight:bold;" target="blank">🗝️🔒 ${element.Chapter} - ${element.LiveClass} Classes</a>
+                </li>`
+            } else {
+                domC2.innerHTML += `
+                <li class="disabled">
+                🔒 ${element.Chapter} - ${element.LiveClass} Classes
+                </li>`
+            }
+        });
+
+        let prac1 = document.getElementById('prac1');
+        as.forEach(element => {
+            prac1.innerHTML += `
+                <li class="disabled">
+                🔒 ${element.Chapter} - ${element.PracticeSheet} Sheets
+                </li>`
+
+        });
+
+        let prac22 = document.getElementById('prac2');
+        as2.forEach(element => {
+            prac22.innerHTML += `
+                <li class="disabled">
+                🔒 ${element.Chapter} - ${element.PracticeSheet} Sheets
+                </li>`
+        });
+
+        let ex1 = document.getElementById('ex1');
+        as.forEach(element => {
+            ex1.innerHTML += `
+                <li class="disabled">
+                🔒 ${element.Chapter} - ${element.Exam} Exam(s)
+                </li>`
+
+        });
+
+        let ex22 = document.getElementById('ex2');
+        as2.forEach(element => {
+            ex22.innerHTML += `
+                <li class="disabled">
+                🔒 ${element.Chapter} - ${element.Exam} Exam(s)
+                </li>`
+        });
+        let guides = document.getElementById('guides');
+        guidelines.forEach(element => {
+            guides.innerHTML += `
+                <li class="disabled">
+                🔒 ${element.sessionName} - ${element.Month}
+                </li>`
+        });
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
