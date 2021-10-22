bb71dad250836239c52b0c4e79e7ed4e500f4021
Maheyan <aparsclassroom@gmail.com>
2021-10-22 20:52:34 +0600

    updated

--- shop/mahir-sir/assets/js/key.js
+++ shop/mahir-sir/assets/js/key.js
@@ -1,3 +1,3 @@
-const shopName = "asg-shop.herokuapp.com";
-const cuponApi = "https://asg-shop.herokuapp.com/Coupon";
+const shopName = "secure.apars.shop";
+const cuponApi = "https://secure.apars.shop/Coupon";
 const courseContent = "https://script.google.com/macros/s/AKfycbxyDXzFTgLyhFk1AUtMSq1yb6Pecl-H9AVz-nxQ8piwuAmqo_KQ76513i5AZYbL5M0WcQ/exec";
