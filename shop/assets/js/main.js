9e36161558b14b0dfdf99ec6baed2e2343d99809
Maheyan <aparsclassroom@gmail.com>
2021-11-10 02:04:26 +0600

    Update main.js

--- shop/assets/js/main.js
+++ shop/assets/js/main.js
@@ -257,5 +257,5 @@ function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = cname + "=" + cvalue + ";domain=.aparsclassroom.com" + expires + ";path=/";
 }
