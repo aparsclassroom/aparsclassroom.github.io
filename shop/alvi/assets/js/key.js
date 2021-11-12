f59ad5651e4c7b8ba3f27163db5f9436edf1adcf
Maheyan <aparsclassroom@gmail.com>
2021-11-13 04:16:25 +0600

    up

---
+++ shop/alvi/assets/js/key.js
@@ -0,0 +1,3 @@
+const shopName = "secure.apars.shop";
+const cuponApi = "https://secure.apars.shop/Coupon";
+const courseContent = "https://script.google.com/macros/s/AKfycbxVqYksx0QJvguYMeo3vIANsW7wez5-iAghdhmCXBMmlcrJXRFDQSLV8RKm0MdUD1f1/exec";
