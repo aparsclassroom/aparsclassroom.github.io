f28839d2381bfd30c6577f060884eee1dc09a8a5
Maheyan <aparsclassroom@gmail.com>
2021-11-14 15:19:01 +0600

    new

--- shop/shahriar/assets/js/key.js
+++ shop/shahriar/assets/js/key.js
@@ -1,3 +1,3 @@
 const shopName = "secure.apars.shop";
 const cuponApi = "https://secure.apars.shop/Coupon";
-const courseContent = "https://script.google.com/macros/s/AKfycbyr0EX8ms4wu4uE-z9CzHXJOrrrTaXxQSG7XDMxjGHnRxXsILSWQVkI0ARZcMwx_LxW/exec";
+const courseContent = "https://script.google.com/macros/s/AKfycbzJQOTXS_K-Ia9J74lTAqhMxmfgOn55GeLtekMj53nrmjHqJKCvNcCJmvbpCcemEicY/exec";
