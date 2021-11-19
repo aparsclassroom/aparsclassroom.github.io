81d0213f1fb2819546584b06a8375fa4c26a6ee3
Maheyan <aparsclassroom@gmail.com>
2021-11-19 12:57:32 +0600

    ok

---
+++ shop/sakib/Varsity_+_Medi_Biology_Special_Private_Program-2021/assets/info.js
@@ -0,0 +1,5 @@
+const product = "Varsity + Medi Biology Special Private Program-2021";
+const productCode = "142";
+const fix = 3999;
+const pls = 2000;
+const appl = "https://www.facebook.com/groups/varsity.medibiology.2021";
