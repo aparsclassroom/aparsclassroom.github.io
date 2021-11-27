dd21cdcd3e44909cfa4fcbf4097d0c920ebc1bea
Maheyan <aparsclassroom@gmail.com>
2021-11-28 00:20:10 +0600

    Update sript.js

--- shop/philosopherStoneBatch/assets/sript.js
+++ shop/philosopherStoneBatch/assets/sript.js
@@ -224,9 +224,8 @@ cpn.addEventListener('click', (e) => {
     cupV.disabled = true;
     cpn.disabled = true;
     fetch(cuponApi + '/' + cpnCode.toUpperCase() + '/' + product, {
-            credentials: "include",
+            credentials: "same-site",
             method: 'GET',
-            mode: 'cors',
         })
         .then((res) => {
             return res.json();
