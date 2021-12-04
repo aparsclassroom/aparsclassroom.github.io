cf7f6c4de9dc70f97c942fb6f4ef06aa7e38756f
Maheyan <aparsclassroom@gmail.com>
2021-12-04 20:27:26 +0600

    bcgb

---
+++ shop/mahir-sir/MMV_2nd_Paper_Short_B22/assets/info.js
@@ -0,0 +1,5 @@
+const product = "MATH 2nd Paper Short Syllabus (22)";
+const productCode = "144";
+const fix = 6000;
+const pls = 1500;
+const appl = "https://www.facebook.com/groups/169092541444666";
