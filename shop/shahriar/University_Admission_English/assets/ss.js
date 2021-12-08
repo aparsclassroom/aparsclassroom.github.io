385de47505f292adbc5f4b756a36070181de66ae
Maheyan <aparsclassroom@gmail.com>
2021-12-09 03:53:54 +0600

    গত

---
+++ shop/shahriar/University_Admission_English/assets/ss.js
@@ -0,0 +1,104 @@
+const product = "University Admission English (A+B+C+D Unit)";
+const productCode = "145";
+const fix = 3100;
+const pls = 2100;
+const appl = "https://www.facebook.com/groups/287167813345651";
+const vidD = document.getElementById('video');
+const clprc = document.getElementById('clprc');
+if (screen.width <= 600) {
+    clprc.classList.add('fixed-bottom');
+} else {
+    clprc.classList.remove('fixed-bottom');
+    vidD.style.position = 'sticky';
+}
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: '9ehCFVi0KZo',
+        playerVars: { 'autoplay': 1, 'playsinline': 1 },
+        events: {
+            'onReady': onPlayerReady
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    document.getElementById('thumb').style.display = "none";
+    event.target.setVolume(100);
+    event.target.playVideo();
+}
+
+fetch(`${courseContent}?productCode=${productCode}`)
+    .then((res) => {
+        return res.json()
+    })
+    .then((data) => {
+        let types = data.types;
+
+        document.getElementById('con1').innerHTML = `<i class="fab fa-youtube"></i>&nbsp; ${types[0].TotalClasses} ${types[0].ContType}`;
+        document.getElementById('con2').innerHTML = `<i class="fas fa-clipboard"></i>&nbsp; ${types[1].TotalClasses} ${types[1].ContType} `;
+        document.getElementById('con3').innerHTML = `<i class="fas fa-calendar-check"></i>&nbsp;${types[2].TotalClasses} ${types[2].ContType} `;
+        document.getElementById('enrolled').setAttribute('countTo', types[3].TotalClasses);
+        if (document.getElementById('enrolled')) {
+            const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
+            if (!countUp.error) {
+                countUp.start();
+            } else {
+                console.error(countUp.error);
+            }
+        }
+
+        document.getElementById('duration').innerHTML = `${types[4].TotalClasses} মাস`;
+        document.getElementById('con7').innerHTML = `<i class="fas fa-tablet-alt"></i>&nbsp; Dedicated Webapp`;
+        document.getElementById('con9').innerHTML = `<i class="fab fa-facebook-square"></i></i>&nbsp; Discussion Group`;
+        document.getElementById('loading2').innerHTML = "";
+        document.getElementById('pra22').innerHTML = "";
+        document.getElementById('exloading22').innerHTML = "";
+
+        let classes = data.classes;
+        var as2 = classes.filter((all) => {
+            return all.SubAndPaper == 'English (2nd Paper)'
+        })
+
+        let domC2 = document.getElementById('Classes2');
+        as2.forEach(element => {
+            if (element.Playlist != "") {
+                domC2.innerHTML += `
+                <li>
+                <a href = "${element.Playlist}" style="color:rgb(37, 173, 55);text-decoration:none;font-weight:bold;" target="blank">🗝️🔒 ${element.Chapter} - ${element.LiveClass} Class(es)</a>
+                </li>`
+            } else {
+                domC2.innerHTML += `
+                <li class="disabled">
+                🔒 ${element.Chapter} - ${element.LiveClass} Class(es)
+                </li>`
+            }
+        });
+
+        let prac22 = document.getElementById('prac2');
+        as2.forEach(element => {
+            prac22.innerHTML += `
+                <li class="disabled">
+                🔒 ${element.Chapter} - ${element.PracticeSheet} Sheet(s)
+                </li>`
+        });
+        let ex22 = document.getElementById('ex2');
+        as2.forEach(element => {
+            ex22.innerHTML += `
+                <li class="disabled">
+                🔒 ${element.Chapter} - ${element.Exam} Exam(s)
+                </li>`
+        });
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
--- shop/shahriar/assets/js/key.js
+++ shop/shahriar/assets/js/key.js
@@ -1,3 +1,3 @@
 const shopName = "shop.aparsclassroom.com";
 const cuponApi = "https://shop.aparsclassroom.com/Coupon";
-const courseContent = "https://script.google.com/macros/s/AKfycbyuFm-fHIuEKMEfra9Qtb608gYJ6ErN64ELM90NXxmBfAT5BjLZqac6bE18kI2KXAvz/exec";
+const courseContent = "https://script.google.com/macros/s/AKfycbzVHX4SVUME3TkoE2092DjBQGDjuhGT4HsOS8KcrHQLhPc244n54M6rxOnIQ4Izzmpx/exec";
