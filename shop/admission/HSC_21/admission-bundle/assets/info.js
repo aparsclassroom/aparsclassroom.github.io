d0fb02a72056d103c2ad38962534ba5c536b3820
Maheyan <aparsclassroom@gmail.com>
2021-12-10 06:23:57 +0600

    ok

--- shop/admission/HSC_21/admission-bundle/assets/info.js
+++ shop/admission/HSC_21/admission-bundle/assets/info.js
@@ -1,5 +1,5 @@
 const product = "Engineering Admission Course 2021 (Bundle)";
 const productCode = "148";
-const fix = 6600;
-const pls = 5000;
+const fix = 12200;
+const pls = 6600;
 const appl = "https://www.facebook.com/groups/engineering.admission2021";
