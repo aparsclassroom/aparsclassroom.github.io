25eb56740301fcbbcdf60956889a285d56930880
Maheyan <aparsclassroom@gmail.com>
2021-12-14 16:38:11 +0600

    ok

--- shop/admission/HSC_21/Chemistry/assets/info.js
+++ shop/admission/HSC_21/Chemistry/assets/info.js
@@ -1,5 +1,5 @@
 const product = "Apar Chinmoy Engineering Chemistry Course";
 const productCode = "139";
 const fix = 3999;
-const pls = 2000;
+const pls = 1500;
 const appl = "https://www.facebook.com/groups/aparchinmoy.chemistry21";
