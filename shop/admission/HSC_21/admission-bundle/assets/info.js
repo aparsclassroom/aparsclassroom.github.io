fe09c79a43415b386c15cf45ff1877065e950077
Maheyan <aparsclassroom@gmail.com>
2021-12-20 06:36:43 +0600

    ok

--- shop/admission/HSC_21/admission-bundle/assets/info.js
+++ shop/admission/HSC_21/admission-bundle/assets/info.js
@@ -1,5 +1,5 @@
 const product = "Apar-Chinmoy Engineering Admission Course + Hasnat's Medi Biology";
 const productCode = "148";
 const fix = 12200;
-const pls = 5000;
+const pls = 6600;
 const appl = "https://www.facebook.com/groups/engineering.admission2021";
