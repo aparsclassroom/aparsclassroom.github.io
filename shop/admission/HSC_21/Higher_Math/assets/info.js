354a1faea0ee3be0f1e720b85ac94dcb7ba1dd6a
Maheyan <aparsclassroom@gmail.com>
2021-12-22 23:42:49 +0600

    ok

--- shop/admission/HSC_21/Higher_Math/assets/info.js
+++ shop/admission/HSC_21/Higher_Math/assets/info.js
@@ -1,5 +1,5 @@
-const product = "Engineering Admission Math Course";
+const product = "Apar-Chinmoy Engineering Admission Math Course";
 const productCode = "140";
 const fix = 3999;
-const pls = 2000;
+const pls = 1250;
 const appl = "https://www.facebook.com/groups/aparchinmoy.math21";
--- shop/sakib/Varsity_+_Medi_Biology_Special_Private_Program-2021/assets/info.js
+++ shop/sakib/Varsity_+_Medi_Biology_Special_Private_Program-2021/assets/info.js
@@ -1,5 +1,5 @@
 const product = "Varsity + Medi Biology Special Private Program-2021";
 const productCode = "142";
 const fix = 9000;
-const pls = 4500;
+const pls = 2500;
 const appl = "https://www.facebook.com/groups/varsity.medibiology.2021";
