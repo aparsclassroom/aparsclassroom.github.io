990b7a7744dc122c192c72cb8417b0ceeb047463
Maheyan <aparsclassroom@gmail.com>
2021-12-24 09:00:56 +0600

    ok

---
+++ shop/admission/HSC_21/engineering-bundle/assets/info.js
@@ -0,0 +1,5 @@
+const product = "Apar-Chinmoy Engineering Admission Course 2021";
+const productCode = "151";
+const fix = 10000;
+const pls = 4000;
+const appl = "https://link.aparsclassroom.com/Enginnering-Discussion";
