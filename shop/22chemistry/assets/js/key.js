154ad741744448d40e38dddb8e41ccdbabd79a98
Maheyan <aparsclassroom@gmail.com>
2022-02-02 15:46:43 +0600

    ok

--- shop/22chemistry/assets/js/key.js
+++
@@ -1,3 +0,0 @@
-const shopName = "shop.aparsclassroom.com";
-const cuponApi = "https://shop.aparsclassroom.com/Coupon";
-const courseContent = "https://script.google.com/macros/s/AKfycbyWIcr4RhTkcP0ie_T43M4aDlk1R4-EwntmPc8b2MWNyADIJMnkvMsgG7O8TvdsZ3GO/exec";
