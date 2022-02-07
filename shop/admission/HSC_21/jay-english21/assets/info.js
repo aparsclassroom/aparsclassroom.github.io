c59fbd709dc24a4bf9d290615b2413dc8dee8fa2
Maheyan <aparsclassroom@gmail.com>
2022-02-07 23:36:19 +0600

    ok

--- shop/admission/HSC_21/jay-english21/assets/info.js
+++ shop/admission/HSC_21/jay-english21/assets/info.js
@@ -2,4 +2,4 @@ const product = "Engineering English 2022";
 const productCode = "152";
 const fix = 1500;
 const pls = 750;
-const appl = "https://www.facebook.com/groups/aparchinmoy.math21";
+const appl = "http://jay-english21.aparsclassroom.com/login";
