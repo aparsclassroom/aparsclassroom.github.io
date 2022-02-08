c29b55423ef785372fd8a71e2fdd6b74af6ee3a1
Maheyan <aparsclassroom@gmail.com>
2022-02-08 12:24:03 +0600

    Update info.js

--- shop/admission/HSC_21/jay-english21/assets/info.js
+++ shop/admission/HSC_21/jay-english21/assets/info.js
@@ -2,4 +2,4 @@ const product = "Engineering English 2022";
 const productCode = "152";
 const fix = 1500;
 const pls = 750;
-const appl = "http://jay-english21.aparsclassroom.com/login";
+const appl = "https://jay-english21.aparsclassroom.com/login";
