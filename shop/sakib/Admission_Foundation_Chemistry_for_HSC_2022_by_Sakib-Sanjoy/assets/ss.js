795d2d8cbf2b903560c2509f68e535c1adcc104c
Maheyan <aparsclassroom@gmail.com>
2022-02-27 15:18:04 +0600

    ok

--- shop/sakib/Admission_Foundation_Chemistry_for_HSC_2022_by_Sakib-Sanjoy/assets/ss.js
+++ shop/sakib/Admission_Foundation_Chemistry_for_HSC_2022_by_Sakib-Sanjoy/assets/ss.js
@@ -32,17 +32,17 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'ZL9dVJhWzcI',
-        playerVars: { 'autoplay': 1, 'playsinline': 1 },
-        events: {
-            'onReady': onPlayerReady
-        }
-    });
-}
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         height: '390',
+//         width: '640',
+//         videoId: '',
+//         playerVars: { 'autoplay': 1, 'playsinline': 1 },
+//         events: {
+//             'onReady': onPlayerReady
+//         }
+//     });
+// }
 
 function onPlayerReady(event) {
     document.getElementById('thumb').style.display = "none";
