b6f69d9c91526a9dfc93f34ef6e74af5934c5caf
Maheyan <aparsclassroom@gmail.com>
2022-03-09 22:24:21 +0600

    ok

---
+++ shop/sakib/Chemistry_Series_Zero_to_Pro_by_Nazmus_Sakib/assets/ss.js
@@ -0,0 +1,4 @@
+const product = "Chemistry Series Zero to Pro by Nazmus Sakib";
+const appl = "https://www.facebook.com/groups/cycle2.chem23sakibsanjoy";
+const WebApp = "https://hsc23.beginner2master.com/login";
+const productCode = "156";
