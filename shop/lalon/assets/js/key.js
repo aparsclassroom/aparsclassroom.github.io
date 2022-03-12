6ab4618acf9061962a0ac780c1f6ab5addfab036
Maheyan <aparsclassroom@gmail.com>
2022-03-12 21:24:24 +0600

    ok

---
+++ shop/lalon/assets/js/key.js
@@ -0,0 +1,2 @@
+const shopName = "shop.aparsclassroom.com";
+const cuponApi = "https://shop.aparsclassroom.com/Coupon";
---
+++ shop/lalon/oporajito_22/assets/info.js
@@ -0,0 +1,5 @@
+const product = "Complete English Batch (অপরাজিত ২২❤️)";
+const productCode = "157";
+const fix = 2500;
+const pls = 1250;
+const appl = "https://apars.shop/157";
