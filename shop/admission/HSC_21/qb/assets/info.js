ec3759dbdba38f73bdf4080f6ed3b9bfe6127382
Maheyan <aparsclassroom@gmail.com>
2022-04-06 14:24:01 +0600

    Update info.js

--- shop/admission/HSC_21/qb/assets/info.js
+++ shop/admission/HSC_21/qb/assets/info.js
@@ -1,5 +1,5 @@
 const product = "QB Solve 22";
-const productName = "অপার'স ফিজিক্স ইউনিভার্সিটি & ইঞ্জিনিয়ারিং প্রশ্নব্যাংক সল্ভ কোর্স";
+const productName = "ফিজিক্স ইউনিভার্সিটি & ইঞ্জিনিয়ারিং প্রশ্নব্যাংক সল্ভ কোর্স";
 const productCode = "160";
 const fix = 500;
 const pls = 149;
