668b5be487638f066299ba9e8726c928b96af67b
Maheyan <aparsclassroom@gmail.com>
2022-04-16 14:32:15 +0600

    new

--- shop/jay/HSC_2023_English_with_SH_Jay copy/assets/info.js
+++ shop/jay/HSC_2023_English_with_SH_Jay copy/assets/info.js
@@ -1,5 +1,5 @@
-const product = "HSC 2022 English with SH Jay";
-const productCode = "158";
+const product = "HSC 2023 English with SH Jay";
+const productCode = "162";
 const fix = 1500;
 const pls = 950;
-const appl = "https://apars.shop/158";
+const appl = "https://apars.shop/162";
