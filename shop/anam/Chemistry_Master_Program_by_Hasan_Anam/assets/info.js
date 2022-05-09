510d494001ba2aa459b1dd2c7b61b292419b7b3a
Maheyan <aparsclassroom@gmail.com>
2022-05-09 15:50:42 +0600

    ok

--- shop/anam/Chemistry_Master_Program_by_Hasan_Anam/assets/info.js
+++ shop/anam/Chemistry_Master_Program_by_Hasan_Anam/assets/info.js
@@ -1,5 +1,5 @@
 const product = "Chemistry Master Program by Hasan Anam";
 const productCode = "154";
 const fix = 2500;
-const pls = 1250;
+const pls = 1550;
 const appl = "https://apars.shop/154";
