c195570efb780303d4319da06250c5108755e950
Maheyan <aparsclassroom@gmail.com>
2022-05-21 20:03:06 +0600

    ok

--- shop/sakib/DU_Exam_Batch_21/assets/info.js
+++ shop/sakib/DU_Exam_Batch_21/assets/info.js
@@ -1,5 +1,5 @@
 const product = "DU Exam Batch 21";
 const productCode = "161";
 const fix = 1250;
-const pls = 750;
+const pls = 500;
 const appl = "https://www.facebook.com/groups/3187624974887701";
