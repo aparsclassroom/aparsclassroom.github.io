6fa7fff1583907c2530f4ba38a7b24cf38c22360
Maheyan <aparsclassroom@gmail.com>
2022-06-06 02:23:42 +0600

    ok

---
+++ shop/sajan/HSC22_ICU_Batch_Target_200_200/assets/info.js
@@ -0,0 +1,6 @@
+const product = "sajan HSC22";
+const productName = "HSC22 ICU Batch Target 200/200";
+const productCode = "171";
+const fix = 6000;
+const pls = 1200;
+const appl = "https://apars.shop/171";
