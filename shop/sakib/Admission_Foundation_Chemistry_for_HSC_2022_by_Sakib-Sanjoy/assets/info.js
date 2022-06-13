a0d8d62dadacd02d9adee0a299a6e613c1598c1e
Maheyan <aparsclassroom@gmail.com>
2022-06-13 18:21:01 +0600

    ok

--- shop/sakib/Admission_Foundation_Chemistry_for_HSC_2022_by_Sakib-Sanjoy/assets/info.js
+++ shop/sakib/Admission_Foundation_Chemistry_for_HSC_2022_by_Sakib-Sanjoy/assets/info.js
@@ -1,4 +1,5 @@
 const product = "Admission Foundation Chemistry for HSC 2022 by Sakib-Sanjoy";
+const productName = "Admission Foundation Chemistry for HSC 2022 by Sakib-Sanjoy";
 const productCode = "153";
 const fix = 2999;
 const pls = 1800;
--- shop/sakib/Varsity_+_Medi_Biology_Special_Private_Program-2021/assets/info.js
+++ shop/sakib/Varsity_+_Medi_Biology_Special_Private_Program-2021/assets/info.js
@@ -1,4 +1,5 @@
 const product = "Varsity + Medi Biology Special Private Program-2021";
+const productName = "Varsity + Medi Biology Special Private Program-2021";
 const productCode = "142";
 const fix = 9000;
 const pls = 3000;
