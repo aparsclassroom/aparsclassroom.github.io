e73d4fb93e5d0e83958233a8f4adc455d64c8247
Maheyan <aparsclassroom@gmail.com>
2022-06-25 15:58:57 +0600

    ok

--- shop/admission/HSC_21/qb/assets/info.js
+++
@@ -1,6 +0,0 @@
-const product = "QB Solve 22";
-const productName = "ফিজিক্স ইউনিভার্সিটি & ইঞ্জিনিয়ারিং প্রশ্নব্যাংক সল্ভ কোর্স";
-const productCode = "160";
-const fix = 500;
-const pls = 149;
-const appl = "https://www.facebook.com/groups/462248365637979";
