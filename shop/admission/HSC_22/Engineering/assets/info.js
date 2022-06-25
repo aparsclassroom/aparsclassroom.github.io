ffd176331f550d7969f2cd697a1bb08bee9a45d8
Maheyan <aparsclassroom@gmail.com>
2022-06-26 02:54:44 +0600

    updated

--- shop/admission/HSC_22/Engineering/assets/info.js
+++ shop/admission/HSC_22/Engineering/assets/info.js
@@ -1,6 +1,6 @@
 const product = "Engineering22";
 const productname = "Engineering Admission Private Batch 2022";
 const productCode = "173";
-const fix = 15000;
+const fix = 4000;
 const pls = 3000;
 const appl = "www.facebook.com/groups/engineering2022/";
