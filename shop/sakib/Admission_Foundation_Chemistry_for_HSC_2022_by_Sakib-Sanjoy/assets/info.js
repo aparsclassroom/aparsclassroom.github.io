6392cd2f63c2ad77889d4f8014b9bb4aa021687b
Maheyan <aparsclassroom@gmail.com>
2022-07-16 05:23:13 +0600

    ok

--- shop/sakib/Admission_Foundation_Chemistry_for_HSC_2022_by_Sakib-Sanjoy/assets/info.js
+++ shop/sakib/Admission_Foundation_Chemistry_for_HSC_2022_by_Sakib-Sanjoy/assets/info.js
@@ -3,4 +3,4 @@ const productName = "Admission Foundation Chemistry for HSC 2022 by Sakib-Sanjoy
 const productCode = "153";
 const fix = 2999;
 const pls = 1800;
-const appl = "https://www.facebook.com/groups/976767582980015/";
+const Platform = "Sakib";
