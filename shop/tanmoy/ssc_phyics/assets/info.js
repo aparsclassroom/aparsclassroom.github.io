a2ac64f73019df6ec3d92c2d352a5f0245411259
Maheyan <aparsclassroom@gmail.com>
2022-07-16 03:37:44 +0600

    ok

--- shop/tanmoy/ssc_phyics/assets/info.js
+++ shop/tanmoy/ssc_phyics/assets/info.js
@@ -1,6 +1,5 @@
 const product = "tanmoy SSC";
-const prName = "SSC Physics Basic to Pro Master Course";
+const productName = "SSC Physics Basic to Pro Master Course";
 const productCode = "166";
 const fix = 10000;
 const pls = 1500;
-const appl = "https://apars.shop/166";
