89abf0ddd55a73ec0654c66d160c9ed375fd6d0f
Maheyan <aparsclassroom@gmail.com>
2022-07-16 07:02:41 +0600

    ok

--- shop/admission/HSC_22/Medical/assets/info.js
+++ shop/admission/HSC_22/Medical/assets/info.js
@@ -1,5 +1,5 @@
 const product = "Medical 22";
-const productname = "মেডিকেল প্রস্তুতি প্রাইভেট ব্যাচ HSC 2022 (ফুল সিলেবাস)";
+const productName = "মেডিকেল প্রস্তুতি প্রাইভেট ব্যাচ HSC 2022 (ফুল সিলেবাস)";
 const productCode = "159";
 const fix = 4000;
 const pls = 3000;
