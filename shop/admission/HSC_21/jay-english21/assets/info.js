74274a9a46f83335a9b24f623ab366dac81e4048
Maheyan <aparsclassroom@gmail.com>
2022-07-19 00:36:07 +0600

    ok

--- shop/admission/HSC_21/jay-english21/assets/info.js
+++ shop/admission/HSC_21/jay-english21/assets/info.js
@@ -1,7 +1,7 @@
 const product = "Engineering English 2022";
 const productName = "Engineering English 2022";
 const productCode = "152";
-const fix = 1500;
-const pls = 750;
+const fix = 2000;
+const pls = 1500;
 const Platform = "Jay";
 const init = 1100;
