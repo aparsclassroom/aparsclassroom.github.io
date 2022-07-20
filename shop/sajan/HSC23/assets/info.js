89009031a83af96981d3e010430b81821328d3fb
Maheyan <aparsclassroom@gmail.com>
2022-07-20 17:14:05 +0600

    ok

--- shop/sajan/HSC23/assets/info.js
+++
@@ -1,7 +0,0 @@
-const product = "sajan HSC23";
-const productName = "HSC 23 Math 1st Paper Basic 2 Engineering";
-const productCode = "179";
-const fix = 6000;
-const pls = 2500;
-const Platform = "Sajan";
-const init = 2;
