3a3cf9720e8e83e3a5ad85eca60d59b47ac409b0
Maheyan <aparsclassroom@gmail.com>
2022-07-28 21:05:02 +0600

    ok

---
+++ shop/hasnat/HSC_Biology_Board_CQ_Solve_Course/assets/info.js
@@ -0,0 +1,7 @@
+const product = "hbio22CQ";
+const productName = "HSC Biology Board CQ Solve Course";
+const productCode = "192";
+const fix = 1000;
+const pls = 750;
+const Platform = "Hasnat";
+const init = 0;
