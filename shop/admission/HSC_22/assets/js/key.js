97c2536106a2d97dfe51f330e9a4a414f9f5887e
Maheyan <aparsclassroom@gmail.com>
2022-07-29 19:07:42 +0600

    ok

--- shop/admission/HSC_22/assets/js/key.js
+++ shop/admission/HSC_22/assets/js/key.js
@@ -1,4 +1,4 @@
 const shopName = "shop.aparsclassroom.com";
 const shopName2 = "shop.aparsclassroom.com";
 const cuponApi = "https://shop.aparsclassroom.com/Coupon";
-const cuponApi2 = "https://shop.aparsclassroom.com/v1/Coupon/check/";
+const cuponApi2 = "https://shop.aparsclassroom.com/v1/Coupon/check";
