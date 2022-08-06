16222dff436ba0a21c935bffae255825c2447727
Maheyan <aparsclassroom@gmail.com>
2022-08-06 15:29:04 +0600

    pl

---
+++ shop/emran/ICT_HSC_23/assets/info.js
@@ -0,0 +1,7 @@
+const product = "emran ICT23";
+const productName = "HSC 23 Beginner to Master Course by Emran Mostofa";
+const productCode = "193";
+const fix = 3050;
+const pls = 1250;
+const Platform = "Emran";
+const init = 0;
