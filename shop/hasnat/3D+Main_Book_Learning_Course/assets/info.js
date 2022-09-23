b97d561df04f150f438d9b9177c688158ca3a88b
Apars Ltd <aparsclassroom@gmail.com>
2022-09-23 14:08:39 +0600

    updated

--- shop/hasnat/3D+Main_Book_Learning_Course/assets/info.js
+++ shop/hasnat/3D+Main_Book_Learning_Course/assets/info.js
@@ -1,7 +1,7 @@
 const product = "hbio23_3D";
 const productName = "HSC Biology 3D + Main book Learning";
 const productCode = "192";
-const fix = 1000;
-const pls = 800;
+const fix = 2000;
+const pls = 1200;
 const Platform = "Hasnat";
 const init = 0;
