24507e91c94c46df92588e44f171c91d762bfd77
Apars Ltd <aparsclassroom@gmail.com>
2022-10-11 20:30:25 +0600

    ok

---
+++ shop/competitive-programming/assets/info.js
@@ -0,0 +1,7 @@
+const product = "CP101";
+const productName = "Competitive Programming 101";
+const productCode = "196";
+const fix = 10000;
+const pls = 5000;
+const Platform = "Super Admin";
+const init = 0;
---
+++ shop/competitive-programming/course/assets/course.js
@@ -0,0 +1,6 @@
+const allClasses = document.getElementById("classes");
+
+allClasses.innerHTML += ``;
+
+
+fetch(`https://shop.aparsclassroom.com/access/${uid}`)
