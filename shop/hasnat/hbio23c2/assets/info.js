ba28dad9b3f667e0c46f1a46dbea291219949220
Apars Ltd <aparsclassroom@gmail.com>
2022-11-12 18:16:06 +0600

    ok

---
+++ shop/hasnat/hbio23c2/assets/info.js
@@ -0,0 +1,7 @@
+const product = "hbio23c2";
+const productName = "HSC 2023 Botany Course (Chapter 2,4,7,8,11)";
+const productCode = "201";
+const fix = 2500;
+const pls = 1500;
+const Platform = "Hasnat";
+const init = 0;
