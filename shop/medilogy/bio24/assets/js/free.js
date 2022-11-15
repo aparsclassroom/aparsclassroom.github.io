f8d9e5e9c3174beba71e25b4b70d557bdf426eee
Apars Ltd <aparsclassroom@gmail.com>
2022-11-15 16:23:17 +0600

    ok

--- shop/medilogy/bio24/assets/js/free.js
+++ shop/medilogy/bio24/assets/js/free.js
@@ -4,7 +4,9 @@ document.getElementById('prod').innerText = productName;
 firebase.auth().onAuthStateChanged(function(e) {
     if (e) {
         document.getElementById('prod').innerHTML = `${productName}<br>(${Cycle})`;
-        document.getElementById('moda').href = 'https://bio24.aparsclassroom.com/'
+        document.getElementById('moda').addEventListener('click', () => {
+            location.href =  'https://bio24.aparsclassroom.com/'
+        })
     } else {
             document.getElementById('moda').addEventListener('click', () => {
                 location.href = "/shop/dashboard/login?signInSuccessUrl=" + encodeURIComponent(location.href)
