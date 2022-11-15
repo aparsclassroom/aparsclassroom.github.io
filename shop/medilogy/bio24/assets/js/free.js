21d3d3dc85e1b1e3e81758406427724fd8192762
Apars Ltd <aparsclassroom@gmail.com>
2022-11-15 16:07:01 +0600

    ok

---
+++ shop/medilogy/bio24/assets/js/free.js
@@ -0,0 +1,13 @@
+document.title = productName + " | ASG Shop";
+document.getElementById('prod').innerText = productName;
+
+firebase.auth().onAuthStateChanged(function(e) {
+    if (e) {
+        document.getElementById('prod').innerHTML = `${productName}<br>(${Cycle})`;
+        document.getElementById('moda').href = 'https://bio24.aparsclassroom.com/'
+    } else {
+            document.getElementById('moda').addEventListener('click', () => {
+                location.href = "/shop/dashboard/login?signInSuccessUrl=" + encodeURIComponent(location.href)
+            })
+    }
+})
