af222042fc534829125789ec87b72a774a12c271
Apars Ltd <aparsclassroom@gmail.com>
2022-11-30 03:58:25 +0600

    ok

--- shop/at-tadrees/quran-shikkha/assets/overview.js
+++ shop/at-tadrees/quran-shikkha/assets/overview.js
@@ -1,21 +1,21 @@
-firebase.auth().onAuthStateChanged(function(e) {
-    if (e) {
-        fetch(`https://${shopName2}/access/${productCode}/${e.uid}`)
-            .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                if (data.status == "success") {
-                   document.getElementById('coursevideo').src = data.course
-                } else {
-                  //  location.replace(`https://${shopName2}/${productCode}`);
-                }
-            })
-            .catch(err => {
-                console.log(err);
-            }
-        );
-    } else {
-       location.replace(`/shop/dashboard/login`);
-    }
-});
+// firebase.auth().onAuthStateChanged(function(e) {
+//     if (e) {
+//         fetch(`https://${shopName2}/access/${productCode}/${e.uid}`)
+//             .then(response => {
+//                 return response.json()
+//             })
+//             .then(data => {
+//                 if (data.status == "success") {
+//                    document.getElementById('coursevideo').src = data.course
+//                 } else {
+//                   //  location.replace(`https://${shopName2}/${productCode}`);
+//                 }
+//             })
+//             .catch(err => {
+//                 console.log(err);
+//             }
+//         );
+//     } else {
+//        location.replace(`/shop/dashboard/login`);
+//     }
+// });
