0d1e7564ec14750c5f190f18a8cdff92b3ee3108
Apars Ltd <aparsclassroom@gmail.com>
2022-12-02 05:26:07 +0600

    ok

--- shop/at-tadrees/quran-shikkha/assets/overview.js
+++ shop/at-tadrees/quran-shikkha/assets/overview.js
@@ -13,50 +13,26 @@ firebase.auth().onAuthStateChanged(function (e) {
                     content.forEach(element => {
                         if (element.type == "yt") {
                             document.getElementById("contents").innerHTML += `
-                            <div class="card bg-primary shadow-soft border-light p-4">
-                            <div class="row align-items-center">
-                                <aside class="col-md-3">
-                                    <a href="./yt?${element._id}">
-                                        <img src="${element.image}" style="border-radius: 15px;" alt="${element.title}">
-                                    </a>
-                                </aside>
-                                <div class="col-md-6">
-                                    <div class="info-main">
-                                        <a href="./yt?${element._id}" class="h5 title bangla">${element.title}</a>
-                                        <p class="bangla">${element.description}</p>
-                                    </div>
-                                </div>
-                                <div class="col-12 col-md-3">
-                                    <div class="d-flex align-items-center">
-                                        <a href="./yt?${element._id}" target="_blank" class="btn btn-primary btn-sm btn-block"><span class="fas fa-shopping-cart mr-1"></span>
-                                        শুরু করুন
-                            </a>
-                                    </div>
+                            <div class="col-12 col-md-6">
+                            <div class="card bg-primary border-light shadow-soft">
+                                <img src="${element.image}" class="card-img-top rounded-top" alt="${element.title}">
+                                <div class="card-body">
+                                    <h3 class="h5 card-title mt-3">${element.title}</h3>
+                                    <p class="card-text">${element.description}</p>
+                                    <a href="./yt?${element._id}" class="btn btn-primary btn-sm">Learn More</a>
                                 </div>
                             </div>
                         </div>
                             `;
                         } else {
                             document.getElementById("contents").innerHTML += `
-                            <div class="card bg-primary shadow-soft border-light p-4">
-                            <div class="row align-items-center">
-                                <aside class="col-md-3">
-                                    <a href="./quiz?${element._id}">
-                                        <img src="${element.image}" style="border-radius: 15px;" alt="${element.title}">
-                                    </a>
-                                </aside>
-                                <div class="col-md-6">
-                                    <div class="info-main">
-                                        <a href="./quiz?${element._id}" class="h5 title bangla">${element.title}</a>
-                                        <p class="bangla">${element.description}</p>
-                                    </div>
-                                </div>
-                                <div class="col-12 col-md-3">
-                                    <div class="d-flex align-items-center">
-                                        <a href="./quiz?${element._id}" target="_blank" class="btn btn-primary btn-sm btn-block"><span class="fas fa-shopping-cart mr-1"></span>
-                                        শুরু করুন
-                            </a>
-                                    </div>
+                            <div class="col-12 col-md-6">
+                            <div class="card bg-primary border-light shadow-soft">
+                                <img src="${element.image}" class="card-img-top rounded-top" alt="${element.title}">
+                                <div class="card-body">
+                                    <h3 class="h5 card-title mt-3">${element.title}</h3>
+                                    <p class="card-text">${element.description}</p>
+                                    <a href="./quiz?${element._id}" class="btn btn-primary btn-sm">Learn More</a>
                                 </div>
                             </div>
                         </div>
@@ -64,23 +40,6 @@ firebase.auth().onAuthStateChanged(function (e) {
                         }
 
                     });
-
-                    document.getElementById("contents").innerHTML += `
-                    <div class="card bg-primary shadow-soft border-light p-4">
-                            <div class="row mb-3 mt-3">
-                                <div class="col-12 mb-3">
-                                    <div class="card bg-primary shadow-soft border-light p-4">
-                                    <div class="card-body">
-                                    <h5 class="card-title
-                                    ">আপনার প্রশ্ন কি ছিল?</h5>
-                                    <p class="card-text">আপনার প্রশ্ন কি ছিল? আমাদের সাথে যোগাযোগ করুন।</p>
-                                    <a href="https://www.facebook.com/groups/atquranshikkha" class="btn btn-primary">যোগাযোগ করুন</a>
-                                </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                            `;
 
                 } else {
                     location.replace(`https://${shopName2}/${productCode}`);
