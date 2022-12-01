7705873253bb99f231a47c0d4642fc2e0c53ccf4
Apars Ltd <aparsclassroom@gmail.com>
2022-12-02 05:38:54 +0600

    ok

--- shop/at-tadrees/quran-shikkha/assets/overview.js
+++ shop/at-tadrees/quran-shikkha/assets/overview.js
@@ -9,33 +9,33 @@ firebase.auth().onAuthStateChanged(function (e) {
                     const content = data.course.content;
 
                     const contentLength = content.length;
-
+                    document.getElementById("contents").innerHTML = "";
                     content.forEach(element => {
                         if (element.type == "yt") {
                             document.getElementById("contents").innerHTML += `
                             <div class="col-12 col-md-6">
-                            <div class="card bg-primary border-light shadow-soft">
-                                <img src="${element.image || "https://i.ibb.co/23R2Ftp/thumbnail.jpg"}" class="card-img-top rounded-top" alt="${element.title}">
-                                <div class="card-body">
-                                    <h3 class="h5 card-title mt-3">${element.title}</h3>
-                                    <p class="card-text">${element.description}</p>
-                                    <a href="./yt?${element._id}" class="btn btn-primary btn-sm">Learn More</a>
+                                <div class="card bg-primary border-light shadow-soft">
+                                    <img src="${element.image || "https://i.ibb.co/23R2Ftp/thumbnail.jpg"}" class="card-img-top rounded-top" alt="${element.title}">
+                                    <div class="card-body">
+                                        <h3 class="h5 card-title mt-3">${element.title}</h3>
+                                        <p class="card-text">${element.description}</p>
+                                        <a href="./yt?${element._id}" class="btn btn-primary btn-sm">Learn More</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                             `;
                         } else {
                             document.getElementById("contents").innerHTML += `
                             <div class="col-12 col-md-6">
-                            <div class="card bg-primary border-light shadow-soft">
-                                <img src="${element.image}" class="card-img-top rounded-top" alt="${element.title}">
-                                <div class="card-body">
-                                    <h3 class="h5 card-title mt-3">${element.title}</h3>
-                                    <p class="card-text">${element.description}</p>
-                                    <a href="./quiz?${element._id}" class="btn btn-primary btn-sm">Learn More</a>
+                                <div class="card bg-primary border-light shadow-soft">
+                                    <img src="${element.image}" class="card-img-top rounded-top" alt="${element.title}">
+                                    <div class="card-body">
+                                        <h3 class="h5 card-title mt-3">${element.title}</h3>
+                                        <p class="card-text">${element.description}</p>
+                                        <a href="./quiz?${element._id}" class="btn btn-primary btn-sm">Learn More</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                             `;
                         }
 
