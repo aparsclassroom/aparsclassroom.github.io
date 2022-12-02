124d4c6ad10609b67d8b8801b23e14b53f4fac8e
Apars Ltd <aparsclassroom@gmail.com>
2022-12-02 07:36:14 +0600

    ok

--- shop/at-tadrees/quran-shikkha/assets/overview.js
+++ shop/at-tadrees/quran-shikkha/assets/overview.js
@@ -9,21 +9,13 @@ firebase.auth().onAuthStateChanged(function (e) {
                     const content = data.course.content;
 
                     const contentLength = content.length;
-
-                    document.getElementById('tabs').innerHTML = `  
-                    <li class="nav-item">
-                    <a class="nav-link mb-sm-3 mb-md-0 active" id="tabs1-tab" data-toggle="tab" href="#tabs1" role="tab" aria-controls="tabs1" aria-selected="true">কুরআন শিক্ষা</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link mb-sm-3 mb-md-0" id="tabs2-tab" data-toggle="tab" href="#tabs2" role="tab" aria-controls="tabs2" aria-selected="false">সূরা মশ্ক</a>
-                </li>`;
                     document.getElementById("contents").innerHTML = "";
                     content.forEach(element => {
 
                         if (element.category == "Regular") { 
                             if (element.type == "yt") {
                             document.getElementById("contents").innerHTML += `
-                            <div class="col-md-6" tab-pane" id="tabs1" role="tabpanel"">
+                            <div class="col-md-6 grid-item" data-category="Regular">
                                 <div class="card bg-primary border-light shadow-soft">
                                 <img src="${element.image || "https://i.ibb.co/23R2Ftp/thumbnail.jpg"}" class="card-img-top rounded-top" alt="${element.title}">
                                         <div class="card-body">
@@ -37,7 +29,7 @@ firebase.auth().onAuthStateChanged(function (e) {
                             `;
                             } else {
                                 document.getElementById("contents").innerHTML += `
-                                <div class="col-md-6" tab-pane" id="tabs1" role="tabpanel"" >
+                                <div class="col-md-6 grid-item" data-category="Regular" >
                                     <div class="card bg-primary border-light shadow-soft">
                                         <img src="${element.image || "https://i.ibb.co/23R2Ftp/thumbnail.jpg"}" class="card-img-top rounded-top" alt="${element.title}">
                                         <div class="card-body">
@@ -53,7 +45,7 @@ firebase.auth().onAuthStateChanged(function (e) {
                     } else {
                         if (element.type == "yt") {
                             document.getElementById("contents").innerHTML += `
-                            <div class="col-md-6 tab-content" id="tabs2" role="tabpanel">
+                            <div class="col-md-6 grid-item" data-category="Bonus">
                             <div class="card bg-primary border-light shadow-soft">
                             <img src="${element.image || "https://i.ibb.co/23R2Ftp/thumbnail.jpg"}" class="card-img-top rounded-top" alt="${element.title}">
                                     <div class="card-body">
@@ -67,7 +59,7 @@ firebase.auth().onAuthStateChanged(function (e) {
                             `;
                             } else {
                                 document.getElementById("contents").innerHTML += `
-                                <div class="col-md-6 tab-pane" id="tabs2" role="tabpanel">
+                                <div class="col-md-6 grid-item" data-category="Bonus">
                                 <div class="card bg-primary border-light shadow-soft">
                                 <img src="${element.image || "https://i.ibb.co/23R2Ftp/thumbnail.jpg"}" class="card-img-top rounded-top" alt="${element.title}">
                                         <div class="card-body">
@@ -81,6 +73,10 @@ firebase.auth().onAuthStateChanged(function (e) {
                                 `;
                         } 
                         }
+
+                        $('#contents').isotope({
+                            itemSelector: '.grid-item'
+                          });
                     });
 
                 } else {
