e795f442ae9e86b610dbb9987c5c913478a0c363
Apars Ltd <aparsclassroom@gmail.com>
2022-12-02 17:07:44 +0600

    ok

--- shop/at-tadrees/quran-shikkha/assets/quiz.js
+++ shop/at-tadrees/quran-shikkha/assets/quiz.js
@@ -12,7 +12,7 @@ firebase.auth().onAuthStateChanged(function (e) {
                 if (data.status == 200) {
                     const content = data.content;
                     document.getElementById("title").innerHTML = content.title;
-                    document.getElementById("description").innerHTML = content.description;
+                    document.getElementById("description").innerText = content.description;
                     if (content.type != "yt" || content.type != "ftp") {
                         document.getElementById('content').src = content.link;
                     } else {
