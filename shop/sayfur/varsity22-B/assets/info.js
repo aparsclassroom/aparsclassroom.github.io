d117c8c8ba1fcb24f48e62a98cd6ee0355a34bc9
Apars Ltd <aparsclassroom@gmail.com>
2022-12-11 12:46:58 +0600

    ok

--- shop/sayfur/varsity22-B/assets/info.js
+++ shop/sayfur/varsity22-B/assets/info.js
@@ -1,5 +1,5 @@
 const product = "sayfurV22-B";
-const productName = "Varsity 'B-Unit' Exclusive Online Program";
+const productName = "School of Excellence “B-Unit” Online Course";
 const productCode = "202";
 const fix = 7500;
 const pls = 2223;
