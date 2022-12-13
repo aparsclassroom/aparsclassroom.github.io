0c6874a1f22fcaceaba34f8f5b8600f2235fdc28
Apars Ltd <aparsclassroom@gmail.com>
2022-12-14 02:45:43 +0600

    ok

---
+++ shop/emon/aoc24/assets/info.js
@@ -0,0 +1,7 @@
+const product = "aoc24";
+const productName = "Amar Online College HSC-24";
+const productCode = "207";
+const fix = 2500;
+const pls = 1000;
+const Platform = "Super Admin";
+const init = 0;
--- shop/pavel/varsity_medical_english/assets/info.js
+++
@@ -1,7 +0,0 @@
-const product = "sayfurV22-B";
-const productName = "School of Excellence “B-Unit” Online Course";
-const productCode = "202";
-const fix = 7500;
-const pls = 2223;
-const Platform = "Sayfur";
-const init = 0;
