837b6ec3709c6a211c05aef168cffc61e45f5450
Apars Ltd <aparsclassroom@gmail.com>
2022-12-26 13:01:28 +0600

    ok

---
+++ shop/admission/HSC_22/IBA/assets/info.js
@@ -0,0 +1,7 @@
+const product = "Engineering22";
+const productName = "IBA BBA ADMISSION PROGRAM";
+const productCode = "210";
+const fix = 4000;
+const pls = 3000;
+const Platform = "Super Admin";
+const init = 0;
