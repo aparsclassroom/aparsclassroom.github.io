9567bb38dc6e35a9f3df098f1fb9d8a06726979b
Apars Ltd <aparsclassroom@gmail.com>
2023-01-04 14:35:23 +0600

    ok

---
+++ shop/admission/HSC_22/Medical_Exam_Batch/assets/info.js
@@ -0,0 +1,7 @@
+const product = "ACSMAT22";
+const productName = "Medical Admission Exam Batch PROGRAM";
+const productCode = "213";
+const fix = 2000;
+const pls = 999;
+const Platform = "Super Admin";
+const init = 0;
