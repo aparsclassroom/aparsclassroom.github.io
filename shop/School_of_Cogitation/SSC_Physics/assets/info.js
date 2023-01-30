6a9dfd0f01687fa8af6bbb0513f788031350c403
Apars Ltd <aparsclassroom@gmail.com>
2023-01-31 00:10:17 +0600

    ok

--- shop/School_of_Cogitation/SSC_Physics/assets/info.js
+++ shop/School_of_Cogitation/SSC_Physics/assets/info.js
@@ -1,7 +1,7 @@
 const product = "sscPhy";
 const productName = "SSC Physics A Full Fledged course for SSC- 2024 & 2025";
 const productCode = "216";
-const fix = 1000;
-const pls = 500;
+const fix = 5000;
+const pls = 2000;
 const Platform = "Ucchash";
 const init = 0;
