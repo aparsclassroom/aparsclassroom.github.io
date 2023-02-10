26a80d6c3c3e02c8ebc84c13c26fa77558916e16
Apars Ltd <aparsclassroom@gmail.com>
2023-02-10 19:40:42 +0600

    ok

---
+++ shop/FRB23/assets/info.js
@@ -0,0 +1,7 @@
+const product = "FRB23";
+const productName = "HSC 23 FINAL REVISION BATCH";
+const productCode = "218";
+const fix = 5000;
+const pls = 2000;
+const Platform = "Super Admin";
+const init = 0;
