4073b22db817d875b8ad3a1c7b35405330800d7f
Apars Ltd <aparsclassroom@gmail.com>
2023-04-10 19:46:45 +0600

    ok

--- shop/admission/HSC_22/Engineering_Exam_Batch/assets/info.js
+++ shop/admission/HSC_22/Engineering_Exam_Batch/assets/info.js
@@ -2,6 +2,6 @@ const product = "ACSEEB22";
 const productName = "Engineering Exam Batch";
 const productCode = "230";
 const fix = 2000;
-const pls = 999;
+const pls = 1000;
 const Platform = "Super Admin";
 const init = 0;
