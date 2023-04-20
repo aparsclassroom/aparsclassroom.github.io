c63105212a5f9ff913b9c1c0f96124525a0a59e2
Apars Ltd <aparsclassroom@gmail.com>
2023-04-20 16:00:21 +0600

    ok

--- shop/reza/HSC_24_English_A_to_Z/assets/info.js
+++ shop/reza/HSC_24_English_A_to_Z/assets/info.js
@@ -1,5 +1,5 @@
 const product = "reza HSC24";
-const productName = "HSC 24 English A to Z";
+const productName = "HSC 24 English A to Z (2nd Paper)";
 const productCode = "212";
 const fix = 2500;
 const pls = 1500;
