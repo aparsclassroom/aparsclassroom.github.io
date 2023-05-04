61b19791a30ac362f313d03a611a0e7bb1b537ff
Apars Ltd <aparsclassroom@gmail.com>
2023-05-04 22:37:59 +0600

    ok

---
+++ shop/The_U_Academy/ICT24/assets/info.js
@@ -0,0 +1,7 @@
+const product = "uAcaICT24";
+const productName = "HSC ICT : A Full Fledged Course for HSC-2024";
+const productCode = "235";
+const fix = 1000;
+const pls = 500;
+const Platform = "Ucchash";
+const init = 0;
