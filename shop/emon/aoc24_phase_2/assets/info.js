6f004bb6e65eda87cf494766c16ec03816bc48c0
Apars Ltd <aparsclassroom@gmail.com>
2023-05-20 12:56:03 +0600

    ok

---
+++ shop/emon/aoc24_phase_2/assets/info.js
@@ -0,0 +1,7 @@
+const product = "aoc24_p2";
+const productName = "Amar Online College Phase-2 HSC-24";
+const productCode = "236";
+const fix = 5000;
+const pls = 2500;
+const Platform = "Super Admin";
+const init = 0;
