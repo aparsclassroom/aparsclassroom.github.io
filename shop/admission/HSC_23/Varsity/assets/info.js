ad841ff58af60ac752c6a65bfabdba76d8ca8596
Apars Ltd <aparsclassroom@gmail.com>
2023-06-08 22:07:14 +0600

    ok

---
+++ shop/admission/HSC_23/Varsity/assets/info.js
@@ -0,0 +1,7 @@
+const product = "Varsity23";
+const productName = "Varsity Admission Private Batch 2023";
+const productCode = "231";
+const fix = 5000;
+const pls = 4000;
+const Platform = "Super Admin";
+const init = 0;
