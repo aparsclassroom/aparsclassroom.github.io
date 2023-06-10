43d917f5c206a1a5226758e70571258c6f62b492
Apars Ltd <aparsclassroom@gmail.com>
2023-06-10 19:15:18 +0600

    ok

--- shop/admission/HSC_23/Engineering/assets/info.js
+++ shop/admission/HSC_23/Engineering/assets/info.js
@@ -1,5 +1,5 @@
 const product = "Engineering23";
-const productName = "Engineering Admission Private Batch 2023";
+const productName = "ACS Engineering Admission Private Batch 2023";
 const productCode = "231";
 const fix = 5000;
 const pls = 4000;
