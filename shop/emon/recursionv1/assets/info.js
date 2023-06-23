9945828fc0b4357d9b456a5d9f8afdfa9fbae3a8
Apars Ltd <aparsclassroom@gmail.com>
2023-06-24 01:13:16 +0600

    ok

--- shop/emon/recursionv1/assets/info.js
+++ shop/emon/recursionv1/assets/info.js
@@ -1,7 +1,7 @@
-const product = "aoc24";
-const productName = "Amar Online College HSC-24";
-const productCode = "207";
-const fix = 5000;
-const pls = 2500;
+const product = "recursionv1";
+const productName = "Batch Recursion";
+const productCode = "242";
+const fix = 1000;
+const pls = 799;
 const Platform = "Super Admin";
 const init = 0;
