642abeba443fb4a8d87721eadc5accbc7417d307
Apars Ltd <aparsclassroom@gmail.com>
2023-06-24 00:57:47 +0600

    ok

---
+++ shop/emon/recursionv1/assets/info.js
@@ -0,0 +1,7 @@
+const product = "aoc24";
+const productName = "Amar Online College HSC-24";
+const productCode = "207";
+const fix = 5000;
+const pls = 2500;
+const Platform = "Super Admin";
+const init = 0;
