2aaf6f084e5ead5458cda80d205408498bb95bd6
Apars Ltd <aparsclassroom@gmail.com>
2023-06-23 19:50:54 +0600

    ok

--- shop/dmc-dreamers/b2b24/assets/js/free.js
+++ shop/dmc-dreamers/b2b24/assets/js/free.js
@@ -3,7 +3,7 @@ document.getElementById('prod').innerText = productName;
 
 firebase.auth().onAuthStateChanged(function(e) {
     if (e) {
-        document.getElementById('prod').innerHTML = `${productName}<br>(${Cycle})`;
+        document.getElementById('prod').innerHTML = `${productName}<br>(Free Cycle)`;
         document.getElementById('moda').addEventListener('click', () => {
             location.href =  'https://b2b24.aparsclassroom.com/'
         });
