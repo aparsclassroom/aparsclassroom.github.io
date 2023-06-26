43c653d11e9d0e1e87dd4ab13a3d38a0ccc9a849
Apars Ltd <aparsclassroom@gmail.com>
2023-06-26 10:45:28 +0600

    ok

--- shop/dmc-dreamers/b2b24/assets/js/free.js
+++ shop/dmc-dreamers/b2b24/assets/js/free.js
@@ -5,7 +5,7 @@ firebase.auth().onAuthStateChanged(function(e) {
     if (e) {
         document.getElementById('prod').innerHTML = `${productName}<br>(Free Cycle)`;
         document.getElementById('moda').addEventListener('click', () => {
-            location.href =  'https://b2b24.aparsclassroom.com/'
+            location.href =  'https://b2b24.aparsclassroom.com/b2b24/Cycle-0'
         });
         document.getElementById('moda').innerHTML = 'ক্লাসে জয়েন করো <i class="fas fa-arrow-right"></i>';
     } else {
