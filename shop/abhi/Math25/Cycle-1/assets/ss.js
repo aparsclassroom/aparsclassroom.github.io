d99683e3450ef027008deb09baff2c56f24aa253
Apars Ltd <aparsclassroom@gmail.com>
2023-07-10 21:19:51 +0600

    ok

--- shop/abhi/Math25/Cycle-1/assets/ss.js
+++ shop/abhi/Math25/Cycle-1/assets/ss.js
@@ -1,6 +1,6 @@
 const product = "Math25";
 const productName = "Higher Math 1st Paper (HSC 25)";
-const productCode = "253";
+const productCode = "254";
 const fix = 1500;
 const pls = 1000;
 const init = 0;
