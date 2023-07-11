2b4f71d67343198dbdf903be946f65519d40d183
Apars Ltd <aparsclassroom@gmail.com>
2023-07-11 13:46:21 +0600

    ok

---
+++ shop/robotics/assets/info.js
@@ -0,0 +1,7 @@
+const product = "Robotics";
+const productName = "Robotics - A journey with Arduino";
+const productCode = "255";
+const fix = 10000;
+const pls = 6000;
+const Platform = "Super Admin";
+const init = 0;
