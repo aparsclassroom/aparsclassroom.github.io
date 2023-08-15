e2812fa6f21cd683c30b3a4dee8abe8bc72f3981
Apars Ltd <aparsclassroom@gmail.com>
2023-08-15 18:56:54 +0600

    ok

--- shop/dmc-dreamers/b2b25/assets/js/index.js
+++ shop/dmc-dreamers/b2b25/assets/js/index.js
@@ -1,7 +1,16 @@
 function wait(time) {
-    swal({
-        title: "Not Published",
-        icon: "info",
-        text: "Please Wait till " + time.split("-")[0] +" for this cycle to be published"
-    })
+    if (time) {
+        swal({
+            title: "Not Published",
+            icon: "info",
+            text: "Please Wait till " + time.split("-")[0] +" for this cycle to be published"
+        })
+    } else {
+        swal({
+            title: "Not Published",
+            icon: "info",
+            text: "Please Wait for this cycle to be published"
+        })
+    }
+    
 }
