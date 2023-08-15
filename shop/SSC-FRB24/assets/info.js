21fd3a95922b58ba926ad1e17f8a3e4c08d1c54c
Apars Ltd <aparsclassroom@gmail.com>
2023-08-15 19:49:34 +0600

    ok

---
+++ shop/SSC-FRB24/assets/info.js
@@ -0,0 +1,7 @@
+const product = "SSCFRB25";
+const productName = "SSC 24 Batch Final Revision Batch";
+const productCode = "265";
+const fix = 2000;
+const pls = 1250;
+const Platform = "Super Admin";
+const init = 0;
