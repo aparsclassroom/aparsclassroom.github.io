f38ccab1e6aa3a6f3d00cac753bb9738ea3ce152
Apars Ltd <aparsclassroom@gmail.com>
2023-08-15 18:45:58 +0600

    ok

--- shop/dmc-dreamers/b2b25/Cycle-0/assets/ss.js
+++ shop/dmc-dreamers/b2b25/Cycle-0/assets/ss.js
@@ -12,30 +12,30 @@ if (screen.width <= 600) {
     vidD.style.position = 'sticky';
 }
 
-var tag = document.createElement('script');
+// var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-var player;
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// var player;
 
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'HVSyVTFVfAg',
-        playerVars: { 'autoplay': 1, 'playsinline': 1 },
-        events: {
-            'onReady': onPlayerReady
-        }
-    });
-}
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         height: '390',
+//         width: '640',
+//         videoId: '',
+//         playerVars: { 'autoplay': 1, 'playsinline': 1 },
+//         events: {
+//             'onReady': onPlayerReady
+//         }
+//     });
+// }
 
-function onPlayerReady(event) {
-    document.getElementById('thumb').style.display = "none";
-    event.target.setVolume(100);
-    event.target.playVideo();
-}
+// function onPlayerReady(event) {
+//     document.getElementById('thumb').style.display = "none";
+//     event.target.setVolume(100);
+//     event.target.playVideo();
+// }
 fetch(`https://b2b25.aparsclassroom.com/api/public/check/enrollment/${Cycle}`)
     .then((res) => {
         return res.json()
