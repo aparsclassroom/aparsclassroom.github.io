ecda2c943477987c1b47bdc69af98e438643b14e
Apars Ltd <aparsclassroom@gmail.com>
2023-09-09 16:31:13 +0600

    ok

--- assets/js/stat.js
+++ assets/js/stat.js
@@ -9,29 +9,4 @@ firebase.auth().onAuthStateChanged(function(user) {
     } else {
         document.getElementById('prof').href = "/shop/dashboard/login?signInSuccessUrl=" + encodeURIComponent(location.href);
     }
-    var element = $('#countdown-gampang');
-var finish_d = new Date('July 30, 2022');
-finish_d.setDate(finish_d.getDate());
-element.CountdownGampang({
-    rampung: finish_d,
-    theme: "flat-colors-black"
-}, function() {
-    document.getElementById('hsc').innerHTML = "Today is the Gst Exam !<br><strong>Good Luck ❤️</strong>";
-
 });
-})
-  var clipboard = new ClipboardJS('.fa-copy');
-  clipboard.on('success', function(e) {
-      Toastify({
-        text: "Discount Coupon Copied !",
-        duration: 3000,
-        gravity: "top",
-        position: "right",
-        stopOnFocus: true
-      }).showToast();
-      e.clearSelection();
-  });
-  
-  clipboard.on('error', function(e) {
-      alert('Action:', e.action);
-  });
