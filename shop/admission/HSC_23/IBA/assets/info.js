769df692c4864ed513abcb4cf02de2e1898052e3
Apars Ltd <aparsclassroom@gmail.com>
2023-09-13 13:22:16 +0600

    ok

---
+++ shop/admission/HSC_23/IBA/assets/info.js
@@ -0,0 +1,7 @@
+const product = "iba23";
+const productName = "IBA BBA ADMISSION PROGRAM";
+const productCode = "270";
+const fix = 4000;
+const pls = 2500;
+const Platform = "Super Admin";
+const init = 0;
