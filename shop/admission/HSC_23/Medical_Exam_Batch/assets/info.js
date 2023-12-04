87cd5279c2c1781dc8481286471bb013cdb1e736
Apars Ltd <aparsclassroom@gmail.com>
2023-12-04 10:34:32 +0600

    ok

---
+++ shop/admission/HSC_23/Medical_Exam_Batch/assets/info.js
@@ -0,0 +1,7 @@
+const product = "ACSMAT23";
+const productName = "ACS Medical Admission Exam Batch 23";
+const productCode = "287";
+const fix = 999;
+const pls = 500;
+const Platform = "Super Admin";
+const init = 0;
