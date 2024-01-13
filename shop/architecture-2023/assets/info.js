55504fcca50f3495c150dd11040f9cb06b0a378a
Apars Ltd <aparsclassroom@gmail.com>
2024-01-13 17:18:14 +0600

    update

---
+++ shop/architecture-2023/assets/info.js
@@ -0,0 +1,7 @@
+const product = "Architecture23";
+const productName = "HSC 2023 - Architecture Admission Course";
+const productCode = "288";
+const fix = 5000;
+const pls = 2000;
+const Platform = "Super Admin";
+const init = 0;
