af6f0106d4afdb8514c65b0294eb4b491d603423
Apars Ltd <aparsclassroom@gmail.com>
2024-02-18 16:05:04 +0600

    ok

--- shop/admission/HSC_24/Medical/assets/info.js
+++ shop/admission/HSC_24/Medical/assets/info.js
@@ -1,7 +1,7 @@
 const product = "Medical24";
 const productName = "ACS Medical Admission Private Batch 2024";
 const productCode = "291";
-const fix = 5000;
-const pls = 4000;
+const fix = 6000;
+const pls = 5000;
 const Platform = "Super Admin";
 const init = 0;
