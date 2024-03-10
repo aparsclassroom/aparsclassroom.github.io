43cd141189e530bd1b6860ef61035e02c7400415
Apars Ltd <aparsclassroom@gmail.com>
2024-03-10 12:52:22 +0600

    Delete unused JavaScript files

--- shop/mahir-sir/khonik22/assets/info.js
+++
@@ -1,7 +0,0 @@
-const product = "khonik22";
-const productName = "Higher Math Topicwise CQ+MCQ ব্যাচ ক্ষণিক";
-const productCode = "190";
-const fix = 3000;
-const pls = 2000;
-const Platform = "Mahir";
-const init = 529;
