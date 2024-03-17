6e410b8605f0ddf45df5e8d5e834ed23d8849378
Apars Ltd <aparsclassroom@gmail.com>
2024-03-18 01:38:09 +0600

    Update function to handle edge cases

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyD4WuQA56koZ-qWV56rDXDTtczaCVnGft8",
+    authDomain: "asg-shop.firebaseapp.com",
+    projectId: "asg-shop",
+    storageBucket: "asg-shop.appspot.com",
+    messagingSenderId: "374714320984",
+    appId: "1:374714320984:web:d2d308f1ea2a9f46bbe22d",
+    measurementId: "G-P18HXDWK2Y"
+});
+
+const messaging = firebase.messaging();
