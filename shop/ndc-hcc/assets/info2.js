83c1e4a8858494ba2e8d1f889b284ec91ce3f95c
Apars Ltd <aparsclassroom@gmail.com>
2024-03-29 17:26:48 +0600

    Update product name in info2.js

--- shop/ndc-hcc/assets/info2.js
+++ shop/ndc-hcc/assets/info2.js
@@ -1,5 +1,5 @@
 const product = "ndc-hcch25";
-const productName = "ACS College Admission Free Course 2023";
+const productName = "ACS College Admission Free Course 2024";
 const productCode = "000";
 const Platform = "Super Admin";
 const init = 0;
