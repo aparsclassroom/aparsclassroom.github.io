22d5f56d5e3e826ed9dc4dd78b39909fbf80b7c7
Apars Ltd <aparsclassroom@gmail.com>
2024-04-09 12:11:15 +0600

    Add ASG SHOP description to index.html

---
+++ shop/abhi/decoder_26/assets/info.js
@@ -0,0 +1,7 @@
+const product = "decoder";
+const productName = "ACS ICT Batch Decoder 2026";
+const productCode = "261";
+const fix = 1500;
+const pls = 1000;
+const Platform = "Abhi";
+const init = 0;
