ea89d37543e6957ad9a2a2f7c288f83fcbdf156e
Apars Ltd <aparsclassroom@gmail.com>
2024-04-09 15:24:46 +0600

    Update product information and add pre-booking details for decoder_26

--- shop/abhi/decoder_26/assets/info.js
+++ shop/abhi/decoder_26/assets/info.js
@@ -1,6 +1,6 @@
-const product = "decoder";
+const product = "decoder_26";
 const productName = "ACS ICT Batch Decoder 2026";
-const productCode = "261";
+const productCode = "300";
 const fix = 1500;
 const pls = 1000;
 const Platform = "Abhi";
