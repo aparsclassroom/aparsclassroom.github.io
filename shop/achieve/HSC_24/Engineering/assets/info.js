b7f977bcbb18f93fcc52dcf954f0ac31c371b117
Apars Ltd <aparsclassroom@gmail.com>
2024-04-14 17:33:47 +0600

    Add form fields for name, phone, email, and course selection

---
+++ shop/achieve/HSC_24/Engineering/assets/info.js
@@ -0,0 +1,7 @@
+const product = "Engineering24";
+const productName = "ACS Engineering Admission Private Batch 2024";
+const productCode = "293";
+const fix = 10000;
+const pls = 5000;
+const Platform = "Super Admin";
+const init = 0;
