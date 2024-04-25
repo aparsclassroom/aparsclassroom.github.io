66ec668a6b282d90e2b07129d02fb99d5fe2d873
Apars Ltd <aparsclassroom@gmail.com>
2024-04-25 22:55:53 +0600

    Update productCode and pls values in info.js, add HSC 26 option in hscBatch select in index.html, and add meta refresh tag in pre-book.html

--- shop/abhi/decoder_26/assets/info.js
+++ shop/abhi/decoder_26/assets/info.js
@@ -1,7 +1,7 @@
 const product = "decoder_26";
 const productName = "ACS ICT Batch Decoder 2026";
-const productCode = "300";
+const productCode = "303";
 const fix = 1500;
-const pls = 1000;
-const Platform = "Abhi";
+const pls = 1250;
+const Platform = "Online";
 const init = 0;
