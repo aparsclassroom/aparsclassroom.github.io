6eec369753463efd9a328608d0c1c5febea38536
Apars Ltd <aparsclassroom@gmail.com>
2024-05-06 16:33:14 +0600

    Update HSC Batch options to include HSC'26 links and fix typos in URLs

--- shop/achieve/HSC_24/Medical/assets/info.js
+++ shop/achieve/HSC_24/Medical/assets/info.js
@@ -2,6 +2,6 @@ const product = "AchieveMedical24";
 const productName = "ACS Achieve Medical Exam Batch 2024";
 const productCode = "301";
 const fix = 15000;
-const pls = 10000;
+const pls = 8000;
 const Platform = "Offline";
 const init = 0;
