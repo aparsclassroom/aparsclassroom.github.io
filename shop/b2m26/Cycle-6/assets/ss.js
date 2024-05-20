ed9a59f337b54874981efbec4aa3cea4395d6544
Apars Ltd <aparsclassroom@gmail.com>
2024-05-20 21:13:19 +0600

    Update batch information and chapter details for Cycle 1, Cycle 2, Cycle 3, Cycle 4, and Cycle 5

--- shop/b2m26/Cycle-6/assets/ss.js
+++ shop/b2m26/Cycle-6/assets/ss.js
@@ -15,30 +15,30 @@ if (screen.width <= 600) {
     vidD.style.position = 'sticky';
 }
 
-var tag = document.createElement('script');
+// var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-var player;
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// var player;
 
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'qAXDnWz13ag',
-        playerVars: { 'autoplay': 1, 'playsinline': 1 },
-        events: {
-            'onReady': onPlayerReady
-        }
-    });
-}
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         height: '390',
+//         width: '640',
+//         videoId: '',
+//         playerVars: { 'autoplay': 1, 'playsinline': 1 },
+//         events: {
+//             'onReady': onPlayerReady
+//         }
+//     });
+// }
 
-function onPlayerReady(event) {
-    document.getElementById('thumb').style.display = "none";
-    event.target.setVolume(100);
-    event.target.playVideo();
-}
+// function onPlayerReady(event) {
+//     document.getElementById('thumb').style.display = "none";
+//     event.target.setVolume(100);
+//     event.target.playVideo();
+// }
 fetch(`https://${shopName2}/enrollment/${Cycle}?productCode=${productCode}`)
     .then((res) => {
         return res.json()
@@ -59,7 +59,7 @@ fetch(`https://${shopName2}/enrollment/${Cycle}?productCode=${productCode}`)
         console.log(err)
     })
     document.getElementById('con1').innerHTML = `<i class="fab fa-youtube"></i>&nbsp; সপ্তাহে দুুদিন ক্লাস`;
-    document.getElementById('con3').innerHTML = `<i class="fas fa-clinic-medical"></i>&nbsp; পদার্থবিজ্ঞান দ্বিতীয় পত্র অধ‍্যায় ৮,৯,১০`;
+    document.getElementById('con3').innerHTML = `<i class="fas fa-clinic-medical"></i>&nbsp;দ্বিতীয় পত্র- অধ্যায় (৪,৫,৮)`;
     document.getElementById('con4').innerHTML = `<i class="fas fa-question-circle"></i>&nbsp;x Exams `;
     document.getElementById('con5').innerHTML = `<i class="fas fa-calendar-check"></i>&nbsp;x Guideline Sessions `;
     document.getElementById('con7').innerHTML = `<i class="fas fa-tablet-alt"></i>&nbsp; Dedicated Web App`;
