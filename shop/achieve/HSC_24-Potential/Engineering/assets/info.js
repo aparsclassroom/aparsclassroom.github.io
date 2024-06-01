e7bde0c83c8f4410ba36362206e8a65da7fc518a
Apars Ltd <aparsclassroom@gmail.com>
2024-06-01 15:18:40 +0600

    o

---
+++ shop/achieve/HSC_24-Potential/Engineering/assets/info.js
@@ -0,0 +1,7 @@
+const product = "potentialBUET24";
+const productName = "ACS Potential BUETian Offline Exam 2024";
+const productCode = "333";
+const fix = 0;
+const pls = 0;
+const Platform = "Offline";
+const init = 0;
---
+++ shop/achieve/HSC_24-Potential/Medical/assets/info.js
@@ -0,0 +1,7 @@
+const product = "AchieveMedical24";
+const productName = "ACS Achieve Medical Exam Batch 2024";
+const productCode = "301";
+const fix = 15000;
+const pls = 8000;
+const Platform = "Offline";
+const init = 0;
