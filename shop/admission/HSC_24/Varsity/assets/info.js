c409e4d6b4f1fb92cb9dab75e3237dc91db585fd
Apars Ltd <aparsclassroom@gmail.com>
2024-07-05 17:44:49 +0600

    ok

--- shop/admission/HSC_24/Varsity/assets/info.js
+++ shop/admission/HSC_24/Varsity/assets/info.js
@@ -1,7 +1,7 @@
-const product = "Varsity24";
-const productName = "ACS Varsity + GST Admission Private Batch 2024";
+const product = "varsity24";
+const productName = "ACS Varsity + GST Special Private Programme 2024";
 const productCode = "334";
 const fix = 10000;
-const pls = 5000;
+const pls = 4000;
 const Platform = "Online";
 const init = 0;
