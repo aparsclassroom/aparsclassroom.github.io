de4a3431f9dafcffd28b29dc5fd20c7d1f491a97
Apars Ltd <aparsclassroom@gmail.com>
2024-08-14 01:08:39 +0600

    গত

--- shop/SSC-FRB24/assets/info.js
+++ shop/SSC-FRB24/assets/info.js
@@ -1,6 +1,6 @@
-const product = "SSCFRB25";
+const product = "ACSSSCFRB25";
 const productName = "SSC 24 Batch Final Revision Batch";
-const productCode = "265";
+const productCode = "236";
 const fix = 2000;
 const pls = 1250;
 const Platform = "Online";
