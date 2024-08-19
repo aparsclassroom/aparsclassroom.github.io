e01ccabc60945fec598eaba289fad55e59a9158d
Apars Ltd <aparsclassroom@gmail.com>
2024-08-19 23:32:38 +0600

    ielts course

---
+++ shop/IELTS/assets/info.js
@@ -0,0 +1,7 @@
+const product = "ielts";
+const productName = "IELTS Crash Course + Abroad Admission Tips";
+const productCode = "338";
+const fix = 10000;
+const pls = 4000;
+const Platform = "Online";
+const init = 0;
