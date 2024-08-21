072d7f6a91db876e12d33b5ca9c65d6f30326a08
Apars Ltd <aparsclassroom@gmail.com>
2024-08-21 23:17:26 +0600

    ok

--- shop/SSC-FRB25/assets/info.js
+++ shop/SSC-FRB25/assets/info.js
@@ -1,6 +1,6 @@
-const product = "ACS.SSC.FRB25";
+const product = "ACS_SSC_FRB25";
 const productName = "SSC 25 Batch Final Revision Batch";
-const productCode = "340";
+const productCode = "336";
 const fix = 1500;
 const pls = 1250;
 const Platform = "Online";
