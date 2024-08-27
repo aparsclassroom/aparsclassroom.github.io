0f1d07b041db421c744c336458e9a4a29de6ae56
Apars Ltd <aparsclassroom@gmail.com>
2024-08-27 20:07:34 +0600

    ok

--- shop/achieve/HSC_24-Potential/Engineering/assets/info.js
+++ shop/achieve/HSC_24-Potential/Engineering/assets/info.js
@@ -1,6 +1,6 @@
 const product = "potentialBUET24";
 const productName = "ACS Potential BUETian Offline Exam 2024";
-const productCode = "333";
+const productCode = "300";
 const fix = 0;
 const pls = 0;
 const Platform = "Offline";
