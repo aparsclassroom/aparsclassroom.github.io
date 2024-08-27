80f29645080c438db55248f85c8c9d6904593147
Apars Ltd <aparsclassroom@gmail.com>
2024-08-27 20:25:58 +0600

    med

--- shop/achieve/HSC_24-Potential/Medical/assets/info.js
+++ shop/achieve/HSC_24-Potential/Medical/assets/info.js
@@ -1,6 +1,6 @@
 const product = "AchieveMedical24";
 const productName = "ACS Potential Medicalian Exam 2024 ";
-const productCode = "301";
+const productCode = "340";
 const fix = 15000;
 const pls = 8000;
 const Platform = "Offline";
