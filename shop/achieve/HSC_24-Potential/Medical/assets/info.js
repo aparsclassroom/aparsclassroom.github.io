3baed1a62725e118c19d9ae594440e1821022ddc
Apars Ltd <aparsclassroom@gmail.com>
2024-08-27 19:58:07 +0600

    ok

--- shop/achieve/HSC_24-Potential/Medical/assets/info.js
+++ shop/achieve/HSC_24-Potential/Medical/assets/info.js
@@ -1,5 +1,5 @@
 const product = "AchieveMedical24";
-const productName = "ACS Achieve Medical Exam Batch 2024";
+const productName = "ACS Potential Medicalian Exam 2024 ";
 const productCode = "301";
 const fix = 15000;
 const pls = 8000;
