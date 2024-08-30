abef5b7bc9e0583273c6893e50edeabf7e3e98a9
Apars Ltd <aparsclassroom@gmail.com>
2024-08-31 01:45:15 +0600

    benjon 2.0

---
+++ shop/sakib/benjon25_26/assets/info.js
@@ -0,0 +1,7 @@
+const product = "benjon25_26";
+const productName = "বাংলা ব্যঞ্জন ২৫-২৬ ব্যাচ";
+const productCode = "342";
+const fix = 1500;
+const pls = 1000;
+const Platform = "Sakib";
+const init = 0;
