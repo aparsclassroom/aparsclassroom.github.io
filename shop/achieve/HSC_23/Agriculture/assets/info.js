affc740136534209da52f3df3d6de193195afc4e
Apars Ltd <aparsclassroom@gmail.com>
2024-09-21 21:01:09 +0600

    price and routine

--- shop/achieve/HSC_23/Agriculture/assets/info.js
+++ shop/achieve/HSC_23/Agriculture/assets/info.js
@@ -1,7 +1,7 @@
 const product = "AchieveAgriculture24";
 const productName = "Achieve Agriculture Exam Batch 2024";
 const productCode = "345";
-const fix = 10000;
-const pls = 5000;
+const fix = 5000;
+const pls = 1500;
 const Platform = "Offline";
 const init = 0;
