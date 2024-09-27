6671992cf67e37f1c00b1f1f49396d625a04664d
Apars Ltd <aparsclassroom@gmail.com>
2024-09-27 16:09:57 +0600

    Architecture24

---
+++ shop/architecture-2024/assets/info.js
@@ -0,0 +1,7 @@
+const product = "Architecture24";
+const productName = "HSC 2024 - Architecture Admission Course";
+const productCode = "347";
+const fix = 5000;
+const pls = 2000;
+const Platform = "Online";
+const init = 0;
