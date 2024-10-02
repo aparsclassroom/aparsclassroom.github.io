b04c4dd0b42a9a33095d6af61ed3459d87a805ee
Apars Ltd <aparsclassroom@gmail.com>
2024-10-02 12:23:46 +0600

    engineeringBiology

---
+++ shop/admission/HSC_24/EngineeringBiology/assets/info.js
@@ -0,0 +1,7 @@
+const product = "engineeringBiology";
+const productName = "Recorded Varsity Biology for ACS Engineering 2024";
+const productCode = "348";
+const fix = 5000;
+const pls = 1000;
+const Platform = "Online";
+const init = 0;
