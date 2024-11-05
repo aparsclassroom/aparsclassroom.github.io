573febeba0261c1d7885b7006c8ae19898ef898f
Apars Ltd <aparsclassroom@gmail.com>
2024-11-05 18:44:27 +0600

    ok

--- shop/achieve/HSC_24/Varsity-FMT-Books/assets/info.js
+++ shop/achieve/HSC_24/Varsity-FMT-Books/assets/info.js
@@ -1,6 +1,6 @@
 const product = "AchieveVarsity24FMTwithBooks";
 const productName = "Achieve Varsity Final Model Test with ACS Practice Books";
-const productCode = "335352";
+const productCode = "353";
 const fix = 12000;
 const pls = 5500;
 const Platform = "Offline";
