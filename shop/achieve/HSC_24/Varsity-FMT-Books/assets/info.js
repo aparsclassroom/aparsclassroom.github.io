a2d830beaaa2b5a2d9f7d11554fd932052093f2d
Apars Ltd <aparsclassroom@gmail.com>
2024-11-05 18:16:11 +0600

    achieve FMT, FRB25

---
+++ shop/achieve/HSC_24/Varsity-FMT-Books/assets/info.js
@@ -0,0 +1,7 @@
+const product = "AchieveVarsity24FMTwithBooks";
+const productName = "Achieve Varsity Final Model Test with ACS Practice Books";
+const productCode = "335352";
+const fix = 12000;
+const pls = 5500;
+const Platform = "Offline";
+const init = 0;
---
+++ shop/achieve/HSC_24/Varsity-FMT/assets/info.js
@@ -0,0 +1,7 @@
+const product = "AchieveVarsity24FMT";
+const productName = "Achieve Varsity Final Model Test";
+const productCode = "352";
+const fix = 10000;
+const pls = 3500;
+const Platform = "Offline";
+const init = 0;
