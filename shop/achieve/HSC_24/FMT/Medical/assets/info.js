8d4929b9ac4433d5dcb13ef79bc0816809d17aa3
Apars Ltd <aparsclassroom@gmail.com>
2024-11-10 20:06:15 +0600

    MATFMT

---
+++ shop/achieve/HSC_24/FMT/Medical/assets/info.js
@@ -0,0 +1,7 @@
+const product = "AchieveMedical24FMT";
+const productName = "Achieve Medical Final Model Test";
+const productCode = "354";
+const fix = 2000;
+const pls = 1500;
+const Platform = "Offline";
+const init = 0;
