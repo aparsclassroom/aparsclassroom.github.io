6d832e835e3c45419d638b6ad1bdd4f50014684c
Apars Ltd <aparsclassroom@gmail.com>
2024-11-18 14:17:28 +0600

    iba

---
+++ shop/achieve/HSC_24/FMT/IBA/assets/info.js
@@ -0,0 +1,7 @@
+const product = "AchieveIBA";
+const productName = "Achieve IBA + BUP Final Mock Test";
+const productCode = "355";
+const fix = 5000;
+const pls = 3000;
+const Platform = "Offline";
+const init = 0;
