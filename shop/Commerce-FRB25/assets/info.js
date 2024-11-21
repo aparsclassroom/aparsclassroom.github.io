23ad95708d0631896d33a9e092be5311adfad2c1
Apars Ltd <aparsclassroom@gmail.com>
2024-11-21 12:16:23 +0600

    comfrb25

--- shop/Commerce-FRB25/assets/info.js
+++ shop/Commerce-FRB25/assets/info.js
@@ -1,6 +1,7 @@
-const product = "Commerce-FRB25";
+const product = "CommerceFRB25";
 const productName = "HSC 25 Final Revision Batch for Commerce Students";
 const productCode = "360";
+const productCode2 = "360";
 const fix = 3000;
 const pls = 1000;
 const Platform = "Online";
