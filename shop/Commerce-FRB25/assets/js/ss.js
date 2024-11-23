689906e121a43bed3424512fd27a6b0d9247f821
Apars Ltd <aparsclassroom@gmail.com>
2024-11-23 21:33:38 +0600

    ok

--- shop/Commerce-FRB25/assets/js/ss.js
+++ shop/Commerce-FRB25/assets/js/ss.js
@@ -48,16 +48,22 @@ var requestOptions = {
     method: 'GET',
     redirect: 'follow'
   };
-fetch("https://crm.aparsclassroom.com/api/total-enrolled/FRB25", requestOptions)
-    .then(response => response.json())
-    .then(result => {
-        document.getElementById('enrolled').setAttribute('countTo', result.enrolled[0].totalTransactions + init);
-       // document.getElementById("thumImg").src = thumbImage + (result.enrolled[0].totalTransactions + init);
-        const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
-        if (!countUp.error) {
-            countUp.start();
-        } else {
-            console.error(countUp.error);
-        }
-    })
-    .catch(error => console.log('error', error));
+  fetch(`https://${shopName2}/enrollment?productCode=${productCode}`)
+  .then((res) => {
+      return res.json()
+  })
+  .then((data) => {
+      document.getElementById('enrolled').setAttribute('countTo', data.count + init);
+      if (document.getElementById('enrolled')) {
+          const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
+          if (!countUp.error) {
+              countUp.start();
+          } else {
+              console.error(countUp.error);
+          }
+      }
+
+  })
+  .catch((err) => {
+      console.log(err)
+  })
