b17becee3e5b4b24b3b0978492b5542c70344801
Apars Ltd <rmahmud578@gmail.com>
2025-01-22 12:05:25 +0600

    medi second timer prebooking added

---
+++ shop/admission/HSC_24/MedicalSecondTimer/assets/info.js
@@ -0,0 +1,7 @@
+const product = "Medical24";
+const productName = "ACS Medical Admission Second Timer Private Batch Prebooking 2025";
+const productCode = "291";
+const fix = 6000;
+const pls = 5000;
+const Platform = "Online";
+const init = 0;
