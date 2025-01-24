36d01573600d13b8bcdc1c5a9e4acbc522a1c992
Apars Ltd <rmahmud578@gmail.com>
2025-01-25 01:48:46 +0600

    japanese course prebooking added

---
+++ shop/Japanese/assets/info.js
@@ -0,0 +1,8 @@
+const product = "FRB25";
+const productName = "Japanese Language Course - N5 Level";
+const productCode = "350";
+const productCode2 = "351";
+const fix = 5000;
+const pls = 2250;
+const Platform = "Online";
+const init = 0;
