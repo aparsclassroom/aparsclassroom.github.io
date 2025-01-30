86995395fc1a231a61686fc88fa4ff6222f7a0a4
Apars Ltd <rmahmud578@gmail.com>
2025-01-30 21:22:27 +0600

    engg 25 prebooking start

---
+++ shop/admission/HSC_25/Engineering/assets/info.js
@@ -0,0 +1,7 @@
+const product = "Engineering24";
+const productName = "ACS Engineering Admission Private Batch 2025";
+const productCode = "293";
+const fix = 10000;
+const pls = 5000;
+const Platform = "Online";
+const init = 0;
