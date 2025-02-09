afcad228b0ef057adf221f7e62c8103c2b216984
Apars Ltd <rmahmud578@gmail.com>
2025-02-09 20:36:25 +0600

    agri prebooking edit

--- shop/admission/HSC_24/Agri/assets/info.js
+++ shop/admission/HSC_24/Agri/assets/info.js
@@ -1,5 +1,5 @@
 const product = "Medical25";
-const productName = "Physics hunters - ACS Agri Batch 2025";
+const productName = "Physics Hunters - ACS Agri Batch 2025";
 const productCode = "366";
 const fix = 8000;
 const pls = 5000;
