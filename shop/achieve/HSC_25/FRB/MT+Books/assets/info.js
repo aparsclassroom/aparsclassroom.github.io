2130421485919c0755d8869597122a440aaf7efa
Apars Ltd <rmahmud578@gmail.com>
2025-02-12 13:01:09 +0600

    Achieve FRB Price increase

--- shop/achieve/HSC_25/FRB/MT+Books/assets/info.js
+++ shop/achieve/HSC_25/FRB/MT+Books/assets/info.js
@@ -2,6 +2,6 @@ const product = "AchieveFRBwithBooks";
 const productName = "Achieve Final Revision Exam Batch with ACS Compact Suggestion Books";
 const productCode = "359";
 const fix = 10000;
-const pls = 8000;
+const pls = 8500;
 const Platform = "Offline";
 const init = 0;
--- shop/achieve/HSC_25/FRB/MT/assets/info.js
+++ shop/achieve/HSC_25/FRB/MT/assets/info.js
@@ -2,6 +2,6 @@ const product = "AchieveFRB";
 const productName = "ACS Achieve Final Revision Exam Batch";
 const productCode = "358";
 const fix = 8000;
-const pls = 6000;
+const pls = 6500;
 const Platform = "Offline";
 const init = 0;
