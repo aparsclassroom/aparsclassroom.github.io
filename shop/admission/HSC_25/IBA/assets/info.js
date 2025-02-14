3d431ea017d5ee388fe7ad7f9af31cfd2d211f24
Apars Ltd <rmahmud578@gmail.com>
2025-02-14 11:39:59 +0600

    iba25 prebooking start

---
+++ shop/admission/HSC_25/IBA/assets/info.js
@@ -0,0 +1,7 @@
+const product = "iba24";
+const productName = "ACS IBA BBA Admission Private Batch 2025";
+const productCode = "337";
+const fix = 8000;
+const pls = 5000;
+const Platform = "Online";
+const init = 0;
