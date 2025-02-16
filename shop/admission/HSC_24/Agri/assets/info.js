10c0d38d209eba1e4396874b8d20ecebdda86352
Apars Ltd <rmahmud578@gmail.com>
2025-02-16 20:33:13 +0600

    agri24 fix

--- shop/admission/HSC_24/Agri/assets/info.js
+++ shop/admission/HSC_24/Agri/assets/info.js
@@ -1,7 +1,7 @@
 const product = "Agri24";
 const productName = "Physics Hunters - ACS Agri Batch 2025";
 const productCode = "370";
-const fix = 3000;
-const pls = 2250;
+const fix = 2999;
+const pls = 2499;
 const Platform = "Online";
 const init = 0;
