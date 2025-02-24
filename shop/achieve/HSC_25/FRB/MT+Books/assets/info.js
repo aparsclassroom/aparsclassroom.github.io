d705df2ceb025123c5511d843aad17b3c1e0c5c7
Apars Ltd <rmahmud578@gmail.com>
2025-02-24 23:12:24 +0600

    frb offline price updated

--- shop/achieve/HSC_25/FRB/MT+Books/assets/info.js
+++ shop/achieve/HSC_25/FRB/MT+Books/assets/info.js
@@ -1,7 +1,7 @@
 const product = "AchieveFRBwithBooks";
 const productName = "Achieve Final Revision Exam Batch with ACS Compact Suggestion Books";
 const productCode = "359";
-const fix = 10000;
-const pls = 8500;
+const fix = 12000;
+const pls = 10000;
 const Platform = "Offline";
 const init = 0;
--- shop/achieve/HSC_25/FRB/MT/assets/info.js
+++ shop/achieve/HSC_25/FRB/MT/assets/info.js
@@ -1,7 +1,7 @@
 const product = "AchieveFRB";
 const productName = "ACS Achieve Final Revision Exam Batch";
 const productCode = "358";
-const fix = 8000;
-const pls = 6500;
+const fix = 10000;
+const pls = 8000;
 const Platform = "Offline";
 const init = 0;
