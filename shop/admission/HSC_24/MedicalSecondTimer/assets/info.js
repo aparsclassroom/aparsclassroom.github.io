4bfe4d11186cb2549a47a53ccb191d1b82f1db9e
Apars Ltd <rmahmud578@gmail.com>
2025-03-01 22:17:36 +0600

    Onushilon course update

--- shop/admission/HSC_24/MedicalSecondTimer/assets/info.js
+++ shop/admission/HSC_24/MedicalSecondTimer/assets/info.js
@@ -1,7 +1,7 @@
-const product = "Medical24";
-const productName = "ACS Medical Admission Second Timer Private Batch Prebooking 2025";
-const productCode = "291";
-const fix = 6000;
-const pls = 5000;
+const product = "MedicalSecondTimer25";
+const productName = "ACS Medical Admission Second Timer Private Batch 2025";
+const productCode = "372";
+const fix = 8000;
+const pls = 5500;
 const Platform = "Online";
 const init = 0;
