53e1f79ccab8132e91ae5dcc11c52f0fa5b7d457
Apars Ltd <rmahmud578@gmail.com>
2025-03-04 05:14:01 +0600

    recursive course added

---
+++ shop/emon/recursionv3/assets/info.js
@@ -0,0 +1,7 @@
+const product = "recursionv3";
+const productName = "Engineering Academic C & C++ Course";
+const productCode = "375";
+const fix = 1500;
+const pls = 1000;
+const Platform = "Online";
+const init = 0;
