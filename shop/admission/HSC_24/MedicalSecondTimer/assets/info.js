8f232064aa8d40a75239e8c681a0c23be10b7fe0
Apars Ltd <rmahmud578@gmail.com>
2025-03-04 17:05:14 +0600

    medical second timer price update

--- shop/admission/HSC_24/MedicalSecondTimer/assets/info.js
+++ shop/admission/HSC_24/MedicalSecondTimer/assets/info.js
@@ -2,6 +2,6 @@ const product = "MedicalSecondTimer25";
 const productName = "ACS Medical Admission Second Timer Private Batch 2025";
 const productCode = "372";
 const fix = 8000;
-const pls = 5500;
+const pls = 5000;
 const Platform = "Online";
 const init = 0;
