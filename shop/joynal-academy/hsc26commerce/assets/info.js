380e79fb53d1e3b85391906040b58f338e6480f4
Apars Ltd <rmahmud578@gmail.com>
2025-03-11 17:12:46 +0600

    joynal bhai course updated

---
+++ shop/joynal-academy/hsc26commerce/assets/info.js
@@ -0,0 +1,7 @@
+const product = "hsc26commerce";
+const productName = "HSC 26 (Arts & Commerce) Academic Course";
+const productCode = "379";
+const fix = 2000;
+const pls = 1750;
+const Platform = "Online";
+const init = 0;
