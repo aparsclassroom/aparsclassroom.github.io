ab60e9856a0d2fbdba2ae3cb1e9bf2e09ae230f5
Apars Ltd <rmahmud578@gmail.com>
2025-03-11 22:18:49 +0600

    vsb added

---
+++ shop/vsb/vsb1st/assets/info.js
@@ -0,0 +1,7 @@
+const product = "vsb1st";
+const productName = " Math 1st Paper , Ketab Uddin sir";
+const productCode = "377";
+const fix = 1025;
+const pls = 519;
+const Platform = "Online";
+const init = 0;
---
+++ shop/vsb/vsb2nd/assets/info.js
@@ -0,0 +1,7 @@
+const product = "vsb";
+const productName = "Math 2nd Paper , Ketab Uddin sir";
+const productCode = "378";
+const fix = 1025;
+const pls = 519;
+const Platform = "Online";
+const init = 0;
