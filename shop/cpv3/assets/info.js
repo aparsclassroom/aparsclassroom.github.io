94f166ee4b48b01a0d02d5f864b1e4f5389660ee
Apars Ltd <rmahmud578@gmail.com>
2025-03-14 00:58:45 +0600

    cpv3 prebooking

---
+++ shop/cpv3/assets/info.js
@@ -0,0 +1,7 @@
+const product = "cpv3";
+const productName = "Competitive Programming 3.0 TurboCharged";
+const productCode = "380";
+const fix = 6500;
+const pls = 4500;
+const Platform = "Online";
+const init = 0;
