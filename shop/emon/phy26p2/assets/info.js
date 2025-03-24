681621b8f721c97875ee48be4c1193857698d403
Samiha Tasnim <samiha@aparsclassroom.com>
2025-03-24 11:24:36 +0600

    remove phy26p1 and p2

--- shop/emon/phy26p2/assets/info.js
+++
@@ -1,7 +0,0 @@
-const product = "phy26p2";
-const productName = "HSC 26 পদার্থবিজ্ঞান ২য় পত্র কমপ্লিট কোর্স";
-const productCode = "365";
-const fix = 5000;
-const pls = 1750;
-const Platform = "Online";
-const init = 0;
