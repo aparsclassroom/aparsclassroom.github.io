fc077e70cedf980b9df73fb659be7dffa0297920
Samiha Tasnim <samiha@aparsclassroom.com>
2025-03-26 21:34:50 +0600

    change price of agri

--- shop/admission/HSC_24/Agri/assets/info.js
+++ shop/admission/HSC_24/Agri/assets/info.js
@@ -2,6 +2,6 @@ const product = "Agri24";
 const productName = "Physics Hunters - ACS Agri Batch 2025";
 const productCode = "370";
 const fix = 2999;
-const pls = 2499;
+const pls = 1250;
 const Platform = "Online";
 const init = 0;
