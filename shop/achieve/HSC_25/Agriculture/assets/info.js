646eae93dfbdb078e2a3177782632b639374380c
Samiha Tasnim <samiha@aparsclassroom.com>
2025-03-28 14:43:42 +0600

    agri exam batch price fix

--- shop/achieve/HSC_25/Agriculture/assets/info.js
+++ shop/achieve/HSC_25/Agriculture/assets/info.js
@@ -2,6 +2,6 @@ const product = "AchieveAgriculture25";
 const productName = "Physics hunters Agri Exam Batch 2025";
 const productCode = "381";
 const fix = 2000;
-const pls = 1500;
+const pls = 999;
 const Platform = "Offline";
 const init = 0;
