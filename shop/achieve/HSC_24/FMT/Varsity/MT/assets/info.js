dfe52ebe70af44727405b26c8cc46e50befeb2df
Samiha <samxtasnim@gmail.com>
2025-04-08 23:30:21 +0600

    acheive24varsity fmt title change

--- shop/achieve/HSC_24/FMT/Varsity/MT/assets/info.js
+++ shop/achieve/HSC_24/FMT/Varsity/MT/assets/info.js
@@ -1,5 +1,5 @@
 const product = "AchieveVarsity24FMT";
-const productName = "Achieve Varsity Final Model Test";
+const productName = "Achieve GST Final Model Test";
 const productCode = "352";
 const fix = 10000;
 const pls = 2000;
