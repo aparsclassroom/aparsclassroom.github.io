f65cc2f153fa6074000f699ab331531e66f68865
Samiha Tasnim <samiha@aparsclassroom.com>
2025-04-15 15:32:12 +0600

    frb25fmt total count add

--- shop/achieve/HSC_25/FRB/FMT/assets/ss.js
+++ shop/achieve/HSC_25/FRB/FMT/assets/ss.js
@@ -33,22 +33,52 @@ if (screen.width <= 600) {
 //     event.target.setVolume(100);
 //     event.target.playVideo();
 // }
-fetch(`https://${shopName2}/enrollment?productCode=${productCode}`)
-    .then((res) => {
-        return res.json()
-    })
-    .then((data) => {
-        document.getElementById('enrolled').setAttribute('countTo', data.count + init);
-        if (document.getElementById('enrolled')) {
-            const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
+// fetch(`https://${shopName2}/enrollment?productCode=${productCode}`)
+//     .then((res) => {
+//         return res.json()
+//     })
+//     .then((data) => {
+//         document.getElementById('enrolled').setAttribute('countTo', data.count + init);
+//         if (document.getElementById('enrolled')) {
+//             const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
+//             if (!countUp.error) {
+//                 countUp.start();
+//             } else {
+//                 console.error(countUp.error);
+//             }
+//         }
+
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     })
+
+const productCodes = ['382', '358', '359']; 
+const sum = 0;
+
+const fetchEnrollment = async (code) => {
+    try {
+        const res = await fetch(`https://${shopName2}/enrollment?productCode=${code}`);
+        const data = await res.json();
+        return data.count;
+    } catch (err) {
+        console.error(`Error fetching productCode ${code}:`, err);
+        return 0;
+    }
+};
+
+Promise.all(productCodes.map(fetchEnrollment))
+    .then(counts => {
+        const totalCount = counts.reduce((sum, count) => sum + count, 0) + sum;
+
+        const enrolledElement = document.getElementById('enrolled');
+        if (enrolledElement) {
+            enrolledElement.setAttribute('countTo', totalCount);
+            const countUp = new CountUp('enrolled', totalCount);
             if (!countUp.error) {
                 countUp.start();
             } else {
                 console.error(countUp.error);
             }
         }
-
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+    });
