49aad26ecce05f53c7a7accff60b0f0df1b28a3b
Samiha Tasnim <samiha@aparsclassroom.com>
2025-04-15 14:31:38 +0600

    achieve25frbfmt launch

---
+++ shop/achieve/HSC_25/FRB/FMT/assets/info.js
@@ -0,0 +1,7 @@
+const product = "AchieveFrb25FMT";
+const productName = "ACS Achieve FRB Final Model Test 2025";
+const productCode = "382";
+const fix = 3000;
+const pls = 2500;
+const Platform = "Offline";
+const init = 0;
