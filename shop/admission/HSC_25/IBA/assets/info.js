17af285016a04e451f4a938957c936f32cac2073
Samiha <samxtasnim@gmail.com>
2025-04-17 00:00:57 +0600

    iba25 launch

--- shop/admission/HSC_25/IBA/assets/info.js
+++ shop/admission/HSC_25/IBA/assets/info.js
@@ -1,7 +1,7 @@
-const product = "iba24";
+const product = "iba25";
 const productName = "ACS IBA BBA Admission Private Batch 2025";
-const productCode = "337";
+const productCode = "383";
 const fix = 8000;
-const pls = 5000;
+const pls = 6000;
 const Platform = "Online";
 const init = 0;
--- shop/admission/HSC_25/IBA/assets/ss.js
+++ shop/admission/HSC_25/IBA/assets/ss.js
@@ -17,44 +17,44 @@ if (screen.width <= 600) {
 }
 var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-var player;
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// var player;
 
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'KTqnAXRMoOQ',
-        playerVars: { 'autoplay': 1, 'playsinline': 1 },
-        events: {
-            'onReady': onPlayerReady
-        }
-    });
-}
-
-function onPlayerReady(event) {
-    document.getElementById('thumb').style.display = "none";
-    event.target.setVolume(100);
-    event.target.playVideo();
-}
-// fetch(`https://${shopName2}/enrollment?productCode=${productCode}`)
-//     .then((res) => {
-//         return res.json()
-//     })
-//     .then((data) => {
-//         document.getElementById('enrolled').setAttribute('countTo', data.count + init);
-//         if (document.getElementById('enrolled')) {
-//             const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
-//             if (!countUp.error) {
-//                 countUp.start();
-//             } else {
-//                 console.error(countUp.error);
-//             }
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         height: '390',
+//         width: '640',
+//         videoId: 'KTqnAXRMoOQ',
+//         playerVars: { 'autoplay': 1, 'playsinline': 1 },
+//         events: {
+//             'onReady': onPlayerReady
 //         }
+//     });
+// }
 
-//     })
-//     .catch((err) => {
-//         console.log(err)
-//     })
+// function onPlayerReady(event) {
+//     document.getElementById('thumb').style.display = "none";
+//     event.target.setVolume(100);
+//     event.target.playVideo();
+// }
+fetch(`https://${shopName2}/enrollment?productCode=${productCode}`)
+    .then((res) => {
+        return res.json()
+    })
+    .then((data) => {
+        document.getElementById('enrolled').setAttribute('countTo', data.count + init);
+        if (document.getElementById('enrolled')) {
+            const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
+            if (!countUp.error) {
+                countUp.start();
+            } else {
+                console.error(countUp.error);
+            }
+        }
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
