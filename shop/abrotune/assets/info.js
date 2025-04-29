5086d5a0196c44eed451044aae3d1b192536401f
Samiha <samxtasnim@gmail.com>
2025-04-29 22:11:06 +0600

    abrotune prebooking live

---
+++ shop/abrotune/assets/info.js
@@ -0,0 +1,7 @@
+const product = "abrotune";
+const productName = "Abrotune DSAT Master Program with USA Application Pre-Cohort";
+const productCode = "407";
+const fix = 10000;
+const pls = 4000;
+const Platform = "Online";
+const init = 0;
