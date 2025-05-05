32c60b8e1254b55396aa841f4bb16d6ca08ca61c
Samiha Tasnim <samiha@aparsclassroom.com>
2025-05-05 13:01:46 +0600

    abrotune live

--- shop/abrotune/assets/info.js
+++ shop/abrotune/assets/info.js
@@ -1,7 +1,7 @@
 const product = "abrotune";
 const productName = "Abrotune DSAT Master Program with USA Application Pre-Cohort";
 const productCode = "407";
-const fix = 10000;
-const pls = 4000;
+const fix = 12500;
+const pls = 5000;
 const Platform = "Online";
 const init = 0;
