6187ec8a1bb7b65a8a1def47cda7081a82b82ea5
Samiha Tasnim <samiha@aparsclassroom.com>
2025-05-08 19:14:39 +0600

    oporichita26 live

---
+++ shop/oporichita26/assets/info.js
@@ -0,0 +1,7 @@
+const product = "oporichita26";
+const productName = "অপরিচিতা ২৬ ব্যাচ";
+const productCode = "413";
+const fix = 1500;
+const pls = 999;
+const Platform = "Online";
+const init = 0;
