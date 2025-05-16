eb42aee60f9fc31371238b3c46644986dc8a9724
Samiha Tasnim <samiha@aparsclassroom.com>
2025-05-16 15:45:36 +0600

    live restart25

---
+++ shop/joynal-academy/restart25/assets/info.js
@@ -0,0 +1,7 @@
+const product = "restart25";
+const productName = "2nd Timer Restart Batch";
+const productCode = "426";
+const fix = 5000;
+const pls = 3000;
+const Platform = "Online";
+const init = 0;
