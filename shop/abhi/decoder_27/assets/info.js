065e8e8a9ddc30d81abc19e22ac63b4bb435530e
Samiha Tasnim <samiha@aparsclassroom.com>
2025-05-19 16:53:54 +0600

    decoder edits

--- shop/abhi/decoder_27/assets/info.js
+++ shop/abhi/decoder_27/assets/info.js
@@ -2,6 +2,6 @@ const product = "decoder_27";
 const productName = "ACS ICT BATCH DECODER HSC 27";
 const productCode = "470";
 const fix = 2000;
-const pls = 1250;
+const pls = 1500;
 const Platform = "Online";
 const init = 0;
