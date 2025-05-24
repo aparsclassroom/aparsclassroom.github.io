8ff67828324fe158b9449b5ae89ef184dbfc9e54
Samiha Tasnim <samiha@aparsclassroom.com>
2025-05-24 18:41:16 +0600

    achieve2ndtimer course launch

---
+++ shop/achieve/HSC_24/Medical2ndTimer/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achieve2ndtimemedi25";
+const productName = "Achieve Medical 2nd Time Exam Batch 25";
+const productCode = "497";
+const fix = 10000;
+const pls = 7000;
+const Platform = "Offline";
+const init = 0;
