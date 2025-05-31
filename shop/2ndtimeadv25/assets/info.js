7bffd05e3a5f66aef2cd2f11262665a8838732e9
Samiha Tasnim <samiha@aparsclassroom.com>
2025-05-31 18:37:20 +0600

    edits and 2ndtimeadv25 live

---
+++ shop/2ndtimeadv25/assets/info.js
@@ -0,0 +1,7 @@
+const product = "2ndtimeadv25";
+const productName = "2nd Time মানবিক+বিভাগ পরিবর্তন Advanced Batch - 2025";
+const productCode = "520";
+const fix = 6000;
+const pls = 5000;
+const Platform = "Online";
+const init = 0;
--- shop/abhi/decoder_26/assets/info.js
+++ shop/abhi/decoder_26/assets/info.js
@@ -1,7 +1,7 @@
 const product = "decoder_26";
 const productName = "ACS ICT Batch Decoder 2026";
 const productCode = "303";
-const fix = 1500;
-const pls = 1250;
+const fix = 2000;
+const pls = 1500;
 const Platform = "Online";
 const init = 0;
