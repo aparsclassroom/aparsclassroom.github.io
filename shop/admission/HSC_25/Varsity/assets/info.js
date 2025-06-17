33d439d3ff1c89d11d9378ace6b53b35455e0e63
Samiha Tasnim <samiha@aparsclassroom.com>
2025-06-17 19:19:13 +0600

    fix varsity25 enrollment count include special

--- shop/admission/HSC_25/Varsity/assets/info.js
+++ shop/admission/HSC_25/Varsity/assets/info.js
@@ -1,6 +1,7 @@
 const product = "varsity25";
 const productName = "ACS Varsity + GST Special Private Programme 2025";
 const productCode = "525";
+const productCode2 = "420";
 const fix = 10000;
 const pls = 5000;
 const Platform = "Online";
