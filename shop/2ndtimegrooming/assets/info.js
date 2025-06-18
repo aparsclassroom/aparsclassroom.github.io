b5b8512998776e8ce4592220c73cd5c4a525c011
Samiha Tasnim <samiha@aparsclassroom.com>
2025-06-18 16:34:33 +0600

    2ndtimegrooming launch

---
+++ shop/2ndtimegrooming/assets/info.js
@@ -0,0 +1,7 @@
+const product = "2ndtimegrooming";
+const productName = "ACS 2nd Time Online Grooming Hub";
+const productCode = "527";
+const fix = 0;
+const pls = 0;
+const Platform = "Online";
+const init = 0;
