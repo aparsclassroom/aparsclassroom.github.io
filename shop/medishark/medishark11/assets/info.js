08ee797c966206fdc057542aa7e61bad011f10b8
Samiha Tasnim <samiha@aparsclassroom.com>
2025-06-21 14:10:24 +0600

    medishark11 launch

---
+++ shop/medishark/medishark11/assets/info.js
@@ -0,0 +1,7 @@
+const product = "medishark11";
+const productName = "1st Year 1st Term Combo";
+const productCode = "526";
+const fix = 4000;
+const pls = 3000;
+const Platform = "Online";
+const init = 0;
