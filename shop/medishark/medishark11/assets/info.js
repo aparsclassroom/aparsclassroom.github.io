7ef55ff22b957df0c67c497540b5c83ab1a1fe40
Samiha Tasnim <samiha@aparsclassroom.com>
2025-06-21 14:36:14 +0600

    edit name of medishark course

--- shop/medishark/medishark11/assets/info.js
+++ shop/medishark/medishark11/assets/info.js
@@ -1,5 +1,5 @@
 const product = "medishark11";
-const productName = "1st Year 1st Term Combo";
+const productName = "Medical 1st Year 1st Term Combo";
 const productCode = "526";
 const fix = 4000;
 const pls = 3000;
