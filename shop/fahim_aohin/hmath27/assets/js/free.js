22fd296edae926979a837b00c2e63cbe18498098
samiha012 <samiha@aparsclassroom.com>
2025-06-25 18:37:38 +0600

    update cycle 0 to 1

--- shop/fahim_aohin/hmath27/assets/js/free.js
+++ shop/fahim_aohin/hmath27/assets/js/free.js
@@ -5,7 +5,7 @@ firebase.auth().onAuthStateChanged(function(e) {
     if (e) {
         document.getElementById('prod').innerHTML = `${productName}<br>(Free Phase)`;
         document.getElementById('moda').addEventListener('click', () => {
-            location.href =  'https://hmath27.aparsclassroom.com/b2b24/Cycle-0'
+            location.href =  'https://hmath27.aparsclassroom.com/b2b24/Cycle-1'
         });
         document.getElementById('moda').innerHTML = 'ক্লাসে জয়েন করো <i class="fas fa-arrow-right"></i>';
     } else {
