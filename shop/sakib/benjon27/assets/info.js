d07636516e38306a7086c8fb044babe8660ad0ca
samiha012 <samiha@aparsclassroom.com>
2025-06-28 23:41:52 +0600

    benjon27 files ready

---
+++ shop/sakib/benjon27/assets/info.js
@@ -0,0 +1,7 @@
+const product = "benjon27";
+const productName = "অনুসর্গের ব্যঞ্জন-২৭";
+const productCode = "528";
+const fix = 2000;
+const pls = 1200;
+const Platform = "Online";
+const init = 0;
