74f38b41aa9735193a14fc150653aed9d667c37f
samiha012 <samiha@aparsclassroom.com>
2025-07-13 19:08:45 +0600

    launch varsity chemistry 25

---
+++ shop/sakib/varsitychem25/assets/info.js
@@ -0,0 +1,7 @@
+const product = "varsitychem25";
+const productName = "ACS Varsity Chemistry Private Batch 2025";
+const productCode = "529";
+const fix = 1500;
+const pls = 1000;
+const Platform = "Online";
+const init = 0;
