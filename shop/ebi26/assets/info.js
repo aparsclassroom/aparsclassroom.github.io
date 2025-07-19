39b12e0bf30640fbe539a823787608d846d07830
Samiha Tasnim <samiha@aparsclassroom.com>
2025-07-19 17:12:23 +0600

    ebi26 live

---
+++ shop/ebi26/assets/info.js
@@ -0,0 +1,7 @@
+const product = "ebi26";
+const productName = "EBI By PMS (English + Bangla + ICT)";
+const productCode = "531";
+const fix = 2299;
+const pls = 1499;
+const Platform = "Online";
+const init = 0;
