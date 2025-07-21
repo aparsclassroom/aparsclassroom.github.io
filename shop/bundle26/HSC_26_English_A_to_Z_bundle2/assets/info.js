245a59f3ff8712d1c3c066ba105c1e8eec377b9c
Samiha Tasnim <samiha@aparsclassroom.com>
2025-07-21 13:22:22 +0600

    hsc26 reza english bundle launch

---
+++ shop/bundle26/HSC_26_English_A_to_Z_bundle2/assets/info.js
@@ -0,0 +1,7 @@
+const product = "HSC_26_English_A_to_Z_bundle2";
+const productName = "HSC 26 English A to Z Bundle 02";
+const productCode = "533";
+const fix = 1600;
+const pls = 1450;
+const Platform = "Online";
+const init = 0;
