44cebd6a7cd0eb71043ada376489ccb38edd21fb
Samiha Tasnim <samiha@aparsclassroom.com>
2025-07-23 15:59:29 +0600

    goalhackers25 course launch

---
+++ shop/crowning/goalhackers25/assets/info.js
@@ -0,0 +1,7 @@
+const product = "goalhackers25";
+const productName = "GOALHACKERS (VARSITY + ENGINEERING English Private Batch)";
+const productCode = "534";
+const fix = 1500;
+const pls = 800;
+const Platform = "Online";
+const init = 0;
