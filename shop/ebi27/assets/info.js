97e1e3483904ba607f6b0d11e5beb1f63a78bdd3
Samiha Tasnim <samiha@aparsclassroom.com>
2025-07-28 20:18:53 +0600

    ebi27 launch

---
+++ shop/ebi27/assets/info.js
@@ -0,0 +1,7 @@
+const product = "ebi27";
+const productName = "EBI By PMS 2.0 ( English + Bangla + ICT)";
+const productCode = "537";
+const fix = 2799;
+const pls = 1899;
+const Platform = "Online";
+const init = 0;
