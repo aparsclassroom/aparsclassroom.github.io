f9213a948264bacb600811b46b9774ce6e8de0c0
samiha012 <samiha@aparsclassroom.com>
2025-08-06 22:28:42 +0600

    varsityphy25 launch

---
+++ shop/physics/varsityphy25/assets/info.js
@@ -0,0 +1,7 @@
+const product = "varsityphy25";
+const productName = "ACS Varsity Physics 2025";
+const productCode = "540";
+const fix = 2000;
+const pls = 1500;
+const Platform = "Online";
+const init = 0;
