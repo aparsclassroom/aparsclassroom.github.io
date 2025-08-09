a1cb067071a11f251feff4a060a73405708986a2
Samiha Tasnim <samiha@aparsclassroom.com>
2025-08-09 19:29:05 +0600

    varsitybangla25 launch

---
+++ shop/admission/HSC_25/varsitybangla25/assets/info.js
@@ -0,0 +1,7 @@
+const product = "varsitybangla25";
+const productName = "ACS ভার্সিটি বাংলা প্রাইভেট ব্যাচ-২০২৫	";
+const productCode = "546";
+const fix = 2000;
+const pls = 1000;
+const Platform = "Online";
+const init = 0;
