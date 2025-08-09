39c8f48665d9cab4f3249d1d720a738e22a1a2f5
Samiha Tasnim <samiha@aparsclassroom.com>
2025-08-09 18:51:22 +0600

    varsitybio25 launch

---
+++ shop/admission/HSC_25/varsitybio25/assets/info.js
@@ -0,0 +1,7 @@
+const product = "varsitybio25";
+const productName = "ACS Varsity Biology Private Batch 2025";
+const productCode = "545";
+const fix = 2000;
+const pls = 1000;
+const Platform = "Online";
+const init = 0;
