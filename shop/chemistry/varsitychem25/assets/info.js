a092525a8f04fc212593826b7a8760b5d2cd9d23
Samiha Tasnim <samiha@aparsclassroom.com>
2025-08-11 14:27:08 +0600

    himel varsitychem title edit

--- shop/chemistry/varsitychem25/assets/info.js
+++ shop/chemistry/varsitychem25/assets/info.js
@@ -1,5 +1,5 @@
 const product = "varsitychem25";
-const productName = "ACS Varsity Chemistry 2025";
+const productName = "ACS Varsity + GST Chemistry Private Batch 2025";
 const productCode = "541";
 const fix = 2000;
 const pls = 1000;
