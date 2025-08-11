d9c6def0a47a0a01caba5ff32358e880a8b551f2
Samiha Tasnim <samiha@aparsclassroom.com>
2025-08-11 13:50:54 +0600

    varsity organic 25 launch

---
+++ shop/admission/HSC_25/varsityorganic25/assets/info.js
@@ -0,0 +1,7 @@
+const product = "varsityorganic25";
+const productName = "ACS Varsity Organic Chemistry Private Batch 2025";
+const productCode = "548";
+const fix = 500;
+const pls = 399;
+const Platform = "Online";
+const init = 0;
