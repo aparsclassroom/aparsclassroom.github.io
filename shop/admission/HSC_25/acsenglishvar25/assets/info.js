497144acce0b7ff6511f4a447c07027aab60cfc5
samiha012 <samiha@aparsclassroom.com>
2025-08-13 10:56:07 +0600

    soe english course live

---
+++ shop/admission/HSC_25/acsenglishvar25/assets/info.js
@@ -0,0 +1,7 @@
+const product = "acsenglishvar25";
+const productName = "ACS-SoE Varsity, Medical, Engineering English Private Batch";
+const productCode = "549";
+const fix = 2000;
+const pls = 1000;
+const Platform = "Online";
+const init = 0;
