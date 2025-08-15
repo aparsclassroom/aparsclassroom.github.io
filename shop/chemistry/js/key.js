2bf2ae3d0d80b21c54cf7d45f445aacbc0e878a6
samiha012 <samiha@aparsclassroom.com>
2025-08-15 22:46:32 +0600

    remove himel varsity chemistry

--- shop/chemistry/js/key.js
+++
@@ -1,3 +0,0 @@
-const shopName = "shop.aparsclassroom.com";
-const shopName2 = "shop.aparsclassroom.com";
-const cuponApi = "https://shop.aparsclassroom.com/v1/Coupon/check";
--- shop/chemistry/varsitychem25/assets/info.js
+++
@@ -1,7 +0,0 @@
-const product = "varsitychem25";
-const productName = "ACS Varsity + GST Chemistry Private Batch 2025";
-const productCode = "541";
-const fix = 2000;
-const pls = 1000;
-const Platform = "Online";
-const init = 0;
