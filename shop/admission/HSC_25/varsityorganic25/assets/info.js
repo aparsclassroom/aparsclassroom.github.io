e227593f982bb6d4dc22ae292e1bfe00e00995a9
samiha012 <samiha@aparsclassroom.com>
2025-08-16 15:30:42 +0600

    remove sanjay da varsity organic course

--- shop/admission/HSC_25/varsityorganic25/assets/info.js
+++
@@ -1,7 +0,0 @@
-const product = "varsityorganic25";
-const productName = "ACS Varsity Organic Chemistry Private Batch 2025";
-const productCode = "548";
-const fix = 500;
-const pls = 399;
-const Platform = "Online";
-const init = 0;
