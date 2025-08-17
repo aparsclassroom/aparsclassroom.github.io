b8dc3d306b6bb2d0d3ed601e2506b9c8c534064e
samiha012 <samiha@aparsclassroom.com>
2025-08-17 23:11:54 +0600

    remove omor admission english course

--- shop/admission/HSC_25/AdmissionEng25/assets/info.js
+++
@@ -1,7 +0,0 @@
-const product = "admissionenglish25";
-const productName = "ACS Varsity, Medical, Engineering Admission English Private Batch 2025";
-const productCode = "536";
-const fix = 2000;
-const pls = 999;
-const Platform = "Online";
-const init = 0;
