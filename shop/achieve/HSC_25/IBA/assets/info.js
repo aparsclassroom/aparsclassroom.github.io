172d375fc6f4a322ddd0bbaf918d21ecfa29e593
Samiha Tasnim <samiha@aparsclassroom.com>
2025-08-18 11:20:25 +0600

    achieveiba25 launch

---
+++ shop/achieve/HSC_25/IBA/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achieveIBA25";
+const productName = "ACS IBA + BUP Full Season Mock Exam Batch";
+const productCode = "550";
+const fix = 8000;
+const pls = 7000;
+const Platform = "Offline";
+const init = 0;
