2cf8e77714aaa03af1b952f3ba0ff92a841fd5f6
samiha012 <samiha@aparsclassroom.com>
2025-08-20 18:47:48 +0600

    unitchange25 launch

---
+++ shop/joynal-academy/unitchange25/assets/info.js
@@ -0,0 +1,7 @@
+const product = "unitchange25";
+const productName = "Unit Change (Varsity + GST) Admission Program 2025";
+const productCode = "551";
+const fix = 5000;
+const pls = 3000;
+const Platform = "Online";
+const init = 0;
---
+++ shop/joynal-academy/unitchange25/assets/ss.js
@@ -0,0 +1,60 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 5,
+    loop: false,
+    autoplay: {
+        delay: 4000,
+    },
+});
+
+const vidD = document.getElementById('video');
+const clprc = document.getElementById('clprc');
+if (screen.width <= 600) {
+    clprc.classList.add('fixed-bottom');
+} else {
+    clprc.classList.remove('fixed-bottom');
+    vidD.style.position = 'sticky';
+}
+var tag = document.createElement('script');
+
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// var player;
+
+//  function onYouTubeIframeAPIReady() {
+//      player = new YT.Player('player', {
+//          height: '390',
+//          width: '640',
+//          videoId: 'ROArjvv4ujc',
+//          playerVars: { 'autoplay': 1, 'playsinline': 1 },
+//          events: {
+//              'onReady': onPlayerReady
+//          }
+//      });
+//  }
+
+//  function onPlayerReady(event) {
+//      document.getElementById('thumb').style.display = "none";
+//      event.target.setVolume(100);
+//      event.target.playVideo();
+//  }
+fetch(`https://${shopName2}/enrollment/?productCode=${productCode}`)
+    .then((res) => {
+        return res.json()
+    })
+    .then((data) => {
+        document.getElementById('enrolled').setAttribute('countTo', data.count + init);
+        if (document.getElementById('enrolled')) {
+            const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
+            if (!countUp.error) {
+                countUp.start();
+            } else {
+                console.error(countUp.error);
+            }
+        }
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
