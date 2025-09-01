1296f334a612e5a310b45095f86042c4f73c5a6c
Samiha Tasnim <samiha@aparsclassroom.com>
2025-09-01 16:25:40 +0600

    remove varsity25 and add telesales special varsity course

--- shop/admission/HSC_25/Varsity/assets/info.js
+++
@@ -1,8 +0,0 @@
-const product = "varsity25";
-const productName = "ACS Varsity + GST Special Private Programme 2025";
-const productCode = "525";
-const productCode2 = "420";
-const fix = 10000;
-const pls = 5000;
-const Platform = "Online";
-const init = 0;
---
+++ shop/admission/HSC_25/Varsityspecial/assets/info.js
@@ -0,0 +1,8 @@
+const product = "varsity25 (FRB Special)";
+const productName = "ACS Varsity + GST Special Private Programme 2025 (Special)";
+const productCode = "420";
+const productCode2 = "525";
+const fix = 10000;
+const pls = 5000;
+const Platform = "Online";
+const init = 0;
