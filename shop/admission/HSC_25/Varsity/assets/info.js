561d1bd7e44842016cd4c18b40ca5df01f8a28a4
Samiha Tasnim <samiha@aparsclassroom.com>
2025-09-02 11:50:09 +0600

    varsity25 combo launch

---
+++ shop/admission/HSC_25/Varsity/assets/info.js
@@ -0,0 +1,8 @@
+const product = "varsity25combo1";
+const productName = "ACS Varsity + GST Special Private Programme 2025 \n (Physics, Chemistry, Maths & Biology)";
+const productCode = "555";
+const productCode2 = "556";
+const fix = 10000;
+const pls = 4000;
+const Platform = "Online";
+const init = 0;
