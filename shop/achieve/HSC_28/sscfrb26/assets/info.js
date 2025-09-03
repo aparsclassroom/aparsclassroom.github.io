c7010852cbd6edd82bc98b343c14ba60835fd715
Samiha Tasnim <samiha@aparsclassroom.com>
2025-09-03 22:26:36 +0600

    achieve ssc afs course live

---
+++ shop/achieve/HSC_28/sscfrb26/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achievesscfrb26";
+const productName = "SSC 2026 Achieve Offline Model Test";
+const productCode = "559";
+const fix = 10000;
+const pls = 8500;
+const Platform = "Offline";
+const init = 0;
