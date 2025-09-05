2f85c8028c0f4eb937fcfe53fc55ca35148c3ef6
Samiha Tasnim <samiha@aparsclassroom.com>
2025-09-05 16:20:38 +0600

    combo pricing update

--- shop/admission/HSC_25/Varsitycombo/assets/info.js
+++ shop/admission/HSC_25/Varsitycombo/assets/info.js
@@ -3,6 +3,6 @@ const productName = "ACS Varsity + GST Special Private Programme 2025 \n (Physic
 const productCode = "555";
 const productCode2 = "556";
 const fix = 10000;
-const pls = 4000;
+const pls = 5000;
 const Platform = "Online";
 const init = 0;
