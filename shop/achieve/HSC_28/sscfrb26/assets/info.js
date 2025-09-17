fbbb849c503af9d0675f015e0bf8675ffe73c76a
Samiha Tasnim <samiha@aparsclassroom.com>
2025-09-17 18:33:37 +0600

    change sscfrb26 pricing

--- shop/achieve/HSC_28/sscfrb26/assets/info.js
+++ shop/achieve/HSC_28/sscfrb26/assets/info.js
@@ -2,6 +2,6 @@ const product = "achievesscfrb26";
 const productName = "SSC 2026 Achieve Offline Model Test";
 const productCode = "559";
 const fix = 10000;
-const pls = 8500;
+const pls = 9000;
 const Platform = "Offline";
 const init = 0;
