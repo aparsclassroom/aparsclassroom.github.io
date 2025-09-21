4ff5014ad32030a832f3b6971e324fafd7b19a69
samiha012 <samiha@aparsclassroom.com>
2025-09-21 17:28:29 +0600

    sscfrb26 pricing update

--- shop/achieve/HSC_28/sscfrb26/assets/info.js
+++ shop/achieve/HSC_28/sscfrb26/assets/info.js
@@ -1,7 +1,7 @@
 const product = "achievesscfrb26";
 const productName = "SSC 2026 Achieve Offline Model Test";
 const productCode = "559";
-const fix = 10000;
-const pls = 9000;
+const fix = 8000;
+const pls = 7000;
 const Platform = "Offline";
 const init = 0;
