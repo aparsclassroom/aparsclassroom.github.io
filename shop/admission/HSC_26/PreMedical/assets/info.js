425537cebe26480a94b2c271c100cbbf8a035996
Samiha Tasnim <samiha@aparsclassroom.com>
2025-09-22 18:37:00 +0600

    pre medical 26 launch

---
+++ shop/admission/HSC_26/PreMedical/assets/info.js
@@ -0,0 +1,8 @@
+const product = "medical26";
+const productName = "ACS pre-medical batch 2026";
+const productCode = "560";
+//const productCode2 = "369";
+const fix = 5500;
+const pls = 5000;
+const Platform = "Online";
+const init = 0;
