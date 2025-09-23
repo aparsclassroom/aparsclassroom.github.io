635d27bb90a99a6dece403d12ddca91f9c174009
Samiha Tasnim <samiha@aparsclassroom.com>
2025-09-23 17:04:17 +0600

    x factor launch

---
+++ shop/admission/HSC_25/xfactor/assets/info.js
@@ -0,0 +1,8 @@
+const product = "factor25";
+const productName = "Medical X-factor 2025";
+const productCode = "561";
+//const productCode2 = "369";
+const fix = 2000;
+const pls = 1000;
+const Platform = "Online"; 
+const init = 0;
