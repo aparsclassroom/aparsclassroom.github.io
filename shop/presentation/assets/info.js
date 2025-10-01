1ee8312cb2d1e4d61ce882caded9e646ad32378d
Samiha Tasnim <samiha@aparsclassroom.com>
2025-10-01 17:40:13 +0600

    presentation course name change

--- shop/presentation/assets/info.js
+++ shop/presentation/assets/info.js
@@ -1,5 +1,5 @@
 const product = "presentation";
-const productName = "Mustering presentation skills: The real world success";
+const productName = "Mastering presentation skills: The real world success";
 const productCode = "544";
 const fix = 4000;
 const pls = 3500;
