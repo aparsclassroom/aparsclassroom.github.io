9e4de9c7ba5508f012f6360a8256f5f5cb3faec6
Samiha Tasnim <samiha@aparsclassroom.com>
2025-10-06 18:29:30 +0600

    achieve varsity fmt 25 launch

---
+++ shop/achieve/HSC_25/VarsityFMT/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achievevarsityfmt25";
+const productName = "Varsity 2nd Time Final Model Test";
+const productCode = "568";
+const fix = 5000;
+const pls = 3000;
+const Platform = "Offline";
+const init = 0;
