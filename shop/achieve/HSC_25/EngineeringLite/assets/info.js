48b2458d51a5fdc9f1e9b3e8ea5cfa11259d2100
Samiha Tasnim <samiha@apars.shop>
2025-10-09 01:02:36 +0600

    achieve varsity and engineering lite 25 launch

---
+++ shop/achieve/HSC_25/EngineeringLite/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achieveengglite25";
+const productName = "Achieve Engineering lite couse";
+const productCode = "571";
+const fix = 8000;
+const pls = 6500;
+const Platform = "Offline";
+const init = 0;
---
+++ shop/achieve/HSC_25/VarsityLite/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achievevarsitylite25";
+const productName = "Achieve Varsity lite couse";
+const productCode = "570";
+const fix = 8000;
+const pls = 6500;
+const Platform = "Offline";
+const init = 0;
