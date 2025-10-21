b14985a5e979d4dffa1ac2ea3fb7f9bfbc99170d
Samiha Tasnim <samiha@aparsclassroom.com>
2025-10-21 15:54:57 +0600

    medimath25 coupon remove

--- shop/abhi/varsitymath25/assets/info.js
+++ shop/abhi/varsitymath25/assets/info.js
@@ -2,6 +2,6 @@ const product = "medimath25";
 const productName = "ACS Varsity + Medical Higher Math Private Batch 2025";
 const productCode = "530";
 const fix = 2000;
-const pls = 1750;
+const pls = 1250;
 const Platform = "Online";
 const init = 0;
