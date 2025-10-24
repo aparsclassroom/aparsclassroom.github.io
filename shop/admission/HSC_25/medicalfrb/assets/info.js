f8af039eeb463cd28c40e484992feab97c06997f
Samiha Tasnim <samiha@aparsclassroom.com>
2025-10-24 15:26:51 +0600

    medicalfrb25 launch

---
+++ shop/admission/HSC_25/medicalfrb/assets/info.js
@@ -0,0 +1,8 @@
+const product = "medicalfrb25";
+const productName = "Medical Final revision & Exam Batch 2025";
+const productCode = "562";
+//const productCode2 = "369";
+const fix = 2000;
+const pls = 1000;
+const Platform = "Online";
+const init = 0;
