b4a7ec4453ee966f0f7c671e156c8a6afeff9a44
Samiha Tasnim <samiha@apars.shop>
2025-10-24 20:55:12 +0600

    presentation course pricing change

--- shop/presentation/assets/info.js
+++ shop/presentation/assets/info.js
@@ -2,6 +2,6 @@ const product = "presentation";
 const productName = "Mastering presentation skills: The real world success";
 const productCode = "544";
 const fix = 4000;
-const pls = 3500;
+const pls = 1000;
 const Platform = "Online";
 const init = 0;
