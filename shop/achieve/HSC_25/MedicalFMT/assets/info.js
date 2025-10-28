e851be92d5abccfb1d0b031593d86179d764c6ce
Samiha Tasnim <samiha@aparsclassroom.com>
2025-10-28 16:38:02 +0600

    launch achieve medical fmt 25

---
+++ shop/achieve/HSC_25/MedicalFMT/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achievertdmedi25fmt";
+const productName = "Achieve Medical Final model test";
+const productCode = "580";
+const fix = 5000;
+const pls = 3000;
+const Platform = "Offline";
+const init = 0;
