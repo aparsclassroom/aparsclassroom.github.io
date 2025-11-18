531599393805de1d1fa2d6fced74b4af83120c1c
Samiha Tasnim <samiha@aparsclassroom.com>
2025-11-18 19:12:55 +0600

    medicalfrb edits and pricing edit

--- shop/admission/HSC_25/medicalfrb/assets/info.js
+++ shop/admission/HSC_25/medicalfrb/assets/info.js
@@ -1,8 +1,8 @@
 const product = "medicalfrb25";
-const productName = "Medical Final revision & Exam Batch 2025";
+const productName = "Medical-25 Final Model Test Series";
 const productCode = "562";
 //const productCode2 = "369";
-const fix = 2000;
-const pls = 1000;
+const fix = 799;
+const pls = 499;
 const Platform = "Online";
 const init = 0;
