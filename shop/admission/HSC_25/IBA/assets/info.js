3c65ecc86f92861c659da5835bde20a26bf0e316
Samiha Tasnim <samiha@aparsclassroom.com>
2025-12-04 17:27:46 +0600

    iba title edit

--- shop/admission/HSC_25/IBA/assets/info.js
+++ shop/admission/HSC_25/IBA/assets/info.js
@@ -1,5 +1,5 @@
 const product = "iba25";
-const productName = "ACS IBA BBA Admission Private Batch 2025";
+const productName = "ACS IBA+BUP Admission Private Batch 2025";
 const productCode = "383";
 const fix = 8000;
 const pls = 6000;
