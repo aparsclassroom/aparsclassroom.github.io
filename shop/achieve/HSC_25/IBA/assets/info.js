3fd5a9e3b66db014f88dfa954d6a7680181e3860
Samiha Tasnim <samiha@apars.shop>
2025-12-07 15:47:39 +0600

    achieve iba pricing update

--- shop/achieve/HSC_25/IBA/assets/info.js
+++ shop/achieve/HSC_25/IBA/assets/info.js
@@ -1,7 +1,7 @@
 const product = "achieveIBA25";
 const productName = "ACS IBA + BUP Mock Exam Batch";
 const productCode = "550";
-const fix = 4000;
-const pls = 3500;
+const fix = 3500;
+const pls = 1500;
 const Platform = "Offline";
 const init = 0;
