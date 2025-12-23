a5e0745a70af6e8f3bcf178e5dd168de1f1c2ccd
Samiha Tasnim <samiha@aparsclassroom.com>
2025-12-23 13:00:50 +0600

    launch crowning comeback 26 course

---
+++ shop/crowning/crowningcomeback26/assets/info.js
@@ -0,0 +1,7 @@
+const product = "crowningcomeback26";
+const productName = "Crowning Comeback Course (CCC)";
+const productCode = "589";
+const fix = 2000;
+const pls = 800;
+const Platform = "Online";
+const init = 0;
