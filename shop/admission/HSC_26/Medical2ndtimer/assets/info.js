d0838cf9fca4c79e82535b319414f37d4077a316
Samiha Tasnim <samiha@apars.shop>
2026-01-11 19:00:09 +0600

    medi2ndtimer26 live

---
+++ shop/admission/HSC_26/Medical2ndtimer/assets/info.js
@@ -0,0 +1,8 @@
+const product = "medi2ndtimer26";
+const productName = "ACS Second Timers Medical Admission Private Batch 2026";
+const productCode = "593";
+//const productCode2 = "369";
+const fix = 6000;
+const pls = 5500;
+const Platform = "Online";
+const init = 0;
