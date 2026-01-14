24ad196758b00b8f505ba3badd3a9877af7ea4d2
Samiha Tasnim <samiha@aparsclassroom.com>
2026-01-14 17:33:38 +0600

    achievegst25 launch

---
+++ shop/achieve/HSC_25/achievegst25/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achievegst25";
+const productName = "ACHIEVE GST Special Model Test 2025/26";
+const productCode = "594";
+const fix = 3000;
+const pls = 2500;
+const Platform = "Offline";
+const init = 0;
