4fae94e51227038d5a326bf30472c7aa80b11609
Samiha Tasnim <samiha@aparsclassroom.com>
2026-01-17 13:24:02 +0600

    achieve26ebi launch

---
+++ shop/achieve/HSC_26/achievehscEBI26/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achievehscEBI26";
+const productName = "ACHIEVE HSC FRB EBI model test 26";
+const productCode = "595";
+const fix = 2500;
+const pls = 2000;
+const Platform = "Offline";
+const init = 0;
