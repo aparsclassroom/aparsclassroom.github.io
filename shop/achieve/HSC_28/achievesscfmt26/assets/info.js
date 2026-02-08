667860257987e7a525fcbda4e6191fa707b9999b
Samiha Tasnim <samiha@apars.shop>
2026-02-08 13:51:56 +0600

    achievesscfmt26 launch

---
+++ shop/achieve/HSC_28/achievesscfmt26/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achievesscfmt26";
+const productName = "SSC 2026 Achieve Final Model Test";
+const productCode = "604";
+const fix = 4000;
+const pls = 3500;
+const Platform = "Offline";
+const init = 0;
