44e534602dfbbb3fcbee101c8d2f06b617c4ab41
Samiha Tasnim <samiha@aparsclassroom.com>
2026-02-09 15:20:11 +0600

    fix enrollment count

--- shop/bio27/Cycle-7/assets/ss.js
+++ shop/bio27/Cycle-7/assets/ss.js
@@ -1,14 +1,14 @@
 const product = "hbio27";
 const productName = "Pre-Medical General Knowledge & English Foundation Program 2027";
 const productCode = "601";
-//const productCode2 = "602";
+const productCode2 = "602";
 const fix = 2000;
 const pls = 1000;
 //const pls2 = 1250;
 const init = 0;
 let Platform = "Online";
 const Cycle = "Cycle-7";    
-//const Cycle2 = "Cycle-8";
+const Cycle2 = "Cycle-8";
 const vidD = document.getElementById('video');
 const clprc = document.getElementById('clprc');
 if (screen.width <= 600) {
@@ -62,44 +62,25 @@ if (screen.width <= 600) {
 //         console.log(err)
 //     })
 
-// Promise.all([
-//     fetch(`https://${shopName2}/enrollment/${Cycle}?productCode=${productCode}`).then(res => res.json()),
-//     fetch(`https://${shopName2}/enrollment/${Cycle}?productCode=${productCode2}`).then(res => res.json())
-// ])
-//     .then(([data1, data2]) => {
-//         const totalEnrollment = (data1.count || 0) + (data2.count || 0) + init;
+Promise.all([
+    fetch(`https://${shopName2}/enrollment/${Cycle}?productCode=${productCode}`).then(res => res.json()),
+    fetch(`https://${shopName2}/enrollment/${Cycle2}?productCode=${productCode2}`).then(res => res.json())
+])
+    .then(([data1, data2]) => {
+        const totalEnrollment = (data1.count || 0) + (data2.count || 0) + init;
 
-//         document.getElementById('enrolled').setAttribute('countTo', totalEnrollment);
+        document.getElementById('enrolled').setAttribute('countTo', totalEnrollment);
 
-//         if (document.getElementById('enrolled')) {
-//             const countUp = new CountUp('enrolled', totalEnrollment);
-//             if (!countUp.error) {
-//                 countUp.start();
-//             } else {
-//                 console.error(countUp.error);
-//             }
-//         }
-//     })
-//     .catch(err => {
-//         console.log("Enrollment fetch error:", err);
-//     });
-
-fetch(`https://${shopName2}/enrollment/${Cycle}?productCode=${productCode}`)
-    .then((res) => {
-        return res.json()
-    })
-    .then((data) => {
-        document.getElementById('enrolled').setAttribute('countTo', data.count + init);
         if (document.getElementById('enrolled')) {
-            const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
+            const countUp = new CountUp('enrolled', totalEnrollment);
             if (!countUp.error) {
                 countUp.start();
             } else {
                 console.error(countUp.error);
             }
         }
+    })
+    .catch(err => {
+        console.log("Enrollment fetch error:", err);
+    });
 
-    })
-    .catch((err) => {
-        console.log(err)
-    })
