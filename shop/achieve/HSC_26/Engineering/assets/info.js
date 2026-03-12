2c1a13ecdc9eedc65ec353116c9f83e4664e1198
Samiha Tasnim <samiha@aparsclassroom.com>
2026-03-12 15:35:07 +0600

    achieve eng 26 launch

---
+++ shop/achieve/HSC_26/Engineering/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achieveEngg26";
+const productName = "Achieve Engineering Exam Batch 26";
+const productCode = "608";
+const fix = 10500;
+const pls = 8000;
+const Platform = "Offline";
+const init = 0;
