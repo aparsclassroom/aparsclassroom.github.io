e77bb9bff9559f134eea2a250ee46700b80175d0
Samiha Tasnim <samiha@aparsclassroom.com>
2026-03-12 15:48:45 +0600

    achieve varsity 26 launch

---
+++ shop/achieve/HSC_26/Varsity/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achievevarsity26";
+const productName = "Achieve Varsity Exam Batch 26";
+const productCode = "607";
+const fix = 10500;
+const pls = 8000;
+const Platform = "Offline";
+const init = 0;
