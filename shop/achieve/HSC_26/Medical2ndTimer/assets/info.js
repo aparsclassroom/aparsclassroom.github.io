e841b54f4b6bc5ea15c1d4f71689e83470a75398
Samiha Tasnim <samiha@aparsclassroom.com>
2026-03-25 10:35:40 +0600

    medical2nd timer achieve launch

---
+++ shop/achieve/HSC_26/Medical2ndTimer/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achieve2ndtimemedi26";
+const productName = "ACHIEVE-RTD Medical 2nd Timer Exam Batch 2026";
+const productCode = "609";
+const fix = 10000;
+const pls = 7000;
+const Platform = "Offline";
+const init = 0;
