af6f8c4f5cd8b4e3b396c97b67945c07f5afd624
Samiha Tasnim <samiha@aparsclassroom.com>
2026-03-28 12:06:26 +0600

    launch english test paper solve course 26

---
+++ shop/testpapersolve26/assets/info.js
@@ -0,0 +1,7 @@
+const product = "testpapersolve26";
+const productName = "ACS HSC English Test Paper Solving A+ Power Pack Course";
+const productCode = "610";
+const fix = 1200;
+const pls = 690;
+const Platform = "Online";
+const init = 0;
