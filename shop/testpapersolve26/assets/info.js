9602c1b19a2975b7b43b5ab8de70a1f3dd1c1433
Samiha Tasnim <samiha@apars.shop>
2026-03-29 19:18:19 +0600

    testpapersolve course pricing reduce

--- shop/testpapersolve26/assets/info.js
+++ shop/testpapersolve26/assets/info.js
@@ -2,6 +2,6 @@ const product = "testpapersolve26";
 const productName = "ACS HSC English Test Paper Solving A+ Power Pack Course";
 const productCode = "610";
 const fix = 1200;
-const pls = 690;
+const pls = 490;
 const Platform = "Online";
 const init = 0;
