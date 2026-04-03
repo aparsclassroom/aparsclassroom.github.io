80018c623d5df5c1ef6697e6321a9722e42d88e7
Samiha Tasnim <samiha@apars.shop>
2026-04-03 14:50:12 +0600

    varsity launch

---
+++ shop/admission/HSC_26/Varsity/assets/info.js
@@ -0,0 +1,7 @@
+const product = "varsity26";
+const productName = "ACS Varsity + GST Special Private Programme 2026";
+const productCode = "613";
+const fix = 5000;
+const pls = 4000;
+const Platform = "Online";
+const init = 0;
