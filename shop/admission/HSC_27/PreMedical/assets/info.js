049bb1e2230e870817aa7655b8ac70a186f64471
Samiha Tasnim <samiha@aparsclassroom.com>
2026-04-17 16:09:50 +0600

    launch 27 pre medical

---
+++ shop/admission/HSC_27/PreMedical/assets/info.js
@@ -0,0 +1,8 @@
+const product = "medical27";
+const productName = "ACS Advanced Pre-Medical 27";
+const productCode = "620";
+//const productCode2 = "369";
+const fix = 6000;
+const pls = 5500;
+const Platform = "Online";
+const init = 0;
