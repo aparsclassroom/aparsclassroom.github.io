b7dae6d8a0fe1e593594975166573f0cead21aa8
Samiha Tasnim <samiha@apars.shop>
2026-04-18 21:44:52 +0600

    uddomi27 live

---
+++ shop/SchoolofExcellence/uddomi27/assets/info.js
@@ -0,0 +1,7 @@
+const product = "uddomi27";
+const productName = "ACS একাডেমিক বাংলা উদ্যমী ২৭ ব্যাচ";
+const productCode = "586";
+const fix = 2000;
+const pls = 990;
+const Platform = "Online";
+const init = 0;
