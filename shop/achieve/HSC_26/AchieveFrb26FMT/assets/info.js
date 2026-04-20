3bd50f66c12ceaaa7a63169d1aa4abe063303146
Samiha Tasnim <samiha@aparsclassroom.com>
2026-04-20 10:51:47 +0600

    achieve fmt new course replace

--- shop/achieve/HSC_26/AchieveFrb26FMT/assets/info.js
+++ shop/achieve/HSC_26/AchieveFrb26FMT/assets/info.js
@@ -1,7 +1,7 @@
-const product = "AchieveFrb26FMT";
+const product = "AchieveFrb26FMTnew";
 const productName = "ACHIEVE FRB Final Model Test 2026";
-const productCode = "617";
+const productCode = "634";
 const fix = 3500;
-const pls = 3000;
+const pls = 2500;
 const Platform = "Offline";
 const init = 0;
