70794f6e77f920571075547ae1c34956e4faad66
Samiha Tasnim <samiha@aparsclassroom.com>
2026-04-23 15:34:31 +0600

    achieve 2nd timer varsity 26 course launch

---
+++ shop/achieve/HSC_26/achievevarsity2ndtime26/assets/info.js
@@ -0,0 +1,7 @@
+const product = "achievevarsity2ndtime26";
+const productName = "ACHIEVE Varsity 2nd Time Exam Batch 2026";
+const productCode = "635";
+const fix = 10000;
+const pls = 6000;
+const Platform = "Offline";
+const init = 0;
