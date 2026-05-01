81a7dd8b9be8522332c1847a621538a39047935b
Samiha Tasnim <samiha@apars.shop>
2026-05-01 19:00:19 +0600

    abida apu course launch

---
+++ shop/sakib/quickrevisionbangla/assets/info.js
@@ -0,0 +1,7 @@
+const product = "quickrevisionbangla";
+const productName = "HSC কুইক রিভিশন অ্যান্ড ফাউন্ডেশন ব্যাচ";
+const productCode = "639";
+const fix = 500;
+const pls = 349;
+const Platform = "Online";
+const init = 0;
