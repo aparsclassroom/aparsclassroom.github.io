56dd765e19c0ba6a56e08abd61aa0a5f0cbd0cc2
Samiha Tasnim <samiha@apars.shop>
2026-05-05 22:19:18 +0600

    math28 pre book on

---
+++ shop/abhi/Math28/assets/info.js
@@ -0,0 +1,7 @@
+const product = "ebi28";
+const productName = "ACS Higher Math Academic Cycle";
+const productCode = "303";
+const fix = 2000;
+const pls = 1500;
+const Platform = "Online";
+const init = 0;
