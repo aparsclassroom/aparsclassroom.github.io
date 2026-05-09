8399d72db9bfbf593d06a891eb136ae03c63bd49
Samiha Tasnim <samxtasnim@gmail.com>
2026-05-09 20:58:09 +0600

    physics 28 pre book on

---
+++ shop/physics/b2m28/assets/info.js
@@ -0,0 +1,7 @@
+const product = "b2m28";
+const productName = "ACS Physics Academic Cycle";
+const productCode = "303";
+const fix = 2000;
+const pls = 1500;
+const Platform = "Online";
+const init = 0;
