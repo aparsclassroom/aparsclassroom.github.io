711141136923ed7fb129e7cf32a23e5ddab28ba0
Samiha Tasnim <samiha@aparsclassroom.com>
2026-05-11 23:24:18 +0600

    dr fahad pre book on

---
+++ shop/drfahad/SignatureSeries/Cycle-3/assets/info.js
@@ -0,0 +1,7 @@
+const product = "ebi28";
+const productName = "BIO SURVIVAL CODE: HSC 27 ACADEMIC COMEBACK";
+const productCode = "303";
+const fix = 2000;
+const pls = 1500;
+const Platform = "Online";
+const init = 0;
