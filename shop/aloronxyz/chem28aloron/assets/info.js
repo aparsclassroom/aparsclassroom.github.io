4243d0f1425b5087dc0031e77568816dcd410916
Samiha Tasnim <samiha@aparsclassroom.com>
2026-05-11 16:03:34 +0600

    update course name

--- shop/aloronxyz/chem28aloron/assets/info.js
+++ shop/aloronxyz/chem28aloron/assets/info.js
@@ -1,5 +1,5 @@
 const product = "ebi28";
-const productName = "ACS Chemistry Academic Cycle";
+const productName = "ACS HSC 28 Chemistry Academic Cycle";
 const productCode = "303";
 const fix = 2000;
 const pls = 1500;
