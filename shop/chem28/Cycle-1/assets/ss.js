ee688a08a8c936fe94920482c19d1a3f8e2d7dd6
Samiha Tasnim <samiha@aparsclassroom.com>
2026-05-13 11:56:05 +0600

    chem28 launch

---
+++ shop/chem28/Cycle-1/assets/ss.js
@@ -0,0 +1,102 @@
+// const product = "chem27aloron";
+// const productName = "ACS HSC 27 chemistry by Mottasin Pahlovi";
+// const productCode = "421";
+// const fix = 1500;
+// const pls = 1000;
+// const init = 0;
+// const Platform = "Online";
+const product = "chem28";
+const product2 = "chem28wbooks";
+const productName = "ACS Chemistry by ChemShifu";
+const productName2 = "ACS Chemistry by ChemShifu with books";
+let productCode = "650";
+let productCode2 = "465";
+const fix = 1500;
+const pls = 1000;
+const pls2 = 1350;
+const init = 0;
+const Platform = "Online";
+const Platform2 = "Physical";
+const Cycle = location.pathname.split('/')[3];
+const vidD = document.getElementById('video');
+const clprc = document.getElementById('clprc');
+if (screen.width <= 600) {
+    clprc.classList.add('fixed-bottom');
+} else {
+    clprc.classList.remove('fixed-bottom');
+    vidD.style.position = 'sticky';
+}
+
+var tag = document.createElement('script');
+
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// var player;
+
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         height: '390',
+//         width: '640',
+//         videoId: 'V0kspDWeEeA',
+//         playerVars: { 'autoplay': 1, 'playsinline': 1 },
+//         events: {
+//             'onReady': onPlayerReady
+//         }
+//     });
+// }
+
+// function onPlayerReady(event) {
+//     document.getElementById('thumb').style.display = "none";
+//     event.target.setVolume(100);
+//     event.target.playVideo();
+// }
+//  document.getElementById('moda').addEventListener('click', function () {
+//     if (player && typeof player.pauseVideo === 'function') {
+//         player.pauseVideo();
+//     }
+// });
+// fetch(`https://${shopName2}/enrollment/${Cycle}?productCode=${productCode}`)
+//     .then((res) => {
+//         return res.json()
+//     })
+//     .then((data) => {
+//         document.getElementById('enrolled').setAttribute('countTo', data.count + init);
+//         if (document.getElementById('enrolled')) {
+//             const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
+//             if (!countUp.error) {
+//                 countUp.start();
+//             } else {
+//                 console.error(countUp.error);
+//             }
+//         }
+
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     })
+
+// Promise.all([
+//     fetch(`https://${shopName2}/enrollment/${Cycle}?productCode=${productCode}`).then(res => res.json()),
+//     fetch(`https://${shopName2}/enrollment/${Cycle}?productCode=${productCode2}`).then(res => res.json())
+// ])
+//     .then(([data1, data2]) => {
+//         const totalEnrollment = (data1.count || 0) + (data2.count || 0) + init;
+
+//         document.getElementById('enrolled').setAttribute('countTo', totalEnrollment);
+
+//         if (document.getElementById('enrolled')) {
+//             const countUp = new CountUp('enrolled', totalEnrollment);
+//             if (!countUp.error) {
+//                 countUp.start();
+//             } else {
+//                 console.error(countUp.error);
+//             }
+//         }
+//     })
+//     .catch(err => {
+//         console.log("Enrollment fetch error:", err);
+//     });
+
+
+
