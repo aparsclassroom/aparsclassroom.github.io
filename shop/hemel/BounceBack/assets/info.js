b95aaeab25b29634833d5106fc7355acfc6eb57c
Samiha Tasnim <samiha@aparsclassroom.com>
2026-05-13 13:29:21 +0600

    bounce back launch

---
+++ shop/hemel/BounceBack/assets/info.js
@@ -0,0 +1,7 @@
+const product = "BounceBack";
+const productName = "Bounce Back 1.0 | SSC 27";
+const productCode = "658";
+const fix = 2000;
+const pls = 1500;
+const Platform = "Online";
+const init = 0;
