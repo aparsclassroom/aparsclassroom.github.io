cc4ecc7a564b39d5df8541c6007d69eecd3e16f7
Samiha Tasnim <samiha@aparsclassroom.com>
2026-05-18 18:55:22 +0600

    price change

--- shop/sakib/benjon28/assets/info.js
+++ shop/sakib/benjon28/assets/info.js
@@ -2,6 +2,6 @@ const product = "benjon28";
 const productName = "বাংলা ব‍্যঞ্জন-২৮";
 const productCode = "664";
 const fix = 2000;
-const pls = 1300;
+const pls = 1500;
 const Platform = "Online";
 const init = 0;
