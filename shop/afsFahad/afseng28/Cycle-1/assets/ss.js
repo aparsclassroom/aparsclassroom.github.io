92172b000296c04efb0a09e08dbb6f3aaf09c531
office <samiha@apars.shop>
2026-05-25 02:23:19 +0600

    fahad course launch

---
+++ shop/afsFahad/afseng28/Cycle-1/assets/ss.js
@@ -0,0 +1,62 @@
+const product = "afseng28";
+const productName = "HSC 28 English 1st Paper Mastery Course";
+const productCode = "700";
+const comboProductCode = "702";
+const fix = 1500;
+const pls = 1200;
+const init = 0;
+const Platform = "Online";
+const Cycle = location.pathname.split('/')[4];
+const vidD = document.getElementById('video');
+const clprc = document.getElementById('clprc');
+if (screen.width <= 600) {
+    clprc.classList.add('fixed-bottom');
+} else {
+    clprc.classList.remove('fixed-bottom');
+    vidD.style.position = 'sticky';
+}
+
+// var tag = document.createElement('script');
+
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// var player;
+
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         height: '390',
+//         width: '640',
+//         videoId: '',
+//         playerVars: { 'autoplay': 1, 'playsinline': 1 },
+//         events: {
+//             'onReady': onPlayerReady
+//         }
+//     });
+// }
+
+// function onPlayerReady(event) {
+//     document.getElementById('thumb').style.display = "none";
+//     event.target.setVolume(100);
+//     event.target.playVideo();
+// }
+Promise.all([
+    fetch(`https://${shopName2}/enrollment/${Cycle}?productCode=${productCode}`).then(res => res.json()),
+    fetch(`https://${shopName2}/enrollment?productCode=${comboProductCode}`).then(res => res.json())
+])
+    .then((enrollments) => {
+        const totalEnrollment = enrollments.reduce((total, data) => total + (data.count || 0), init);
+        document.getElementById('enrolled').setAttribute('countTo', totalEnrollment);
+        if (document.getElementById('enrolled')) {
+            const countUp = new CountUp('enrolled', totalEnrollment);
+            if (!countUp.error) {
+                countUp.start();
+            } else {
+                console.error(countUp.error);
+            }
+        }
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
