efdb72e074cac2314598adac0c0e5baa840995a5
Samiha Tasnim <samiha@aparsclassroom.com>
2026-06-02 17:40:47 +0600

    achieve medical 26 launch

---
+++ shop/achieve/HSC_26/Medical/assets/info.js
@@ -0,0 +1,7 @@
+const product = "AchieveMedical26";
+const productName = "Achieve Medical Exam Batch 26";
+const productCode = "752";
+const fix = 9500;
+const pls = 7000;
+const Platform = "Offline";
+const init = 0;
