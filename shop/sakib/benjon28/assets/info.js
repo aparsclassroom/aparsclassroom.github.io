391b4989c173b0150a388622d97747137bfb47cb
Samiha Tasnim <samiha@aparsclassroom.com>
2026-06-04 10:51:04 +0600

    change pricing benjon 28

--- shop/sakib/benjon28/assets/info.js
+++ shop/sakib/benjon28/assets/info.js
@@ -1,7 +1,7 @@
 const product = "benjon28";
 const productName = "HSC 28 - বাংলা ব্যঞ্জন ৫.০";
 const productCode = "664";
-const fix = 2000;
-const pls = 1500;
+const fix = 2500;
+const pls = 1900;
 const Platform = "Online";
 const init = 0;
