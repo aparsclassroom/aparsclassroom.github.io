0b2506993bb47de77d993b3d8a55386474805fea
Samiha Tasnim <samiha@aparsclassroom.com>
2026-06-18 13:15:48 +0600

    iba26

---
+++ shop/admission/HSC_26/IBA/assets/info.js
@@ -0,0 +1,7 @@
+const product = "iba26";
+const productName = "ACS IBA BBA Admission Private Batch 2026";
+const productCode = "811";
+const fix = 10000;
+const pls = 8000;
+const Platform = "Online";
+const init = 0;
