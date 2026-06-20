4198ad04ca8f4e616a454d9be7e2e4820e3f6c67
Samiha Tasnim <samiha@aparsclassroom.com>
2026-06-20 14:12:23 +0600

    add coupon auto apply

--- shop/admission/HSC_26/IBA/assets/info.js
+++ shop/admission/HSC_26/IBA/assets/info.js
@@ -2,6 +2,6 @@ const product = "iba26";
 const productName = "ACS IBA BBA Admission Private Batch 2026";
 const productCode = "811";
 const fix = 10000;
-const pls = 8000;
+const pls = 6000;
 const Platform = "Online";
 const init = 0;
