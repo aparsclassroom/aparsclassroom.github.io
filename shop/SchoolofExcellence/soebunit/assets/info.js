37bc81797ab0c450cd98e9103c1bcc7beab0876e
Samiha Tasnim <samiha@aparsclassroom.com>
2026-06-23 19:36:09 +0600

    soe course init

---
+++ shop/SchoolofExcellence/soebunit/assets/info.js
@@ -0,0 +1,7 @@
+const product = "soebunit";
+const productName = "School of Excellence 2nd Time (মানবিক ও বিভাগ পরিবর্তন) কোর্স";
+const productCode = "820";
+const fix = 4000;
+const pls = 2950;
+const Platform = "Online";
+const init = 0;
