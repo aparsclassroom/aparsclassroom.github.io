c5a25cf590da5f157155a7e21542852527f064a1
Samiha Tasnim <samiha@aparsclassroom.com>
2026-06-23 17:16:57 +0600

    jamil eng course init

---
+++ shop/jamil/jamileng28/Cycle-1/assets/ss.js
@@ -0,0 +1,70 @@
+const product = "jamileng28";
+const productName = "HSC 28 English Ultimate Preparation by Mokarrom Jamil";
+const init = 0;
+const Platform = "Online";
+const productCode = "817";
+const fix = 2000;
+// const regular discount price is configured below;
+const pls = 1500;
+const comboEnrollmentCode = "819";
+const Cycle = location.pathname.split('/')[4];
+const vidD = document.getElementById('video');
+const clprc = document.getElementById('clprc');
+if (screen.width <= 600) {
+    clprc.classList.add('fixed-bottom');
+} else {
+    clprc.classList.remove('fixed-bottom');
+    vidD.style.position = 'sticky';
+}
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         height: '390',
+//         width: '640',
+//         videoId: 'ksES-z0fNGk',
+//         playerVars: { 'autoplay': 1, 'playsinline': 1 },
+//         events: {
+//             'onReady': onPlayerReady
+//         }
+//     });
+// }
+
+// function onPlayerReady(event) {
+//     document.getElementById('thumb').style.display = "none";
+//     event.target.setVolume(100);
+//     event.target.playVideo();
+// }
+
+//  document.getElementById('moda').addEventListener('click', function () {
+//     if (player && typeof player.pauseVideo === 'function') {
+//         player.pauseVideo();
+//     }
+// });
+
+Promise.all([
+    fetch(`https://${shopName2}/enrollment/${Cycle}?productCode=${productCode}`).then(res => res.json()),
+    fetch(`https://${shopName2}/enrollment/?productCode=${comboEnrollmentCode}`).then(res => res.json())
+])
+    .then((enrollments) => {
+        const totalEnrollment = enrollments.reduce((total, data) => total + (data.count || 0), init);
+        document.getElementById('enrolled').setAttribute('countTo', totalEnrollment);
+        if (document.getElementById('enrolled')) {
+            const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
+            if (!countUp.error) {
+                countUp.start();
+            } else {
+                console.error(countUp.error);
+            }
+        }
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
