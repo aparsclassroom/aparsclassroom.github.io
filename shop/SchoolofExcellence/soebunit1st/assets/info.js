67a658542861d49e08d9aad42e1c70d3be41de6f
office <samiha@apars.shop>
2026-06-26 18:55:04 +0600

    first timer soe launch

---
+++ shop/SchoolofExcellence/soebunit1st/assets/info.js
@@ -0,0 +1,7 @@
+const product = "soebunit1st";
+const productName = "B Unit Excellent Online (মানবিক ও বিভাগ পরিবর্তন) Course";
+const productCode = "821";
+const fix = 5000;
+const pls = 3500;
+const Platform = "Online";
+const init = 0;
---
+++ shop/SchoolofExcellence/soebunit1st/assets/ss.js
@@ -0,0 +1,74 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1.35,
+    spaceBetween: 12,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    breakpoints: {
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 12,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        992: {
+            slidesPerView: 5,
+            spaceBetween: 3,
+        },
+    },
+});
+
+const vidD = document.getElementById('video');
+const clprc = document.getElementById('clprc');
+if (screen.width <= 600) {
+    clprc.classList.add('fixed-bottom');
+} else {
+    clprc.classList.remove('fixed-bottom');
+    vidD.style.position = 'sticky';
+}
+var tag = document.createElement('script');
+
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// var player;
+
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         height: '390',
+//         width: '640',
+//         videoId: '',
+//         playerVars: { 'autoplay': 1, 'playsinline': 1 },
+//         events: {
+//             'onReady': onPlayerReady
+//         }
+//     });
+// }
+
+// function onPlayerReady(event) {
+//     document.getElementById('thumb').style.display = "none";
+//     event.target.setVolume(100);
+//     event.target.playVideo();
+// }
+fetch(`https://${shopName2}/enrollment?productCode=${productCode}`)
+    .then((res) => {
+        return res.json()
+    })
+    .then((data) => {
+        document.getElementById('enrolled').setAttribute('countTo', data.count + init);
+        if (document.getElementById('enrolled')) {
+            const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
+            if (!countUp.error) {
+                countUp.start();
+            } else {
+                console.error(countUp.error);
+            }
+        }
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
