4dfded898cda4790b5a5163594cef3804123577f
Samiha Tasnim <samiha@aparsclassroom.com>
2026-07-07 15:59:37 +0600

    varsityb26 launch

---
+++ shop/achieve/HSC_26/VarsityB/assets/info.js
@@ -0,0 +1,8 @@
+const product = "achievevarsityB26";
+const productName = "Achieve Varsity B unit exam batch 2026";
+const productCode = "824";
+const achieveID = "6a43e1de75ecb00c068815e8";
+const fix = 6500;
+const pls = 4000;
+const Platform = "Offline";
+const init = 0;
