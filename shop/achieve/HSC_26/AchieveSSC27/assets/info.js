8ecdff523ae9bb08e19d8aa37b15318b83cd6b0d
Samiha Tasnim <samiha@aparsclassroom.com>
2026-07-08 11:04:53 +0600

    ssc27 model test achieve

---
+++ shop/achieve/HSC_26/AchieveSSC27/assets/info.js
@@ -0,0 +1,8 @@
+const product = "AchieveSSC27";
+const productName = "Achieve SSC27 Model Test";
+const productCode = "825";
+//const achieveID = "6a43e1de75ecb00c068815e8";
+const fix = 8000;
+const pls = 6000;
+const Platform = "Offline";
+const init = 0;
