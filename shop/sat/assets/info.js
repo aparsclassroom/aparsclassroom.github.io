d0aae7c00bf8d8c2f1bc5580a229da3b4a3dcce2
Samiha Tasnim <samiha@aparsclassroom.com>
2026-07-08 18:59:10 +0600

    sat launch

---
+++ shop/sat/assets/info.js
@@ -0,0 +1,7 @@
+const product = "sat";
+const productName = "SAT Live Course (August)";
+const productCode = "827";
+const fix = 12500;
+const pls = 9000;
+const Platform = "Online";
+const init = 0;
