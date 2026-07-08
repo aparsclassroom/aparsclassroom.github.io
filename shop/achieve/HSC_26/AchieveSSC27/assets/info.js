630e7c41b015c42c434e68b9720442f381b5ecc5
Samiha Tasnim <samiha@aparsclassroom.com>
2026-07-08 11:11:59 +0600

    delete achieve ssc27

--- shop/achieve/HSC_26/AchieveSSC27/assets/info.js
+++
@@ -1,8 +0,0 @@
-const product = "AchieveSSC27";
-const productName = "Achieve SSC27 Model Test";
-const productCode = "825";
-//const achieveID = "6a43e1de75ecb00c068815e8";
-const fix = 8000;
-const pls = 6000;
-const Platform = "Offline";
-const init = 0;
