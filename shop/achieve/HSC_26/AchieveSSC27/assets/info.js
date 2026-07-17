4f6a891f48a208cafae547fc7867e1f9470461fd
Samiha Tasnim <samiha@aparsclassroom.com>
2026-07-17 15:22:12 +0600

    fix achieve id in ssc 27 course

--- shop/achieve/HSC_26/AchieveSSC27/assets/info.js
+++ shop/achieve/HSC_26/AchieveSSC27/assets/info.js
@@ -1,7 +1,7 @@
 const product = "AchieveSSC27";
 const productName = "Achieve SSC27 Model Test";
 const productCode = "825";
-//const achieveID = "6a43e1de75ecb00c068815e8";
+const achieveID = "6a0e0be950f768051be3d07a";
 const fix = 8000;
 const pls = 6000;
 const Platform = "Offline";
