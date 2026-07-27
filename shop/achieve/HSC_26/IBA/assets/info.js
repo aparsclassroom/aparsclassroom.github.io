81a7002628e3f500cb15f73e129315a642025de2
Samiha Tasnim <samiha@aparsclassroom.com>
2026-07-27 11:09:14 +0600

    achieve iba launch

---
+++ shop/achieve/HSC_26/IBA/assets/info.js
@@ -0,0 +1,8 @@
+const product = "achieveIBA26";
+const productName = "Achieve IBA+BUP Full Season Mock Batch 2026";
+const productCode = "831";
+const fix = 10000;
+const pls = 7000;
+const Platform = "Offline";
+const init = 0;
+const achieveID = "6a65db58c95b771811122491";
