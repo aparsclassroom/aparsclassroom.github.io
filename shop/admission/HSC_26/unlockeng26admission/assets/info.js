36be409b354e8ebc3c7f0b11b2836e481aebfed2
Samiha Tasnim <samiha@aparsclassroom.com>
2026-07-28 11:03:09 +0600

    unlock eng admission course launch

---
+++ shop/admission/HSC_26/unlockeng26admission/assets/info.js
@@ -0,0 +1,7 @@
+const product = "unlockeng26admission";
+const productName = "ACS Unlock Admission English (B, C, D Unit & Others)";
+const productCode = "832";
+const fix = 1000;
+const pls = 800;
+const Platform = "Online";
+const init = 0;
