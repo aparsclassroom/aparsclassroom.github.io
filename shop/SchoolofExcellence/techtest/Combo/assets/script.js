c062e865254c36e48e051bb9d60ffe25d414faae
Samiha Tasnim <samiha@aparsclassroom.com>
2026-08-03 19:37:36 +0600

    change course code

--- shop/SchoolofExcellence/techtest/Combo/assets/script.js
+++ shop/SchoolofExcellence/techtest/Combo/assets/script.js
@@ -283,7 +283,7 @@ firebase.auth().onAuthStateChanged(function(e) {
                         redirect: 'follow'
                     };
 
-                    fetch(`https://${shopName2}/${productCode}/init`, requestOptions)
+                    fetch(`https://${shopName2}/836/init`, requestOptions)
                         .then(response => {
                             return response.text()
                         })
