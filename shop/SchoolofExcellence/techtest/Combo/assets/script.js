11d2580905ae627d4605e777b5c8d3bd16873184
Samiha Tasnim <samiha@aparsclassroom.com>
2026-08-03 19:32:19 +0600

    test bkash fail

--- shop/SchoolofExcellence/techtest/Combo/assets/script.js
+++ shop/SchoolofExcellence/techtest/Combo/assets/script.js
@@ -214,7 +214,7 @@ firebase.auth().onAuthStateChanged(function(e) {
                             redirect: 'follow'
                         };
 
-                        fetch(`https://${shopName2}/${productCode}/init`, requestOptions)
+                        fetch(`https://${shopName2}/836/init`, requestOptions)
                             .then(response => {
                                 return response.text()
                             })
