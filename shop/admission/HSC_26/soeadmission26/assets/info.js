c0e15ef547f551374bd4e6d658baea35545718ec
Samiha Tasnim <samiha@aparsclassroom.com>
2026-08-13 15:38:51 +0600

    soe admission course live

---
+++ shop/admission/HSC_26/soeadmission26/assets/info.js
@@ -0,0 +1,7 @@
+const product = "soeadmission26";
+const productName = "ACS-SoE Admission English (Medical, Varsity, Engineering)";
+const productCode = "833";
+const fix = 1190;
+const pls = 690;
+const Platform = "Online";
+const init = 0;
