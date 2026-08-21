8bc0824acc95904861fffd927f05db7828da8c55
Samiha Tasnim <samiha@aparsclassroom.com>
2026-08-21 16:38:15 +0600

    tanvir combo launch

---
+++ shop/tanvir/banglabodh28/Combo/assets/ss.js
@@ -0,0 +1,87 @@
+const product = "banglabodh28combo";
+const productName = "HSC 28 বাংলা বধ 1st paper + 2nd Paper Full Course";
+const productCode = "839";
+const fix = 3000;
+const pls = 2000;
+const init = 0;
+const Platform = "Online";
+const Cycle = location.pathname.split('/')[4];
+const vidD = document.getElementById('video');
+const clprc = document.getElementById('clprc');
+if (screen.width <= 600) {
+    clprc.classList.add('fixed-bottom');
+} else {
+    clprc.classList.remove('fixed-bottom');
+    vidD.style.position = 'sticky';
+}
+
+// var tag = document.createElement('script');
+
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// var player;
+
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         height: '390',
+//         width: '640',
+//         videoId: '',
+//         playerVars: { 'autoplay': 1, 'playsinline': 1 },
+//         events: {
+//             'onReady': onPlayerReady
+//         }
+//     });
+// }
+
+// function onPlayerReady(event) {
+//     document.getElementById('thumb').style.display = "none";
+//     event.target.setVolume(100);
+//     event.target.playVideo();
+// }
+// const sisterEnrollmentCountApi = `https://hsc.acsfutureschool.com/api/enrollments/count?product_code=${productCode}`;
+
+// const getEnrollmentCount = (url, dataPath = (data) => data.count) => {
+//     return fetch(url)
+//         .then((res) => res.json())
+//         .then((data) => Number(dataPath(data)) || 0)
+//         .catch((err) => {
+//             console.log(err);
+//             return 0;
+//         });
+// };
+
+// Promise.all([
+//     getEnrollmentCount(`https://${shopName2}/enrollment/?productCode=${productCode}`),
+//     getEnrollmentCount(sisterEnrollmentCountApi, (data) => data.data && data.data.count)
+// ]).then(([originalCount, sisterCount]) => {
+//     if (document.getElementById('enrolled')) {
+//         document.getElementById('enrolled').setAttribute('countTo', originalCount + sisterCount + init);
+//         const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
+//         if (!countUp.error) {
+//             countUp.start();
+//         } else {
+//             console.error(countUp.error);
+//         }
+//     }
+// });
+
+fetch(`https://${shopName2}/enrollment?productCode=${productCode}`)
+    .then((res) => {
+        return res.json()
+    })
+    .then((data) => {
+        document.getElementById('enrolled').setAttribute('countTo', data.count + init);
+        if (document.getElementById('enrolled')) {
+            const countUp = new CountUp('enrolled', document.getElementById("enrolled").getAttribute("countTo"));
+            if (!countUp.error) {
+                countUp.start();
+            } else {
+                console.error(countUp.error);
+            }
+        }
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
