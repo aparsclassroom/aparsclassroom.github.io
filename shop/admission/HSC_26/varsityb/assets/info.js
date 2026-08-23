aadc7051c5662ac796eebe8beb5f85da381dc771
Samiha Tasnim <samiha@aparsclassroom.com>
2026-08-23 17:58:02 +0600

    varsity b 26 info

--- shop/admission/HSC_26/varsityb/assets/info.js
+++ shop/admission/HSC_26/varsityb/assets/info.js
@@ -1,3 +1,8 @@
 const product = "varsityb26";
-const productName = "ACS ভার্সিটি ‘খ’ + বিভাগ পরিবর্তন এডমিশন প্রাইভেট ব্যাচ ২০২৬";
-const init = 165;
+const productName = "ACS ভার্সিটি ‘খ’ + বিভাগ পরিবর্তন এডমিশন প্রাইভেট ব্যাচ 2026";
+const productCode = "841";
+//const productCode2 = "369";
+const fix = 3000;
+const pls = 2500;
+const Platform = "Online";
+const init = 0;
