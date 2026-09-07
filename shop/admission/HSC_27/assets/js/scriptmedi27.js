6f1ce1d632649cca26c5f0ae26a5517c002787e8
Samiha Tasnim <samiha@aparsclassroom.com>
2026-09-07 10:37:24 +0600

    add special products to purchase block in medi27 fahaad

--- shop/admission/HSC_27/assets/js/scriptmedi27.js
+++ shop/admission/HSC_27/assets/js/scriptmedi27.js
@@ -241,7 +241,7 @@ firebase.auth().onAuthStateChanged(function (e) {
         var myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
         var raw = JSON.stringify({
-            "products": [productCode, productCode2],
+            "products": [productCode, productCode2, "847", "848"],
             'uid': e.uid
         });
 
