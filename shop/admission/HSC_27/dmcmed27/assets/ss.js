f313662679b002ca9434aa77ec096460a0181bc6
Samiha Tasnim <samiha@aparsclassroom.com>
2026-09-10 15:14:10 +0600

    add special products to enrollment count

--- shop/admission/HSC_27/dmcmed27/assets/ss.js
+++ shop/admission/HSC_27/dmcmed27/assets/ss.js
@@ -7,7 +7,7 @@ if (screen.width <= 600) {
     vidD.style.position = 'sticky';
 }
 
-fetch(`https://${shopName2}/enrollment/combined?productCodes=${productCode},${productCode2}`)
+fetch(`https://${shopName2}/enrollment/combined?productCodes=${productCode},${productCode2},854,855`)
     .then((res) => {
         return res.json()
     })
