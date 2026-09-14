6fb88bc764e72a2f21bf00a2b7d68075ebcd482e
office <samiha@apars.shop>
2026-09-14 19:44:39 +0600

    launch achieve medi master

---
+++ shop/achieve/HSC_26/Achievemedimaster26/assets/info.js
@@ -0,0 +1,8 @@
+const product = "Achievemedimaster26";
+const productName = "Achieve Medimaster Exam Batch 2026";
+const productCode = "856";
+const achieveID = "";
+const fix = 8000;
+const pls = 6000;
+const Platform = "Offline";
+const init = 0;
