ce62f4e9c3ff731f1b945131866f92b02782c91c
Samiha Tasnim <samiha@aparsclassroom.com>
2026-09-24 17:21:45 +0600

    achieve exam ssc frb 27 launch

---
+++ shop/achieve/HSC_26/achievesscfrb27/assets/info.js
@@ -0,0 +1,8 @@
+const product = "achievesscfrb27";
+const productName = "ACHIEVE SSC’27 Final Revision Exam Batch 2.0";
+const productCode = "863";
+const achieveID = "";
+const fix = 8000;
+const pls = 6000;
+const Platform = "Offline";
+const init = 0;
