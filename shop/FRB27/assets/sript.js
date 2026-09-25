af7544c1483029aac1c79bc12fced26fb1c856a4
office <samiha@apars.shop>
2026-09-25 21:14:38 +0600

    book img update

--- shop/FRB27/assets/sript.js
+++ shop/FRB27/assets/sript.js
@@ -175,7 +175,7 @@ document.getElementById('addBooks').addEventListener('change', function () {
                     <blockquote style="font-style: italic; color: #444;">"${randomQuote}"</blockquote>
                 </div>
             `,
-            imageUrl: 'https://i.postimg.cc/VNYBTDtZ/compact-series-1-1.jpg',
+            imageUrl: 'https://i.postimg.cc/5NXsmCCc/compressed-image.jpg',
             showCancelButton: true,
             confirmButtonText: 'না আমি বই নিতে চাইনা',
             cancelButtonText: 'হ্যাঁ আমি বই নিতে চাই',
