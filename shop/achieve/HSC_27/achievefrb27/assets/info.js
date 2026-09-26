11d5f2db6c7f6b4364075c06c876634e3cd10a40
Samiha Tasnim <samiha@aparsclassroom.com>
2026-09-26 18:01:08 +0600

    frb 27 achieve launch

---
+++ shop/achieve/HSC_27/achievefrb27/assets/info.js
@@ -0,0 +1,8 @@
+const product = "achievefrb27";
+const productName = "ACHIEVE HSC’27 FRB Offline Model Test";
+const productCode = "864";
+const achieveID = "";
+const fix = 9000;
+const pls = 7000;
+const Platform = "Offline";
+const init = 0;
