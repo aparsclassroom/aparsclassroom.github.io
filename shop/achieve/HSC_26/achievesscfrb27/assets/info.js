2826eb37aea032254be7f5be1538f96a1a341a26
Samiha Tasnim <samiha@aparsclassroom.com>
2026-09-26 18:13:00 +0600

    fix achieve ids

--- shop/achieve/HSC_26/achievesscfrb27/assets/info.js
+++ shop/achieve/HSC_26/achievesscfrb27/assets/info.js
@@ -1,7 +1,7 @@
 const product = "achievesscfrb27";
 const productName = "ACHIEVE SSC’27 Final Revision Exam Batch 2.0";
 const productCode = "863";
-const achieveID = "ssc2027finalrevisionexambatch2";
+const achieveID = "6ab663e200489bfd4540f2dd";
 const fix = 8000;
 const pls = 6000;
 const Platform = "Offline";
--- shop/achieve/HSC_27/achievefrb27/assets/info.js
+++ shop/achieve/HSC_27/achievefrb27/assets/info.js
@@ -1,7 +1,7 @@
 const product = "achievefrb27";
 const productName = "ACHIEVE HSC’27 FRB Offline Model Test";
 const productCode = "864";
-const achieveID = "hsc2027frbmodeltest";
+const achieveID = "6a1f4651dc91cbefac4e7650";
 const fix = 9000;
 const pls = 7000;
 const Platform = "Offline";
