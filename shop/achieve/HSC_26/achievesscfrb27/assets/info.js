febd4283d7508716b45d2fcb37da29ff01bba954
Samiha Tasnim <samiha@aparsclassroom.com>
2026-09-26 18:04:12 +0600

    add achieve id for ssc frb 27

--- shop/achieve/HSC_26/achievesscfrb27/assets/info.js
+++ shop/achieve/HSC_26/achievesscfrb27/assets/info.js
@@ -1,7 +1,7 @@
 const product = "achievesscfrb27";
 const productName = "ACHIEVE SSC’27 Final Revision Exam Batch 2.0";
 const productCode = "863";
-const achieveID = "";
+const achieveID = "ssc2027finalrevisionexambatch2";
 const fix = 8000;
 const pls = 6000;
 const Platform = "Offline";
