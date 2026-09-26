76f4431150850df4d61ed87f94f407a6f9436270
Samiha Tasnim <samiha@aparsclassroom.com>
2026-09-26 18:07:52 +0600

    add achieve id to frb achieve 27

--- shop/achieve/HSC_27/achievefrb27/assets/info.js
+++ shop/achieve/HSC_27/achievefrb27/assets/info.js
@@ -1,7 +1,7 @@
 const product = "achievefrb27";
 const productName = "ACHIEVE HSC’27 FRB Offline Model Test";
 const productCode = "864";
-const achieveID = "";
+const achieveID = "hsc2027frbmodeltest";
 const fix = 9000;
 const pls = 7000;
 const Platform = "Offline";
